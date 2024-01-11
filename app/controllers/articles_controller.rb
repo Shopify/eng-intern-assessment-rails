836fba30d15e8890623da36992330b98261bc67f
AnaCoda <anacod@zoho.com>
2024-01-11 16:36:13 -0700

    Add search functionality to main page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,11 @@
 class ArticlesController < ApplicationController
+  # Search for or display all articles
   def index
-    @articles = Article.all
+    if params[:search]
+      @articles = Article.search(params[:search])
+    else
+      @articles = Article.all
+    end
   end
 
   # New instantiates new article and is used for new.html.erb view
