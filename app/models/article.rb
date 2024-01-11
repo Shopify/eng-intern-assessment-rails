74dad84bd59cb6af381657cfdf54b4be73fee8e3
EliyaFarhat <eliyahfarhat@gmail.com>
2024-01-11 09:07:13 -0500

    Implemented the search functionality for the articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ApplicationRecord
+    # Class method for searching
+    def self.search(query)
+        # Convert title, content, and query to lower case (search wont be case sensitive now) and run an SQL 
+        # query returning articles where the query is contained anywhere within the title or content
+        where("lower(title) LIKE ? OR lower(content) LIKE ?", "%#{query.downcase}%", "%#{query.downcase}%")
+    end
 end
