60b7f3b96c4ba23751337ec679ef0064a9cb8404
AnaCoda <anacod@zoho.com>
2024-01-11 15:27:28 -0700

    Add form validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true, length: { minimum: 10 }
+  validates :author, presence: true
+  validates :date, presence: true
   # Search by content and title
   def self.search(query)
     Article.where('content LIKE ?', "%#{query}%").or(Article.where('title LIKE ?', "%#{query}%"))
