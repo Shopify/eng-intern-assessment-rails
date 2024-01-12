7f8abd3b5596325ad88aef552144731429b57adb
JP <85654561+jp524@users.noreply.github.com>
2024-01-12 16:57:28 -0500

    Add Article#new and #create actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,24 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to article_path(@article), notice: "Article was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content, :author, :date)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
 
   root "articles#index"
 
-  resources :articles, only: [ :index, :show ]
+  resources :articles, only: [ :index, :show, :new, :create ]
 end
