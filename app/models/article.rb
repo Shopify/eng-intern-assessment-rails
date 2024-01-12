a45dfddef19c0204992ff6ee004f178a0385d2f8
Nicholas Wood <horizon.nicholas14@gmail.com>
2024-01-12 15:14:09 -0500

    Update article comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,7 @@ class Article < ApplicationRecord
   validates :content, presence: true
 
   def self.search(query)
+    # Case-insensitive search that matches text from both article title and content
     where("title LIKE :search OR content LIKE :search", search: "%#{query}%").order(title: :asc)
   end
 end
