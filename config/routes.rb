5fcd9371eb4d3c3100a4b97aaf548380c4ec1960
JP <85654561+jp524@users.noreply.github.com>
2024-01-12 16:44:03 -0500

    Add Article#index action, and refactor using article partial

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  root "articles#show"
+  root "articles#index"
 
-  resources :articles, only: [ :show ]
+  resources :articles, only: [ :index, :show ]
 end
