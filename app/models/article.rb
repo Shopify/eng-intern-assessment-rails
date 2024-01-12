c682eea94d46c3464f34fb9565124b1588ae2120
Nicholas Wood <horizon.nicholas14@gmail.com>
2024-01-12 11:49:53 -0500

    Validate presence of title and content article fields

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+
   def self.search(query)
     where("title LIKE :search OR content LIKE :search", search: "%#{query}%").order(title: :asc)
   end
