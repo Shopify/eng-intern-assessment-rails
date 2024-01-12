24449887ffa5edb01fa8a67946d18b454423ebf6
JP <85654561+jp524@users.noreply.github.com>
2024-01-12 17:13:34 -0500

    Add Article#destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [ :show, :edit, :update ]
+  before_action :set_article, only: [ :show, :edit, :update, :destroy ]
 
   def index
     @articles = Article.all
@@ -29,6 +29,11 @@ class ArticlesController < ApplicationController
     else
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path, notice: "Article was successfully removed."
   end
 
   private
