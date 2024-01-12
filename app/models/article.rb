71bee4bcd3d76a2dba1204820b8ef54773f72b58
JP <85654561+jp524@users.noreply.github.com>
2024-01-12 17:50:28 -0500

    Add search scope to Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
+
+  scope :search, ->(query) { where("title LIKE ? or content LIKE ?", "%#{query}%", "%#{query}%") if query.present? }
 end
