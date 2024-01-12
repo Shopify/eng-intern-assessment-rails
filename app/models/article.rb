77c0530da57932a349554095171d5137943fa71d
Allan <allankong176@gmail.com>
2024-01-12 14:48:19 -0700

    allows creating new articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,12 @@
 class Article < ApplicationRecord
+    def check_dates
+      errors.add(:date, "published book date cannot be in the future") if self.date and self.date > Date.today
+    end  
+
+    validates :title, presence: true
+    validates :content, presence: true
+    validates :author, presence: true
+    
+    validates :date, presence: true
+    validate :check_dates
 end
