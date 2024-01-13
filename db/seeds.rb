ee4f2514b5d5ba0732fed0023468e0e53d994f61
JP <85654561+jp524@users.noreply.github.com>
2024-01-13 11:50:27 -0500

    Add database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,3 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Article.create(title: "Sample Article", content: "Lorem ipsum dolor sit amet.")
+Article.create(title: "Another Article", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+Article.create(title: "Article with Metadata", content: "Hello World!", author: "John Doe", date: Date.today)
