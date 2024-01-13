a8219e5b3ea4a76a4df27aa9b5f4c77bdd01db45
laxitshahi <laxitshahi@laxits-mbp.home>
2024-01-13 00:54:39 -0500

    added rspec-rails for bundler exec rspec command

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ApplicationRecord
+    def self.search(query)
+        if query
+            where('title LIKE ? OR content LIKE ?', "%#{query}", "%#{query}")
+        else
+            all
+    end
 end
