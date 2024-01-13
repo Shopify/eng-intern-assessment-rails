c9a79ff3fd6c11a2f9191ec40dc04a573dbb1ff7
JP <85654561+jp524@users.noreply.github.com>
2024-01-13 10:57:24 -0500

    Modify Article.search to be case-insensitive

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,5 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
 
-  scope :search, ->(query) { where("title LIKE ? or content LIKE ?", "%#{query}%", "%#{query}%") if query.present? }
+  scope :search, ->(query) { where("LOWER(title) LIKE ? or LOWER(content) LIKE ?", "%#{query.downcase}%", "%#{query.downcase}%") if query.present? }
 end
