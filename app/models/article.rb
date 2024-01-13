e0b4c1fdd60e8559e43639d4fd1ee33466786148
eqzhou81 <eqzhou81@gmail.com>
2024-01-12 17:55:35 -0800

    Create article model and create, read, and update functionality

---
+++ app/models/article.rb
@@ -0,0 +1,6 @@
+class Article < ApplicationRecord
+    validates :title, presence: true
+    validates :content, presence: true
+    validates :author, presence: true
+    validates :date, presence: true
+end
