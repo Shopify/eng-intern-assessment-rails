140d53f46f3fc344ea6abd74027bf378f2772a1e
Anh Minh Tran <anhminhtran235@gmail.com>
2024-01-12 22:57:01 -0800

    Fix constraints

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-  validates :title, :content, :author, :date, presence: true
+  validates :title, :content, presence: true
 
   def self.search(query)
     where('title LIKE ? OR content LIKE ?', "%#{query}%", "%#{query}%")
