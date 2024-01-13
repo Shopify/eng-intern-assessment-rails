c0cb5d0eaacc91f44639011f63044052112fccee
Johnson Duong <71087545+johnsonduong@users.noreply.github.com>
2024-01-13 17:00:02 -0500

    Add delete controller functionality and boilerplate for search bar view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,22 @@
-# The ArticlesController class handles the logic for managing articles in the application
+# The ArticlesController class handles the logic for managing articles in the application using CRUD operations
+# The ArticlesController class handles the CRUD operations for articles.
 class ArticlesController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
     # Retrieves all articles from the database
+    # GET /articles/
     def index
         @articles = Article.all
     end
 
+    # Retrieves the article with the specified ID
+    # GET /articles/:id
+    def show
+        @article = Article.find(params[:id])
+    end
+
     # Initializes a new article object
+    # GET /articles/new
     def new
         @article = Article.new
     end
@@ -15,10 +24,12 @@ class ArticlesController < ApplicationController
     # Creates a new article with the provided parameters
     # If the article is successfully saved, it redirects to the article's show page
     # If there are validation errors, it renders the new article form again
+    # POST /articles/
     def create
         @article = Article.new(article_params)
 
         if @article.save
+            flash[:notice] = 'Article was successfully created.'
             redirect_to @article
         else
             render 'new'
@@ -26,44 +37,48 @@ class ArticlesController < ApplicationController
     end
 
     # Deletes an article from the database
+    # DELETE /articles/:id
     def destroy
         @article = Article.find(params[:id])
-        @article.destroy
-        redirect_to articles_path
+        if @article.destroy
+          flash[:notice] = 'Article was successfully deleted.'    
+          redirect_to articles_path
+        else
+          flash[:alert] = 'Article could not be deleted.'    
+          redirect_to articles_path
+        end
     end
 
-    # Retrieves the article with the specified ID and assigns it to the instance variable @article.
+    # Retrieves the article with the specified ID and assigns it to the instance variable @article
+    # GET /articles/:id/edit
     def edit
         @article = Article.find(params[:id])
     end
 
     # Updates an article with the given parameters.
     # Redirects to the updated article if successful, otherwise renders the edit view.
+    # PATCH /articles/:id
     def update
         @article = Article.find(params[:id])
         if @article.update(article_params)
             flash[:notice] = 'Article was successfully updated.'
             redirect_to @article
         else
+            flash.now[:alert] = @article.errors.full_messages.join(", ")
             render :edit
         end
     end
     
     private
     
+    # Defines the permitted parameters for an article
     def article_params
         params.require(:article).permit(:title, :content, :author)
     end
 
+    # Handles the case when an article is not found
     def record_not_found
         flash[:alert] = 'Article not found.'
         redirect_to articles_path
-    end
-
-    private
-
-    # Defines the permitted parameters for an article
-    def article_params
-      params.require(:article).permit(:title, :content, :author)
     end
 end
