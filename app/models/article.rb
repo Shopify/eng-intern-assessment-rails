c865744820efb241f47638799b037d67dc236960
JohnneyChen <74382625+JohnneyChen@users.noreply.github.com>
2024-01-13 17:39:25 -0500

    Added validation for article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+
   def self.search(search_string)
     query = "LOWER(title) LIKE :search OR LOWER(content) LIKE :search"
     where(query, search: "%#{search_string.downcase}%")
