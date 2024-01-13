bf3056855b6f789f9397f74b1261f093a057a06c
Anh Minh Tran <anhminhtran235@gmail.com>
2024-01-12 22:36:28 -0800

    Add validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  validates :title, :content, :author, :date, presence: true
+
   def self.search(query)
     where('title LIKE ? OR content LIKE ?', "%#{query}%", "%#{query}%")
   end
