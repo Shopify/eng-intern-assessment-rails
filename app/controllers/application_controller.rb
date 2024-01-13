345333ef5cce4c1078fc94e31de47e9ab86335e9
JohnneyChen <74382625+JohnneyChen@users.noreply.github.com>
2024-01-13 13:59:41 -0500

    added error handling for access to non-existing article ids

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
+  def render_404
+    render :template => "errors/error_404", :status => 404
+  end
 end
