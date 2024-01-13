13983fad119a15132faa8e60dae7326f43ea63fc
Brianna Wang <74729817+b-ultramarine@users.noreply.github.com>
2024-01-12 23:07:34 -0500

    add CRUD actions in controller, views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,4 +12,47 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  # create, save a new article
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  # update, save an article
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # delete article
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content, :author, :date)
+  end
 end
