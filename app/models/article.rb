51542bcf93f8456c8b6e7177e10a186b78ebd246
Raiyan Sayeed <raiyan.business@gmail.com>
2024-01-13 13:16:57 -0500

    adds search functionality to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,12 @@
 class Article < ApplicationRecord
+    after_initialize :init
+
+    def init
+        self.date ||= DateTime.now
+        self.author ||= "System"
+    end
+
+    def self.search(text)
+       return Article.where("content LIKE '%#{text}%' OR title LIKE '%#{text}%'")
+    end
 end
