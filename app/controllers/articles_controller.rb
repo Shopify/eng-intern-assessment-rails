3b528380fc4e096c6b01a49debb970bb42c6d018
JohnneyChen <74382625+JohnneyChen@users.noreply.github.com>
2024-01-13 13:48:14 -0500

    Updated individual article display ui

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,7 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1 or /articles/1.json
   def show
+    @formatted_publish_date = @article.date&.strftime("%B %Y")
   end
 
   # GET /articles/new
