77995e1f30304211571241d7f2a3d9c3bde41df3
laxitshahi <laxitshahi@laxits-mbp.home>
2024-01-13 03:50:29 -0500

    added validation to ensure that empty titles or content is rejected on submission

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
+    # Ensure title and content are not empty, but allow anonymous author
+    validates :title, presence: true
+    validates :content, presence: true
+
     # Return article which matches some words in title or content, else return everything
     def self.search(query)
         if query
