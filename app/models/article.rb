f03229ec8e6b4054c8a06c2d567e23da501249eb
JohnneyChen <74382625+JohnneyChen@users.noreply.github.com>
2024-01-12 21:38:24 -0500

    Added search method on article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+  def self.search(search_string)
+    query = "LOWER(title) LIKE :search OR LOWER(content) LIKE :search"
+    where(query, search: "%#{search_string.downcase}%")
+  end 
 end
