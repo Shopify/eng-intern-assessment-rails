003e24adf70e52cebddb39f9abb6193c01b9fa55
Priyanshu Sharma <sharmapriyanshu119@gmail.com>
2024-01-13 17:47:55 +0530

    update: migration applied to set default for date field

--- db/migrate/20240113120906_defaultdate.rb
+++ db/migrate/20240113120906_defaultdate.rb
@@ -1,5 +1,6 @@
 class Defaultdate < ActiveRecord::Migration[7.1]
   def change
     change_column_default :articles, :date, Time.zone.now
+    
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_13_102446) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_13_120906) do
   create_table "articles", force: :cascade do |t|
     t.string "name"
     t.string "title"
     t.string "content"
-    t.datetime "date"
+    t.datetime "date", default: "2024-01-13 12:17:26"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
