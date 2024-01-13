360c77f7f3e85bedf03a6f2ed750c75d88fbc45c
Johnson Duong <71087545+johnsonduong@users.noreply.github.com>
2024-01-13 15:38:35 -0500

    Add edit functionality and controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 # The ArticlesController class handles the logic for managing articles in the application
 class ArticlesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
     # Retrieves all articles from the database
     def index
         @articles = Article.all
@@ -21,6 +23,41 @@ class ArticlesController < ApplicationController
         else
             render 'new'
         end
+    end
+
+    # Deletes an article from the database
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    # Retrieves the article with the specified ID and assigns it to the instance variable @article.
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    # Updates an article with the given parameters.
+    # Redirects to the updated article if successful, otherwise renders the edit view.
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = 'Article was successfully updated.'
+            redirect_to @article
+        else
+            render :edit
+        end
+    end
+    
+    private
+    
+    def article_params
+        params.require(:article).permit(:title, :content, :author)
+    end
+
+    def record_not_found
+        flash[:alert] = 'Article not found.'
+        redirect_to articles_path
     end
 
     private
