dc116236d28424057ead38d09d38c5f87919d931
ramjayakumar21 <ramjayakumar21@gmail.com>
2024-01-13 21:21:42 -0800

    Implement search box on index view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
     validates :title, presence: true
     validates :content, presence: true
+
+    def self.search(query)
+        Article.where("title like ?", "%#{query}%").or(Article.where("content like ?", "%#{query}%"))
+    end
 end
