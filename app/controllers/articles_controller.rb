eb33b5ed689877dd748ecfd367cfdafb6d615789
RayanBouteldja <70912595+RayanBouteldja@users.noreply.github.com>
2024-01-14 02:32:48 -0500

    Implement create functionality for CRUD operations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,11 +11,32 @@ class ArticlesController < ApplicationController
   def show
   end
 
-  def current_article
-    @article = Article.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    flash[:alert] = 'Article not found'
-    redirect_to articles_path
+  def new
+    @article = Article.new
   end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      flash[:alert] = "The Article was successfully  created!"
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def current_article
+      @article = Article.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      flash[:alert] = 'Article not found'
+      redirect_to articles_path
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :content, :author, :date)
+    end
 end
 
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,13 @@
 class Article < ApplicationRecord
-end
+    validates :title, presence: true
+    validates :content, presence: true, length: { minimum: 10, message: "is too short (minimum is 10 characters)" }
+    validates :author, length: { maximum: 50, message: "is too long (maximum is 50 characters)" }
+    validate :date_cannot_be_in_the_future
+    # Ensure that the article's publication date is not set in the future,
+    # as articles are expected to be written and published on or before the present date.
+    def date_cannot_be_in_the_future
+      if date.present? && date > Date.today
+        errors.add(:date, "cannot be in the future")
+      end
+    end
+  end
