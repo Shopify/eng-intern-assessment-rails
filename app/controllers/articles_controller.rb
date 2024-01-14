f32adbe85b2e8d07a7ec7184f11933822f8a0be1
ramjayakumar21 <ramjayakumar21@gmail.com>
2024-01-13 21:06:24 -0800

    Add error handling, CSS styling, and article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,32 @@
 class ArticlesController < ApplicationController
   def index
-  
+    @articles = Article.all
+
+  end
+
+  def show
+    @article = Article.find(params[:id])
+
   end
 
   def new
-
+    @article = Article.new
   end
 
+  # create new article from params, save it, and redirect to it
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+
+    if (@article.save)
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  # Private method to only allow certain parameters
+  private def article_params
+    params.require(:article).permit(:title, :author, :content, :date )
   end
 
 end
