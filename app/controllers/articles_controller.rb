011990fc4b83e150afdef28e92af1bc2961fda98
notzree <notzree@gmail.com>
2024-01-13 23:12:10 -0500

    Obey MVC patterns

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,8 @@ require 'json'
 
 class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
@@ -15,7 +17,6 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      save_to_elastic_index(@article)
       redirect_to @article
     else
       render :new, status: :unprocessable_entity
@@ -23,18 +24,13 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
-
     if @article.update(article_params)
-      save_to_elastic_index(@article)
       redirect_to @article
     else
       render :edit, status: :unprocessable_entity
@@ -42,9 +38,7 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     if @article.destroy
-      delete_from_elastic_index(@article)
       redirect_to articles_path, status: :see_other
     else
       render :error, status: :unprocessable_entity
@@ -53,20 +47,8 @@ class ArticlesController < ApplicationController
   end
 
   def search
-    @query = params[:query]
-    @type = params[:type]
-    word_count = @query.split(" ").size
-    if @type=="default" #if it satisfies these conditions, likely to be a phrase or description, better to semantic search
-      if @query.length >= 10 && word_count >= 3
-        @search_results = semantic_search(@query)
-      else #use lexical search
-        @search_results = lexical_search(@query)
-      end
-    elsif @type=="lexical"
-        @search_results = lexical_search(@query)
-    elsif @type=="semantic"
-      @search_results = semantic_search(@query)
-    end
+    @search_results = Article.search(params[:query], params[:type])
+    puts(@search_result)
     @articles = Article.all
     render 'index'
   end
@@ -76,86 +58,14 @@ class ArticlesController < ApplicationController
       params.require(:article).permit(:title, :content, :author)
     end
 
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def record_not_found
       flash[:alert] = "Article not found."
       redirect_to articles_path
     end
 
-    def lexical_search(query)
-      begin
-        articles = Article.where("title LIKE ? OR content LIKE ?", "%#{query}%", "%#{query}%").order(title: :asc)
-        @search_result = articles.map do |article|
-          {
-            article: article,
-            score: 10.0,
-          }
-        end
-      rescue => e
-        puts "An error occurred: #{e.message}"
-      end
-    end
 
-    def semantic_search(query)
-
-      begin
-        uri = URI("#{ENV['ES_URL']}/search-shopify-assessment/_search")
-        req = Net::HTTP::Get.new(uri,'Content-Type'=>'application/json', 'Authorization'=> "APIKey #{ENV['API_KEY']}")
-        req.body = {
-          "query": {
-            "text_expansion":{
-              "ml.inference.content_expanded.predicted_value":{
-                "model_id": ".elser_model_2_linux-x86_64",
-                "model_text": query,
-              }
-            }
-          }
-        }.to_json
-        response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
-          http.request(req)
-        end
-        response = JSON.parse(response.body)
-        # puts("response: #{response.inspect}")
-        search_result = []
-        #Elasticsearch returns sorted by score so no need to sort here.
-        response["hits"]['hits'].each do |hit|
-          data = {
-            article: Article.find(hit["_id"]),
-            score: hit["_score"],
-          }
-          puts("data: #{data.inspect}")
-          search_result.push(data)
-        end
-        return search_result
-      rescue => e
-        puts "An error occurred: #{e.message}"
-      end
-
-    end
-
-    def save_to_elastic_index(document)
-      document_data = {
-        content: document.content,
-        title: document.title,
-        author: document.author,
-        _extract_binary_content: true,
-        _reduce_whitespace: true,
-        _run_ml_inference: true
-      }
-      # Set up the HTTP request
-      uri = URI("#{ENV['ES_URL']}/search-shopify-assessment/_doc/#{document.id}?pipeline=search-shopify-assessment")
-      req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json', 'Authorization' => "ApiKey #{ENV['API_KEY']}")
-      req.body = document_data.to_json
-      response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
-        http.request(req)
-      end
-      puts("response: #{response.inspect}")
-    end
-
-    def delete_from_elastic_index(document)
-      uri = URI("#{ENV['ES_URL']}/search-shopify-assessment/_doc/#{document.id}")
-      req = Net::HTTP::Delete.new(uri, 'Content-Type' => 'application/json', 'Authorization' => "ApiKey #{ENV['API_KEY']}")
-      response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
-        http.request(req)
-      end
-    end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,9 +3,119 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true, length: { minimum: 10 }
   validates :author, presence: true
+  after_commit :save_to_elastic_index, on: [:create, :update]
+  after_commit :delete_from_elastic_index, on: [:destroy]
+
+
+  def self.search(query, type)
+    case type
+    when "default"
+      if query.length >= 10 && query.split(" ").size >= 3
+        semantic_search(query)
+      else
+        lexical_search(query)
+      end
+    when "lexical"
+      lexical_search(query)
+    when "semantic"
+      semantic_search(query)
+    else
+      raise "Invalid search type"
+    end
+  end
+
   private
 
   def set_default_date
     self.date ||= Date.today
   end
+
+  def self.lexical_search(query)
+
+    #Searches the database for articles that contain the query in the title or content
+    begin
+      articles = Article.where("title LIKE ? OR content LIKE ?", "%#{query}%", "%#{query}%").order(title: :asc)
+      @search_result = articles.map do |article|
+        {
+          article: article,
+          score: 10.0,
+        }
+      end
+    rescue => e
+      puts "An error occurred: #{e.message}"
+    end
+  end
+
+  #uses elasticsearch elser to look for semantically relevant articles
+  def self.semantic_search(query)
+    begin
+      uri = URI("#{ENV['ES_URL']}/search-shopify-assessment/_search")
+      req = Net::HTTP::Get.new(uri,'Content-Type'=>'application/json', 'Authorization'=> "APIKey #{ENV['API_KEY']}")
+      req.body = {
+        "query": {
+          "text_expansion":{
+            "ml.inference.content_expanded.predicted_value":{
+              "model_id": ".elser_model_2_linux-x86_64",
+              "model_text": query,
+            }
+          }
+        }
+      }.to_json
+      response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
+        http.request(req)
+      end
+
+      parsed_response = JSON.parse(response.body)
+
+      if parsed_response["hits"] && parsed_response["hits"]["hits"]
+        search_results = parsed_response["hits"]["hits"].map do |hit|
+          if hit["_id"]
+            article = Article.find_by(id: hit["_id"])
+            if article
+              { article: article, score: hit["_score"] }
+            end
+          end
+        end
+        return search_results
+      else
+        raise "Unexpected response format"
+      end
+
+      return search_results
+    rescue JSON::ParserError
+      puts "Error parsing the response from Elasticsearch"
+      return []
+    rescue => e
+      puts "An error occurred: #{e.message}"
+      return []
+    end
+  end
+
+  def save_to_elastic_index()
+    document_data = {
+      content: self.content,
+      title: self.title,
+      author: self.author,
+      _extract_binary_content: true,
+      _reduce_whitespace: true,
+      _run_ml_inference: true
+    }
+    # Set up the HTTP request
+    uri = URI("#{ENV['ES_URL']}/search-shopify-assessment/_doc/#{self.id}?pipeline=search-shopify-assessment")
+    req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json', 'Authorization' => "ApiKey #{ENV['API_KEY']}")
+    req.body = document_data.to_json
+    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
+      http.request(req)
+    end
+    puts("response: #{response.inspect}")
+  end
+
+  def delete_from_elastic_index()
+    uri = URI("#{ENV['ES_URL']}/search-shopify-assessment/_doc/#{self.id}")
+    req = Net::HTTP::Delete.new(uri, 'Content-Type' => 'application/json', 'Authorization' => "ApiKey #{ENV['API_KEY']}")
+    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+      http.request(req)
+    end
+  end
+
 end
