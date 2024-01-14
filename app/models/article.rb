7e06fb01a81135dccf4a9a5d7f76b2ddc38ab87a
Hamid Reza Khoramrokh <hr.kh1380@gmail.com>
2024-01-14 13:39:55 -0500

    Pass tests and add comments article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
+  # using validates to ensure that the title and content are present
   validates :title, presence: true
   validates :content, presence: true
+  # using scope to search for articles by title or content
   scope :search, -> (search) { where("title LIKE ? OR content LIKE ?", "%#{search}%", "%#{search}%")}
 
 end
