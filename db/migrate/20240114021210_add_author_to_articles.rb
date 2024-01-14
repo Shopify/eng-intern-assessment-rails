e941c6b8dd0c60955d66bffe3d2730c866abad0c
ryanmckim <mck649@gmail.com>
2024-01-13 19:11:30 -0800

    Delete local DB instances

--- db/migrate/20240114021210_add_author_to_articles.rb
+++
@@ -1,5 +0,0 @@
-class AddAuthorToArticles < ActiveRecord::Migration[7.1]
-  def change
-    add_column :articles, :author, :string
-  end
-end
--- db/migrate/20240114021513_add_date_to_articles.rb
+++
@@ -1,5 +0,0 @@
-class AddDateToArticles < ActiveRecord::Migration[7.1]
-  def change
-    add_column :articles, :date, :date
-  end
-end
