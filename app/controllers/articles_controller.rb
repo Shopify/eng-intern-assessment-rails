cfa26edf26825c2902cc429c33f93105c4856204
Hamid Reza Khoramrokh <hr.kh1380@gmail.com>
2024-01-14 16:00:32 -0500

    Add the edit funtionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
   def new
-    render "new"
+    render :new
   end
   def create
     # parsing the params hash to get the vaules
@@ -18,13 +18,31 @@ class ArticlesController < ApplicationController
       redirect_to root_path
     else
       # if the article doesn't save, show the user the form again with the errors
-      render "new", status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
   def show
     # finding the article by id
     @article = Article.find(params[:id])
     logger.debug @article
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:author, :title, :date, :content)
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :articles
   root "articles#index"
   post "/articles", to: "articles#create"
+  patch "/articles/:id", to: "articles#update"
 end
