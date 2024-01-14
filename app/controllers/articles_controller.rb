26676c2ae4f883dff3f93b30c884df260095ebf5
abdellahhn <abdellahhanane44@gmail.com>
2024-01-14 16:11:54 -0500

    final commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,24 @@
-class ArticlesController < ApplicationController
+# app/controllers/articles_controller.rb
 
+# Public: Controller responsible for managing articles.
+class ArticlesController < ApplicationController
+  # Public: Display the details of a specific article.
+  #
+  # GET /articles/:id
   def show
     @article = Article.find(params[:id])
   end
 
+  # Public: Display a form for creating a new article.
+  #
+  # GET /articles/new
   def new
     @article = Article.new
   end
 
+  # Public: Create a new article based on user input.
+  #
+  # POST /articles
   def create
     @article = Article.new(article_params)
 
@@ -18,12 +29,19 @@ class ArticlesController < ApplicationController
     end
   end
 
+  # Public: Display a form for editing an existing article.
+  #
+  # GET /articles/:id/edit
   def edit
     @article = Article.find(params[:id])
   end
 
+  # Public: Update an existing article based on user input.
+  #
+  # PATCH/PUT /articles/:id
   def update
     @article = Article.find(params[:id])
+
     if @article.update(article_params)
       redirect_to @article
     else
@@ -31,6 +49,9 @@ class ArticlesController < ApplicationController
     end
   end
 
+  # Public: Delete a specific article.
+  #
+  # DELETE /articles/:id
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
@@ -38,6 +59,9 @@ class ArticlesController < ApplicationController
     redirect_to root_path, status: :see_other
   end
 
+  # Public: Display a list of articles based on the search query.
+  #
+  # GET /articles
   def index
     if params[:search]
       @articles = Article.search(params[:search])
@@ -46,7 +70,9 @@ class ArticlesController < ApplicationController
     end
   end
 
-  #rails Strong Parameters, instead of passing the value to Article.New each time
+  # Private: Strong Parameters for article attributes.
+  #
+  # Returns a Hash with permitted article attributes.
   private
   def article_params
     params.require(:article).permit(:title, :content, :author, :date)
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,16 @@
+# app/models/article.rb
+
+# Public: Model representing an article in the application.
 class Article < ApplicationRecord
-  # Handling invalid user input for Articles
+  # Public: Validation rules for article attributes.
   validates :title, presence: true
   validates :content, presence: true, length: { minimum: 10 }
 
-  # validates :author, allow_nil: true
-  # validates :date, allow_nil: true
-
+  # Public: Search articles based on a search term.
+  #
+  # search_term - The String representing the term to search for in titles and content.
+  #
+  # Returns an ActiveRecord::Relation representing the articles matching the search term.
   def self.search(search_term)
     if search_term
       where("title LIKE ? OR content LIKE ?", "%#{search_term}%", "%#{search_term}%")
