73d4813c1e250d338b8ff8b10e202d2fed8d468b
notzree <notzree@gmail.com>
2024-01-13 20:43:28 -0500

    fix schema git bug

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,11 @@
 class Article < ApplicationRecord
+  before_create :set_default_date
   validates :title, presence: true
   validates :content, presence: true, length: { minimum: 10 }
   validates :author, presence: true
+  private
+
+  def set_default_date
+    self.date ||= Date.today
+  end
 end
