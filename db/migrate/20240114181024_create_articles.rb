7dec721e44c8661bb484e9fb38787be201ad7a2d
ZiyadBouazara <ziyad.bouazara1@ulaval.ca>
2024-01-14 13:17:25 -0500

    Migrated DB table Articles

--- db/migrate/20240114181024_create_articles.rb
+++ db/migrate/20240114181024_create_articles.rb
@@ -2,7 +2,9 @@ class CreateArticles < ActiveRecord::Migration[7.1]
   def change
     create_table :articles do |t|
       t.string :title
-      t.text :body
+      t.text :content
+      t.string :author
+      t.date :date
 
       t.timestamps
     end
