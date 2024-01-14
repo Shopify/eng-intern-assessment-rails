3041c4c5642508be372703cb13c741f6567b8a04
ramjayakumar21 <ramjayakumar21@gmail.com>
2024-01-13 22:51:11 -0800

    Fix CSS Styling

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
-    validates :title, presence: true
-    validates :content, presence: true
+    validates :title, presence: true, length: { minimum: 5, maximum: 100 }
+    validates :content, presence: true, length: { minimum: 10, maximum: 1000 }
 
     def self.search(query)
         Article.where("title like ?", "%#{query}%").or(Article.where("content like ?", "%#{query}%"))
--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,7 +1,5 @@
 require "test_helper"
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
 end
