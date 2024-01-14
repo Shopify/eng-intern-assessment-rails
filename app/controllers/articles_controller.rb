f14ed166e0e43b2a840f2633a943c1097f0afc29
Hamid Reza Khoramrokh <hr.kh1380@gmail.com>
2024-01-14 14:56:48 -0500

    Add new view and nedded changes in the controller and index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,20 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+  def new
+    render "new"
+  end
+  def create
+    title = params.require(:title)
+    content = params.require(:content)
+    author = params.require(:author)
+    date = params.require(:date)
+    @article = Article.new(title: title, content: content, author: author, date: date)
+    if @article.save
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  
-  resources :articles
+
+  resources :articles, only: [:new, :create]
   root "articles#index"
 
+  post "/artciles/new", to: "articles#new"
+  post "/articles", to: "articles#create"
 end
