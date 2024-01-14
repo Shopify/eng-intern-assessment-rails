01a8ab982775c6dd5c24de623bcfe9687050a82c
Hamid Reza Khoramrokh <hr.kh1380@gmail.com>
2024-01-14 13:24:47 -0500

    Add search to the article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+  scope :search, -> (search) { where("title LIKE ? OR content LIKE ?", "%#{search}%", "%#{search}%")}
+
 end
