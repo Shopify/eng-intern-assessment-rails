9a8e9a6682ae1317e1a858694681917921d32c06
Saad Qureshi <saadqureshi@saads-mbp.home>
2024-01-14 02:28:50 -0500

    ran rubucop on codebase

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -7,28 +7,24 @@ module Api
       skip_before_action :verify_authenticity_token
 
       def index
-        begin
-          @articles = Article.all
-          if @articles.empty?
-            flash[:alert] = 'No articles to show. Create an article to add to the forum.'
-            render :index
-          else
-            render :index, status: :ok
-          end
-        rescue => e
-          flash[:alert] = e.message
-          render status: :internal_server_error
+        @articles = Article.all
+        if @articles.empty?
+          flash[:alert] = 'No articles to show. Create an article to add to the forum.'
+          render :index
+        else
+          render :index, status: :ok
         end
+      rescue StandardError => e
+        flash[:alert] = e.message
+        render status: :internal_server_error
       end
 
       def show
-        begin
-          @article = Article.find(params[:id])
-          render :show, status: :ok
-        rescue ActiveRecord::RecordNotFound
-          flash[:alert] = 'Article not found'
-          render status: :not_found
-        end
+        @article = Article.find(params[:id])
+        render :show, status: :ok
+      rescue ActiveRecord::RecordNotFound
+        flash[:alert] = 'Article not found'
+        render status: :not_found
       end
 
       def new
@@ -36,60 +32,50 @@ module Api
       end
 
       def create
-        begin
-          @article = Article.new(article_params)
-          @article.date = Date.today
-          if @article.author.blank?
-            @article.author = 'Anonymous'
-          end
-          if @article.save
-            redirect_to article_path(@article)
-          else
-            flash[:alert] = @article.errors.full_messages
-          end
-        rescue => e
-          flash[:alert] = e.message
-          render status: :internal_server_error
+        @article = Article.new(article_params)
+        @article.date = Date.today
+        @article.author = 'Anonymous' if @article.author.blank?
+        if @article.save
+          redirect_to article_path(@article)
+        else
+          flash[:alert] = @article.errors.full_messages
         end
+      rescue StandardError => e
+        flash[:alert] = e.message
+        render status: :internal_server_error
       end
 
       def edit
-        begin 
-          @article = Article.find(params[:id])
-          render :edit, status: :ok
-        rescue ActiveRecord::RecordNotFound
-          flash[:alert] = 'Requested article does not exist'
-          redirect_to articles_path
-        end
+        @article = Article.find(params[:id])
+        render :edit, status: :ok
+      rescue ActiveRecord::RecordNotFound
+        flash[:alert] = 'Requested article does not exist'
+        redirect_to articles_path
       end
 
       def update
-        begin
-          @article = Article.find(params[:id])
-          if @article.update(article_params)
-            redirect_to article_path(@article)
-          else
-            flash[:alert] = 'Failed to update article'
-            render :edit
-          end
-        rescue ActiveRecord::RecordNotFound
-          flash[:alert] = 'Requested article does not exist'
-          redirect_to articles_path
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+          redirect_to article_path(@article)
+        else
+          flash[:alert] = 'Failed to update article'
+          render :edit
         end
+      rescue ActiveRecord::RecordNotFound
+        flash[:alert] = 'Requested article does not exist'
+        redirect_to articles_path
       end
 
       def destroy
-        begin
-          article = Article.find(params[:id])
-          if article&.destroy
-            redirect_to articles_path
-          else
-            render status: :not_found
-          end
-        rescue ActiveRecord::RecordNotFound
-          flash[:alert] = 'Requested article does not exist'
+        article = Article.find(params[:id])
+        if article&.destroy
+          redirect_to articles_path
+        else
           render status: :not_found
         end
+      rescue ActiveRecord::RecordNotFound
+        flash[:alert] = 'Requested article does not exist'
+        render status: :not_found
       end
 
       private
