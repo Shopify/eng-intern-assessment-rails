34054ba48f114021240ab87fe0f409787c123e56
ramjayakumar21 <ramjayakumar21@gmail.com>
2024-01-13 21:40:42 -0800

    Add rspec per specification and implement edit view and functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,12 +5,25 @@ class ArticlesController < ApplicationController
     else
       @articles = Article.all
     end 
-    
+
   end
 
   def show
     @article = Article.find(params[:id])
+  end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if (@article.update(article_params))
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   def new
