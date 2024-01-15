3a108b6afe5ec7fc29b47a5a215d4f893d36defd
Erick Yan <erick.yanzhang@gmail.com>
2024-01-14 23:58:25 -0500

    Created the application views and updated the rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Define a resourceful route for articles
+  resources :articles do
+    # Adding the search route
+    collection do
+      get 'search', to: 'articles#search'
+    end
+  end
+
+  # Set the root path to the articles index
+  root to: 'articles#index'
 end
