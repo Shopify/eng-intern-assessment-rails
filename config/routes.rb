01587cfd5587c62b4ed193c9ea02efa9429f6044
Hamza Mostafa <hamza@Hamzas-MacBook-Pro.local>
2024-01-14 22:02:57 -0500

    added some documentation for model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "articles#index"
 
+  # all basic routes for CRUD operations
   resources :articles
+  # search route
   get '/search', to: "articles#search"
 end
