b9b414d05f48c76cdbe5ac0d354e5ec4cc3e5467
RayanBouteldja <70912595+RayanBouteldja@users.noreply.github.com>
2024-01-14 21:45:08 -0500

    Add validation tests

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,12 +1,31 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
+  setup do
+    Article.delete_all
+  end
+
   test 'starts with no articles' do
     assert_equal 0, Article.count
   end
 
   test 'has search functionality' do
     assert_respond_to Article, :search
+  end
+
+  test 'requires a title' do
+    article = Article.new(content: 'Lorem ipsum dolor sit amet.')
+    assert_not article.valid?
+  end
+
+  test 'requires content' do
+    article = Article.new(title: 'Test Article')
+    assert_not article.valid?
+  end
+
+  test 'cannot create a new article with a future date' do
+    article = Article.create(title: 'Sample Article', content: 'Lorem ipsum dolor sit amet.', date: Date.new(2030,3,4))
+    assert_not article.valid?
   end
 
   test 'creates a new article' do
@@ -54,6 +73,38 @@ class ArticleTest < ActiveSupport::TestCase
     article1 = Article.create(title: 'Sample Article', content: 'Lorem ipsum dolor sit amet.')
     article2 = Article.create(title: 'Another Article', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
     results = Article.search('Lorem ipsum')
+    assert_includes results, article1
+    assert_includes results, article2
+  end
+
+  test 'returns all the encyclopedia when search is empty' do
+    article1 = Article.create(title: 'Sample Article', content: 'Lorem ipsum dolor sit amet.')
+    article2 = Article.create(title: 'Another Article', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
+    results = Article.search('')
+    assert_includes results, article1
+    assert_includes results, article2
+  end
+
+  test 'returns articles that matches the title or content' do
+    article1 = Article.create(title: 'Sample Article', content: 'Lorem ipsum dolor sit amet.')
+    article2 = Article.create(title: 'Another Book', content: 'Lorem ipsum dolor sit amet. Article adipiscing elit.')
+    results = Article.search('Article')
+    assert_includes results, article1
+    assert_includes results, article2
+  end
+
+  test 'returns articles that matches the number' do
+    article1 = Article.create(title: 'Sample Article 1', content: 'Lorem ipsum dolor sit amet.')
+    article2 = Article.create(title: 'Another Book 1', content: 'Lorem ipsum dolor sit amet. Article adipiscing elit.')
+    results = Article.search('1')
+    assert_includes results, article1
+    assert_includes results, article2
+  end
+
+  test 'displays articles that are case insensitive' do
+    article1 = Article.create(title: 'Sample Article', content: 'Lorem ipsum dolor another amet.')
+    article2 = Article.create(title: 'Another Article', content: 'Lorem ipsum dolor sit amet. Article adipiscing elit.')
+    results = Article.search('another')
     assert_includes results, article1
     assert_includes results, article2
   end
