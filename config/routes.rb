8f91c127210412613af56fb571420f38c94c68a4
Kenneth Xing <kenneth.xing@alumni.ubc.ca>
2024-01-15 21:28:37 -0800

    Implemented basic CRUD and MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'articles/index'
+  root 'articles#index'
 
   resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
