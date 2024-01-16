dbfd9f7bf555bf86e2feaa8ff1f8f68110c0e9ce
Jonathan Lee <jonathan.k.lee9@gmail.com>
2024-01-16 17:45:40 -0500

    Added Article model

---
+++ app/models/article.rb
@@ -0,0 +1,14 @@
+class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+  validate :validate_date
+
+  def self.search(text)
+    if query.present?
+      where("title LIKE ? OR content LIKE ?",
+      "%#{sanitize_sql_like(text)}%", "%#{sanitize_sql_like(text)}%")
+    else
+      all
+    end
+  end
+end
