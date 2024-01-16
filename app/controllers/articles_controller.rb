2bfcde5464783ad7af594ed89ee5b77d391c9824
Aman <apraka01@student.ubc.ca>
2024-01-16 02:31:44 -0800

    Added comments for app/controllers/articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,25 +1,32 @@
 class ArticlesController < ApplicationController
+  # Callback to set the article before certain actions
   before_action :set_article, only: %i[ show edit update destroy ]
+  # Make format_date method available to views as a helper method
   helper_method :format_date
 
+  # Retrieve all articles for the index page
   # GET /articles or /articles.json
   def index
     @articles = Article.all
   end
 
+  # Show the details of a specific article
   # GET /articles/1 or /articles/1.json
   def show
   end
 
+  # Initialize a new article for creating a new form
   # GET /articles/new
   def new
     @article = Article.new
   end
 
+  # Retrieve the article for editing
   # GET /articles/1/edit
   def edit
   end
 
+  # Create a new article based on the provided parameters
   # POST /articles or /articles.json
   def create
     @article = Article.new(article_params)
@@ -35,6 +42,7 @@ class ArticlesController < ApplicationController
     end
   end
 
+  # Update the article based on the provided parameters
   # PATCH/PUT /articles/1 or /articles/1.json
   def update
     respond_to do |format|
@@ -48,6 +56,7 @@ class ArticlesController < ApplicationController
     end
   end
 
+  # Destroy the specified article
   # DELETE /articles/1 or /articles/1.json
   def destroy
     @article.destroy!
@@ -58,7 +67,7 @@ class ArticlesController < ApplicationController
     end
   end
 
-  # New action to handle search queries
+  # Perform a search based on the provided query
   def search
     @search_results = Article.search(params[:query])
     render :search_results
@@ -69,12 +78,14 @@ class ArticlesController < ApplicationController
     def format_date(date)
       date.strftime('%B %d, %Y')
     end
-    # Use callbacks to share common setup or constraints between actions.
+
+    # Retrieve the specified article based on the ID
+    # Use callbacks to share common setup or constraints between actions
     def set_article
       @article = Article.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+    # Permit specific parameters for creating or updating an article
     def article_params
       params.require(:article).permit(:title, :content, :author, :date)
     end
