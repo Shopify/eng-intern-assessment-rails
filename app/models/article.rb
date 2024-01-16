b5179095b85eabc175b3be6a457f854e5d43b0a9
rick <36524998+jackhe-umich@users.noreply.github.com>
2024-01-15 18:46:49 -0800

    security

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
     def self.search(keyword)
-        Article.where("title LIKE ? OR content LIKE ?", '%'+keyword+'%', '%'+keyword+'%')
+        sanitized_keyword = Article.sanitize_sql_like(keyword)
+        Article.where("title LIKE ? OR content LIKE ?", '%'+sanitized_keyword+'%', '%'+sanitized_keyword+'%')
     end
 end
