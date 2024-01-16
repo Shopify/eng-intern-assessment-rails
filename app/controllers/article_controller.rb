e0199169c4629e454eac299be5e22a6cd61f40c6
Morgan Sun <64807003+sunmorgan@users.noreply.github.com>
2024-01-16 17:28:25 -0500

    Updated comments for clarity

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,14 +1,18 @@
 class ArticleController < ApplicationController
+    # GET: Fetch 
     def index
         @articles = Article.all
     end
-
+  
+    # Default
     def show 
     end
 
+    # Default
     def new 
     end
 
+    # POST: create a new article 
     def create
         @article = Article.new(article_params)
         if @article.save
@@ -18,22 +22,26 @@ class ArticleController < ApplicationController
         end
     end
 
+    # Default
     def edit
     end
 
+    # PUT: update a specified article
     def update
         if @article.update(article_params)
             redirect_to articles_url, notice: 'Article was successfully updated.'
         else
             render :edit
         end
-    
+   
+    # Delete: delete a specified article
     def destroy
         @article = Article.find(params[:id])
         @article.destroy
         redirect_to articles_path, notice: 'Article was successfully destroyed'
     end
 
+    # GET: searches article that matches query parameters
     def search
         @results = Article.search(params[:query])
     end
@@ -42,6 +50,7 @@ class ArticleController < ApplicationController
         @article = Article.find(params[:id])
     end
 
+    # Only looking for the below parameters
     def article_params
         params.require(:article).permit(:title, :content, :author, :date)
     end
