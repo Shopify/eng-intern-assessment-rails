4265bc389e50633b4d28960e075ec0bc77d0f1a8
Evan Dyce <endyce@gmail.com>
2024-01-16 00:23:49 -0800

    Adding search endpoint and some updates to viewing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "articles#index"
-  
-  get "up" => "rails/health#show", as: :rails_health_check
 
   get "/articles", to: "articles#index"
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get "/articles/:id", to: "articles#find"
 end
