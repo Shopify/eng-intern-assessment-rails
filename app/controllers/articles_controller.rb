fe3a1c504c74cb1cdc9a1643fd6e52682da66e2f
Evan Dyce <endyce@gmail.com>
2024-01-16 00:32:01 -0800

    Updating read options to be more 'rails like'

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  def find
+  def show
     @article = Article.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#find"
+  resources :articles
 end
