d73479cf9a332201a938d864e12d8f32d9ccb997
James Martin <j78marti@uwaterloo.ca>
2024-01-15 22:57:56 -0500

    [Styling] - Adjusted code to follow ruby style guide

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,20 +1,18 @@
 class Article < ApplicationRecord
-    validates :title, presence: true
-    validates :content, presence: true
+  validates :title, presence: true
+  validates :content, presence: true
 
-    # Returns all articles that match the search query or all articles if the search query is empty.
-    def self.search(search)
-        if search
-            Article.where(['title LIKE ?', "%#{search}%"]).or(Article.where(['content LIKE ?', "%#{search}%"]))
-        else
-            Article.all
-        end
-    end
+  # Returns all articles that match the search query or all articles if the search query is empty.
+  def self.search(search)
+    return Article.where(
+        ['title LIKE ?', "%#{search}%"]).or(Article.where(['content LIKE ?', "%#{search}%"])
+    ) if search
+    return Article.all
+  end
 
-    # Returns the author of the article, or 'Anonymous' if no author is specified.
-    def author
-        return 'Anonymous' if self[:author].blank?
-        return self[:author]
-    end
-
+  # Returns the author of the article, or 'Anonymous' if no author is specified.
+  def author
+    return 'Anonymous' if self[:author].blank?
+    return self[:author]
+  end
 end
