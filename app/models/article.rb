6f34dbc8465515f86297983d0b098bb476d64f42
Aman <apraka01@student.ubc.ca>
2024-01-16 01:24:41 -0800

    Articles need to have title and content to be valid.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
+    validates_presence_of :title
+    validates_presence_of :content
+
     def self.search(query)
         where("title LIKE ? OR content LIKE ? OR author LIKE ? OR date LIKE ?", "%#{query}%", "%#{query}%", "%#{query}%", "%#{query}%")
     end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'validates presence of title' do
+      # This test is to ensure that creating an article without a title is considered invalid
+      article = Article.new(content: 'Lorem ipsum')
+      expect(article).not_to be_valid
+    end
+
+    it 'validates presence of title' do
+      # This test is to ensure that creating an article without content is considered invalid
+      article = Article.new(title: 'Lorem ipsum')
+      expect(article).not_to be_valid
+    end
+  end
 end
