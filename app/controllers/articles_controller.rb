3ccfe58a67064c91c58ca54dea8cf9cc87d68c60
Shivom Sharma <shars119@mcmaster.ca>
2024-01-16 15:26:44 -0600

    Basic CRUD functionality created
    Ability to create, read, update and delete articles has been added. Still need to integrate author, as well as the date in the form.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,6 +33,11 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to root_path, status: :see_other
+  end
   private
     def article_params
       params.require(:article).permit(:title, :content)
