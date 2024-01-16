04bf46c577275efa3e4201730fb63fac0fe17285
Evan Dyce <endyce@gmail.com>
2024-01-16 00:07:41 -0800

    Adding initial route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  root "articles#index"
+  
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "/articles", to: "articles#index"
   # Defines the root path route ("/")
   # root "posts#index"
 end
