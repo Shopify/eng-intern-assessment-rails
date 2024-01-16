2198614883b1a5f501070289134df73d7f1ee4ff
Aman <apraka01@student.ubc.ca>
2024-01-15 17:47:20 -0800

    Added search to views so that users can use search bar.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -57,6 +57,12 @@ class ArticlesController < ApplicationController
     end
   end
 
+  # New action to handle search queries
+  def search
+    @search_results = Article.search(params[:query])
+    render :search_results
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_article
