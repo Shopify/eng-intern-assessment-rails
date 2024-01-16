a1898cc54bd93f0e6b80d9b96b9b88e141550e5d
Evan Dyce <endyce@gmail.com>
2024-01-16 00:50:54 -0800

    Updating to match schema of tests:

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,18 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+      @article = Article.new(title: "...", body: "...", author: "...", date: "...")
+
+      if @article.save
+          redirect_to @article
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema[7.1].define(version: 2024_01_16_080505) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.text "body"
+    t.text "content"
     t.string "author"
     t.date "date"
     t.datetime "created_at", null: false
