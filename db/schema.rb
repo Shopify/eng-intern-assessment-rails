f1e4aea1724a2f4b3c6d0e01e7fede111b1ac07c
Evan Dyce <endyce@gmail.com>
2024-01-16 12:34:13 -0800

    Adding default values to database entries

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_16_080505) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.string "author"
-    t.date "date"
+    t.string "author", default: "Anonymous"
+    t.date "date", default: "2024-01-16"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
