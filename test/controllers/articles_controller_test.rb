1f9d678cdb7d8451ad0114da8014417bece232cf
Rishi Ravikumar <rishiravi.k98@gmail.com>
2024-01-17 17:19:44 -0500

    test: add controller tests for show and create routes

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @article = Article.create(title: 'Test Article 1', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lacinia', 
+      author: 'RRK1000', date: Date.today)
+    assert_equal(@article, Article.last)
+  end
+
+  test "should get index" do
+    get articles_url
+    assert_response :success
+  end
+
+  test "should create article" do
+    assert_difference("Article.count") do
+      post articles_url, params: { article: { title: @article.title, content: @article.content, 
+        author: @article.author, date: @article.date } }
+    end
+  
+    assert_redirected_to article_path(Article.last)
+  end
+
+  test 'should show article' do
+    get articles_url(@article)
+    assert_response :success
+  end
+  
+
 end
