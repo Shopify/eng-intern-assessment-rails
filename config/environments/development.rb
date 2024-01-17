2117ddcb2d30bc6b9d358567d98e66cf4c4f40e1
Amal Rajan <amalrajan74@gmail.com>
2024-01-17 14:13:41 -0500

    Update hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,11 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
+  # Allow requests from any host
+  # Because, we cannot predict the host name of the container
+  config.hosts = []
+
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
