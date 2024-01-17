a6f459b07f962d5cad5c4dc8892650f8bdf56d2b
Johnny-Q <johnnyqu4@gmail.com>
2024-01-17 13:43:57 -0500

    add automatic date setting on article creation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.date = Date.today
 
     if @article.save
       redirect_to @article
