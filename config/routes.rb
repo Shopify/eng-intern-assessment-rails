3a23fcfea5309cf5c3bc8e69658ec3b0d4b62386
Rishi Ravikumar <rishiravi.k98@gmail.com>
2024-01-17 15:34:56 -0500

    feat: add route resources method

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
   get "/articles", to: "articles#index"
   get "/articles/:id", to: "articles#show"
 
   # Defines the root path route ("/")
   root "articles#index"
+
+  resources :articles
 end
