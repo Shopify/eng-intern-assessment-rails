3eb2abf43625053c22318b68a0cd41c8fa6f241d
andressamachado <andressa.pamachado@gmail.com>
2024-01-17 16:28:47 -0500

    Add Home/Landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  # Defines the root path route ("/")
+  # TODO: Landing page with link to article list page
+  root "home#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :articles
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  root 'home#index'
 end
