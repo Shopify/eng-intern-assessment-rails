c4c482022e41f5b583743ab2a0178ee5a99fcc4c
Johnny-Q <johnnyqu4@gmail.com>
2024-01-17 13:15:49 -0500

    add basic implementation of article show route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,8 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show #implement error handling if article not found?
+    @article = Article.find(params[:id])
+  end
 end
