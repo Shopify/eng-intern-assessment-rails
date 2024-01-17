8b6314c15ec7a2a5161978fec5bbf2cca1e50d9f
Rishi Ravikumar <rishiravi.k98@gmail.com>
2024-01-17 17:37:55 -0500

    feat: add route to delete an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -35,6 +35,17 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  def home
+    redirect_to root_path
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :content, :author, :date)
