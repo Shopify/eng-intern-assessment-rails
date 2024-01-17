ad4f88cc1cbe21ff2955c7612040067e19b0a8d1
Evan Dyce <endyce@gmail.com>
2024-01-17 00:23:36 -0800

    Fixing logic in db default values

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,14 @@ class Article < ApplicationRecord
     validates :title, presence: true
     validates :content, presence: true
 
+    after_initialize :init
+    
+    # initialize to default values unless they are specified
+    def init
+        self.date = Date.today unless self.date
+        self.author = "Anonymous" unless self.author
+    end
+
     # query to return an article when the search term matches the content or title of an article. 
     def self.search(query)
         if query.present?
--- db/migrate/20240116080505_create_articles.rb
+++ db/migrate/20240116080505_create_articles.rb
@@ -3,8 +3,8 @@ class CreateArticles < ActiveRecord::Migration[7.1]
     create_table :articles do |t|
       t.string :title
       t.text :content
-      t.string :author, default: "Anonymous"
-      t.date :date, default: Date.today
+      t.string :author
+      t.date :date
 
       t.timestamps
     end
