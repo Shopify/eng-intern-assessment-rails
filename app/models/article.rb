27ff36349bd914c5141f5b42e86fc9750fb8dc0d
aliL2000 <aliladha2000@gmail.com>
2024-01-17 14:15:57 -0500

    Adding delete, search functionality

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,13 @@
 class Article < ApplicationRecord
+    #Validate that the required fields: Title and Content, are always supplied to the Model
+    validates :title, presence: true
+    validates :content, presence: true
+
+    def self.search(query)
+        if query
+          where('title LIKE ? OR content LIKE ?', "%#{query}%", "%#{query}%")
+        else
+          all
+        end
+      end
 end
