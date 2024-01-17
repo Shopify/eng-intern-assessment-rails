02b23b0eb6361cef90088049223c80ff387352ba
Johnny-Q <johnnyqu4@gmail.com>
2024-01-17 14:45:55 -0500

    add search function on Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,14 @@ class Article < ApplicationRecord
     validates :content, presence: true, length: { minimum: 10 }
     validate :date_cannot_be_future
 
+    def self.search(search)
+        if search.blank?
+            all
+        else
+            where("title LIKE ? OR content LIKE ?", "%#{sanitize_sql_like(search)}%", "%#{sanitize_sql_like(search)}%") 
+        end
+    end
+
     private
 
     def date_cannot_be_future
