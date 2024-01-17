97d67adb7a855656d9784ca6f47f406982a37ca0
andressamachado <andressa.pamachado@gmail.com>
2024-01-17 16:37:57 -0500

    Add method create to Article model.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,14 @@
+# frozen_string_literal: true
+
 # Article model
 class Article < ApplicationRecord
-  # Ensure Title and Content fields are present.
-  validates :title, :content, presence: true
-  # Ensure Content is unique.
-  validates :content, uniqueness: true
-  # Ensure Title is unique per Author.
-  validates :title, uniqueness: { scope: :author, message: 'must be unique per author' }
+  class << self
+    # Create a new Article instance.
+    def create(title:, content:, author: "Anonymous", date: Time.zone.today)
+      article = Article.new(author: author, title: title, content: content, date: date)
+      article.save
 
-
+      article
+    end
+  end
 end
