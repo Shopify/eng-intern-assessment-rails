ba80c1d18a9da25c5dea3a4f9b53514ab5e9229b
andressamachado <andressa.pamachado@gmail.com>
2024-01-17 16:40:55 -0500

    Add method search to Article model.

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,13 @@
 
 # Article model
 class Article < ApplicationRecord
+  # Ensure Title and Content fields are present.
+  validates :title, :content, :author, :date, presence: true
+  # Ensure Content is unique.
+  validates :content, uniqueness: true
+  # Ensure Title is unique per Author.
+  validates :title, uniqueness: { scope: :author, message: "must be unique per author" }
+
   class << self
     # Create a new Article instance.
     def create(title:, content:, author: "Anonymous", date: Time.zone.today)
@@ -9,6 +16,11 @@ class Article < ApplicationRecord
       article.save
 
       article
+    end
+
+    # Search for articles by Title or Content.
+    def search(term)
+      Article.where("title LIKE ? OR content LIKE ?", "%#{term}%", "%#{term}%")
     end
   end
 end
