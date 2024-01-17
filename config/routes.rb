5aa8464feff851c4c185b1c9fc350622e208045b
Shivom Sharma <shars119@mcmaster.ca>
2024-01-16 21:40:16 -0600

    Completed Intern Challenge!
    Completed the intern challenge, added comments and ran all tests passing! Also some housekeeping on route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-
   #Define root route for articles
   root "articles#index"
-  # get 'search', to: "articles#search"
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   resources :articles
 
