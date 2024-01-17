3e2c0a07a5fd5946f7ec164d9b2a3e41fdc5c9c9
Johnny-Q <johnnyqu4@gmail.com>
2024-01-17 13:36:49 -0500

    add basic implementation for article creation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,24 @@ class ArticlesController < ApplicationController
   def show #implement error handling if article not found?
     @article = Article.find(params[:id])
   end
+
+  def new #implement error handling if article not found?
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content, :author)
+  end
 end
