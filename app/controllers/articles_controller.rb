ae43d93d86ab79fbcf2f4a9cf17ee123eaccb43f
Edward Liono <edward@liono.me>
2024-01-17 18:28:35 -0500

    Implemented article Read and Create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,30 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all # Gets all the articles
+  end
+
+  def show
+    @article = Article.find(params[:id]) # Gets the article with specific id
+  end
+
+  def new
+    @article = Article.new # Creates a new article but doesnt save
+  end
+
+  def create
+    # Initialize a new article with the inputed parameters
+    @article = Article.new(article_params)
+
+    if @article.save # Attempt to save the article
+      redirect_to @article # Redirect to the saved article if sucessful
+    else
+      render :new, status: :unprocessable_entity # Show errors
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content, :author, :date)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,17 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true, length: { minimum:10 }
+  validate :date_cannot_be_in_the_future
+
+  before_validation :set_defaults
+
+  def date_cannot_be_in_the_future
+    errors.add(:date, "Date can't be in the future") if
+      !date.blank? and date > Date.today
+  end
+
+  def set_defaults
+    self.author = "Anonymous" if self.author.blank?
+    self.date ||= Date.today
+  end
 end
