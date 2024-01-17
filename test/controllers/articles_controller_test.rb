185fd0b2fe4ff35fc3c721e3c591a4cd47d3eea7
Rishi Ravikumar <rishiravi.k98@gmail.com>
2024-01-17 17:42:02 -0500

    test: add controller tests for edit article route

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,10 +1,12 @@
-require "test_helper"
+require '/Users/rrk/ruby/eng-intern-assessment-rails/test/test_helper.rb'
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @article = Article.create(title: 'Test Article 1', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lacinia', 
+    @article1 = Article.create(title: 'Test Article 1', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lacinia', 
       author: 'RRK1000', date: Date.today)
-    assert_equal(@article, Article.last)
+    @article2 = Article.create(title: 'Test Article 3', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lacinia', 
+      author: 'RRK1000', date: Date.today)
+    assert_equal(@article2, Article.last)
   end
 
   test "should get index" do
@@ -14,16 +16,21 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
 
   test "should create article" do
     assert_difference("Article.count") do
-      post articles_url, params: { article: { title: @article.title, content: @article.content, 
-        author: @article.author, date: @article.date } }
+      post articles_url, params: { article: { title: @article1.title, content: @article1.content, 
+        author: @article1.author, date: @article1.date } }
     end
   
     assert_redirected_to article_path(Article.last)
   end
 
   test 'should show article' do
-    get articles_url(@article)
+    get articles_url(@article1)
     assert_response :success
+  end
+
+  test 'should edit article' do
+    patch article_url(@article1), params: { article: { title: @article2.title, content: @article2.content, author: @article2.author, date: @article2.date } }
+    assert_redirected_to article_url(@article1)
   end
   
 
