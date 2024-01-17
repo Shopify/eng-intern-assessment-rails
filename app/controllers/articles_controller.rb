dfef35a5acb1f582ebf05b598018c5e264dd31c3
Jonathan Lee <jonathan.k.lee9@gmail.com>
2024-01-17 11:59:24 -0500

    Added new and create article controllers, and articles list in index view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,31 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    @article = Article.find_by(id: params[:id])
+
+    if @article.nil?
+      redirect_to root_path, alert: "Article #{params[:id]} not found"
+    end
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :author, :content, :created_date)
+  end
 end
