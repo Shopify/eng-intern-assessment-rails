47b0b87c2bfd70504793c36e9685b3211ec59bf3
aliL2000 <aliladha2000@gmail.com>
2024-01-17 17:49:21 -0500

    Fix documentation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class ArticlesController < ApplicationController
   # Show articles
   def index
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Article < ApplicationRecord
   # Validate that the required fields: Title and Content, are always supplied to the Model
   validates :title, presence: true
