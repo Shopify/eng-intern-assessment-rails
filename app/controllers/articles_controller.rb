e0d384dfe127701e0746344f9538842204a79ba0
Saeid Alizadeh <saeidalz96@gmail.com>
2024-01-16 20:18:20 -0700

    initial commit; started defining the skeleton of the project

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,26 @@
+class ArticlesController < ApplicationController
+  def index
+  end
+
+  def allarticles
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
---
+++ db/migrate/20240117021610_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration[7.1]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.string :author
+      t.date :date
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
