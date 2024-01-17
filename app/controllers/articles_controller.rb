0be9ada57987910ea92717727c630d9b4e6e36f4
aliL2000 <aliladha2000@gmail.com>
2024-01-17 14:25:18 -0500

    Remove unnecessary code in the controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,19 +3,4 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
-
-  def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
-
-    redirect_to articles_path, status: :see_other
-  end
-
-  def self.search(query)
-    where("title LIKE :query OR content LIKE :query", query: "%#{query}%")
-  end
-
 end
