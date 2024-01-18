8c850508977b44af58b5d6ef341052918cae1f98
Jas Dhaul <jasdhaul0@gmail.com>
2024-01-18 04:58:50 -0800

    Implemented article search feature.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,12 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true, length: { minimum: 5 }
+
+  def self.search(query)
+    if query.present?
+      where("title LIKE :query OR content LIKE :query OR author LIKE :query", query: "%#{query}%")
+    else
+      all
+    end
+  end
 end
