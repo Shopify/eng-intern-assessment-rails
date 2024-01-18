0afc75d76df35ab4a45c7cca4273f3e9e31122fd
Moidhas <moidk3026@gmail.com>
2024-01-18 14:46:56 -0500

    Added Article#search

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,9 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
+
+  def self.search(key)
+    sanitized_key = sanitize_sql_like(key)
+    where("title LIKE ? OR content LIKE ?", "%#{sanitized_key}%",  "%#{sanitized_key}%")
+  end
 end
