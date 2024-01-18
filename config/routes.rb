4a529f58ba3631600ce2fd10dac76799e8de142d
Jas Dhaul <jasdhaul0@gmail.com>
2024-01-18 04:08:50 -0800

    Added functionality to create articles. Also Improved route linking

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
   
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles
 end
