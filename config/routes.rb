825b197479ddccec4ecf452b768adb978f55ff30
michaelzixizhou <106829824+michaelzixizhou@users.noreply.github.com>
2024-01-18 15:43:29 -0500

    Removed article fixture for testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root "articles#index"
+  root 'articles#index'
 
-  resources :articles do 
-    collection do 
+  resources :articles do
+    collection do
       get 'search', to: 'articles#search'
     end
   end
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
