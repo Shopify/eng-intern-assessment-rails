831136b365bcad84df10f1bb5ad40113df4c206b
Moidhas <moidk3026@gmail.com>
2024-01-18 16:01:00 -0500

    Added articles#show and articles#create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,22 +3,33 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  def show
-  end
-
   def new
     @article = Article.new
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def create
+    @article = Article.new(allowed_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
-  def update
+  def destroy
   end
 
-  def destroy
+  private
+
+  def allowed_params
+    params.require(:article).permit(:title, :content, :author, :date)
   end
 end
