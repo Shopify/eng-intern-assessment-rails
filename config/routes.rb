0135eaf6b358f65d455f0941b5c460d8a6cc92f4
michaelzixizhou <106829824+michaelzixizhou@users.noreply.github.com>
2024-01-18 15:51:57 -0500

    Refactored URL for index view

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
       get 'search', to: 'articles#search'
     end
   end
+
+  get 'articles', to: 'articles#index', as: 'articles_index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
