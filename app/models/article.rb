3fe360a51515ff04b3cd35ba672ff346a89c3088
Ben Sun <sunb26@mcmaster.ca>
2024-01-17 20:15:59 -0500

    added articles model * four fields, title, content, author, date

---
+++ app/models/article.rb
@@ -0,0 +1,17 @@
+class Article < ApplicationRecord
+  def self.search(search_term)
+    if search_term
+      terms = search_term.split
+
+      query = terms.each_with_index.map { |term, index| 
+        "title LIKE :term#{index} OR content LIKE :term#{index}" } 
+        .join(' OR ')
+
+      term_bindings = terms.each_with_index.to_h { |term, index| 
+        [:"term#{index}", "%#{term}%"]  
+      }
+      
+      where(query, term_bindings)
+    end
+  end
+end
