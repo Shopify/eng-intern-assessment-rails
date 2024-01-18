d5474fce85eed235da09a463b3265668ff0654e9
Moidhas <moidk3026@gmail.com>
2024-01-18 16:23:51 -0500

    Added articles#destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,7 +36,10 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
 
+    redirect_to root_path, status: :see_other
   end
 
   private
