8177ad5b2ef5b4d63bab050eb4248608682a3e70
michaelzixizhou <106829824+michaelzixizhou@users.noreply.github.com>
2024-01-18 17:54:09 -0500

    Added documentation for Article class

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,18 @@
 class Article < ApplicationRecord
   validates :title, :content, presence: true
 
+  # Class Method: Search
+  #
+  # Searches for articles based on the provided query string.
+  #
+  # Parameters:
+  #   - querystr (String): The search query string.
+  #
+  # Returns:
+  #   - ActiveRecord::Relation: A collection of articles matching the search criteria.
+  #
+  # Example:
+  #   Article.search('Rails') #=> Returns articles with titles or content containing 'Rails'.
   def self.search(querystr)
     query = 'LOWER(title) LIKE ? OR LOWER(content) LIKE ?'
 
