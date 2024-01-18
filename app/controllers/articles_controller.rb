a8bc306583d7dd923312cda5619ea60a6b3f956d
Jas Dhaul <jasdhaul0@gmail.com>
2024-01-18 04:42:44 -0800

    Fixed validation bug & edit route typo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.date = Date.current
 
     if @article.save
       redirect_to @article
@@ -28,7 +29,7 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     
-    if article.update(article_params)
+    if @article.update(article_params)
       redirect_to @article
     else
       render :edit, status: :unprocessable_entity
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,4 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true, length: { minimum: 5 }
-  validates :author, presence: true, length: { minimum: 1 }
 end
