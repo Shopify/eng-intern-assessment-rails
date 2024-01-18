32692271ef2e34a34e10fa3c4fb995961f22e5e2
Rishi Ravikumar <rishiravi.k98@gmail.com>
2024-01-18 14:06:07 -0500

    docs: add documentation to the article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ApplicationRecord
     validates :title, presence: true
     validates :content, presence: true, length: { minimum: 10 }
 
+    # search function to find articles based on the title and content 
     def self.search(search_query)
         if search_query.present?
           where("lower(title) LIKE ? OR lower(content) LIKE ? ", "%#{search_query}%", "%#{search_query}%")
