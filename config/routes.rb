6c1bb481f677dec118c73f96a1ffb7b823777a6d
michaelzixizhou <106829824+michaelzixizhou@users.noreply.github.com>
2024-01-18 17:56:21 -0500

    Added documentation for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   root 'articles#index'
 
+  # Resourceful routes for articles with an additional search route.
   resources :articles do
     collection do
       get 'search', to: 'articles#search'
     end
   end
 
+  # Additional route to alias /articles to the articles index for convenience.
   get 'articles', to: 'articles#index', as: 'articles_index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
