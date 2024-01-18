e6706c6019e6276715698424db7324b5af15b019
michaelzixizhou <106829824+michaelzixizhou@users.noreply.github.com>
2024-01-18 09:30:07 -0500

    Implemented article CRUD controller methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,24 +1,55 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
 
   def show
+    set_article
+  end
 
-  end 
+  def new
+    @article = Article.new
+  end
 
   def create
+    @article = Article.new(article_params)
 
-  end 
+    if @article.save
+      redirect_to @article
+    else
+
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   def edit
-
-  end 
+    @article = Article.find(params[:id])
+  end
 
   def update
+    set_article
 
-  end 
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
   def destroy
+    set_article
+    @article.destroy
 
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content, :author, :date)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
