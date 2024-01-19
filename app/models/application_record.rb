137a6e27fc0fe0d1b2e4eefb2d3f4a2eba46ea64
Michael Li <michael.li.web@gmail.co>
2024-01-18 19:35:14 -0500

    Get project up and running
    Day 1 hour 0 of the take home. I roughly understand the overall structure of how things interact with each other. I have a server up and running and I understand how to display content. Next step is to figure out how this Article class should work in order for the unit tests to pass

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 end
+
+class Article < ApplicationRecord
+  before_create :set_default_values
+
+  private
+
+  def set_default_values 
+    self.count = 0
+  end
+end
