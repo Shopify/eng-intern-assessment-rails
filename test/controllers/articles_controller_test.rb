9861be330673324bd7387f0f491ccf4e0cd9dc0f
Kenneth Xing <kenneth.xing@alumni.ubc.ca>
2024-01-19 00:04:33 -0800

    Added tests for controller

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,8 +1,48 @@
 require "test_helper"
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @article = Article.create(title: "FIrst Article", content: "This is a sentence about stuff.")
+  end
+
   test "should get index" do
-    get articles_path
+    get articles_url
     assert_response :success
+  end
+
+  test "should get new" do
+    get new_article_url
+    assert_response :success
+  end
+
+  test "should create article" do
+    assert_difference("Article.count") do
+      post articles_url, params: { article: { title: "Article Title", content: "This is a sentence about stuff." } }
+    end
+
+    assert_redirected_to article_url(Article.last)
+  end
+
+  test "should show article" do
+    get article_url(@article)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_article_url(@article)
+    assert_response :success
+  end
+
+  test "should update article" do
+    patch article_url(@article), params: { article: { title: "Article Title", content: "This is a sentence about stuff." } }
+    assert_redirected_to article_url(@article)
+  end
+
+  test "should destroy article" do
+    assert_difference("Article.count", -1) do
+      delete article_url(@article)
+    end
+
+    assert_redirected_to root_path
   end
 end
