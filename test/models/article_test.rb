7c8707049e8eaae6554e45b5d5ddee7ebf523912
Ben Sun <sunb26@mcmaster.ca>
2024-01-18 23:57:29 -0500

    add new model unit tests

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -65,4 +65,17 @@ class ArticleTest < ActiveSupport::TestCase
     assert_includes results, article2
     assert_not_includes results, article1
   end
+  
+  test 'returns no results for irrelevant search terms' do
+    article1 = Article.create(title: 'Sample Article', content: 'Lorem ipsum dolor sit amet.', author: 'John Doe', date: Date.today)
+    article2 = Article.create(title: 'Another Article', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', author: 'John Doe', date: Date.today)
+    results = Article.search('foobar')
+    assert_empty results
+  end
+
+  test 'returns error for invalid request fields' do
+    article1 = Article.create(title: 'Sample Article', content: 'Lorem ipsum dolor sit amet.', author: 'John Doe', date: "not a date")
+    assert_not article1.valid?, "Article should be invalid"
+    assert_includes article1.errors[:date], 'is not a valid date', "Error message for invalid date is not included"
+  end
 end
