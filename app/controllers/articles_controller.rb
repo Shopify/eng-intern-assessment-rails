4c7b286fcc2e386a8f2dcf262ab4df965cc50caf
Michael Li <michael.li.web@gmail.co>
2024-01-18 22:07:41 -0500

    CR of CRUD implemented
    conceptually, not super difficult to follow along. Probably need to find some way to spice my project up though seeing how most of the submissions will probably be heavily influenced by the ruby on rails guide (like me).

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,30 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    # on successful instantiation, redirect to the article's respective page
+    if @article.save
+      redirect_to @article # redirect > render since we don't want browser refreshes to make the same call
+    # otherwise, display an error status code
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    # prevent bad actors, specifies what values are allowed in an Article upon creation
+    def article_params
+      params.require(:article).permit(:title, :content)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "articles#index"
 
-  # ok the plan is to have like various buttons that redirect elsewhere
-  # and then in those homepages, go test the views
-  get "/articles", to: "articles#index"
+  # instead of manually specifying all the routes for a resource,
+  # we can instead use `resources` which'll map all the routes
+  resources :articles
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
