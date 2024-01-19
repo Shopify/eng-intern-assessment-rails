ae85402e31344d62aad0a69d46513e4ae9a75eb6
Michael Li <michael.li.web@gmail.co>
2024-01-19 00:18:47 -0500

    Visually display fields that are mandatory
    Improved UI by adding a parameter where you can mention if a field is mandatory or not. If so, the partial form will append the asterisk alongside the name. Validation is still done through the models though

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
-  validates :title, presence: true, length: { minimum: 1 }
+  validates :title, presence: true, length: { minimum: 2 }
+  # validates :content, presence: true, length: { minimum: 2 }
 
   def self.search(query)
     if query.present?
