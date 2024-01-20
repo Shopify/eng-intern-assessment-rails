f7f49455f3129f536ac41aff9f4fc273a70855bc
Mary McDonald <mcdonald.e.mary@gmail.com>
2024-01-20 16:01:08 -0500

    Adding delete functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -37,18 +37,19 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+  
   private
     def article_params
       params.require(:article).permit(:title, :content, :author, :date)
     end
 
   # def search
-
-  # end
-
-
-
-  # def destroy
 
   # end
 end
