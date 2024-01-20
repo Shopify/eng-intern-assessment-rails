2407c389a6b486989a1384ee6045d1c4b684bd58
Marwan Zaarab <marwan.zaarab@gmail.com>
2024-01-20 14:18:41 -0600

    Implement search functionality for articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,17 @@
 
 class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 3, maximum: 100 }, uniqueness: true
+
+  class << self
+    def search(search_term)
+      if search_term.present?
+        where(
+          "title LIKE :query OR content LIKE :query OR date::text LIKE :query OR author LIKE :query",
+          query: "%#{search_term}%",
+        )
+      else
+        all
+      end
+    end
+  end
 end
