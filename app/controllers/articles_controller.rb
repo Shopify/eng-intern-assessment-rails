1a8af1c4f3d3a5aa13da952a403aa3f4d61f0a28
Patchy <usmanov@ualberta.ca>
2024-01-20 16:30:28 -0700

    Added search functionality, though full-text search would be more optimal

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: %i[ show edit update destroy ]
 
   def index
-    @articles = Article.all
+    @articles = Article.search(params[:query])
 
     respond_to do |format|
       format.html
