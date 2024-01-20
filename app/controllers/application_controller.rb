d4c03dce78310d8d7699536fdd6d423d153e940b
Mary McDonald <mcdonald.e.mary@gmail.com>
2024-01-20 13:31:38 -0500

    Adding a form to capture article creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render file: "public/422.html", layout: false
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :content)
+    end
+
+  def search
+
+  end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,38 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+    
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render file: "public/422.html", layout: false
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :content, :author, :date)
+    end
+
+  def search
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
