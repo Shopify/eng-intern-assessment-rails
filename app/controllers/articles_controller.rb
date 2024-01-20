068c339d46d47a7ac5f99866dc7cf485ddc998a5
Patchy <usmanov@ualberta.ca>
2024-01-20 10:37:30 -0700

    Created rudimentary views

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,19 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  # GET /articles/1 or /articles/1.json
+  def show
+  end
+
+  # GET /articles/new
+  def new
+    @article = Article.new
+  end
+
+  # GET /articles/1/edit
+  def edit
+  end
+  
+end
