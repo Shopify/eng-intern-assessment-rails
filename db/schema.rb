2342dfd4f6c6ac48938c548476c29545d1c98154
Kenneth Xing <kenneth.xing@alumni.ubc.ca>
2024-01-19 19:00:26 -0800

    Updated date showing
    Tried to develop a more elegant form of date, by turning it into a
    virtual variable that references updated_at. I did this by adding
    
    def date
      updated_at
    end
    
    to app/models/article.rb
    
    However, this did not comply with the vision of the tests -- date is an
    actual field that can be modified.
    
    So I went back and just edited the show.html.erb file to display
    udpated_at if date is not set.

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_14_175031) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_20_025719) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.string "author"
-    t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "date"
   end
 
 end
