9a47404cef88ab640c61dab06c7ce20589bacdbf
Marwan Zaarab <marwan.zaarab@gmail.com>
2024-01-20 20:36:28 -0600

    Implement stimulus search controller for dynamic search results

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -2,7 +2,29 @@ import { Controller } from '@hotwired/stimulus';
 
 // Connects to data-controller="search"
 export default class extends Controller {
+  static targets = ['title', 'content', 'author'];
+
   connect() {
     console.log('Hello from Stimulus Search Controller', this.connection);
+  }
+
+  filterArticles(event) {
+    const searchTerm = event.target.value.toLowerCase();
+
+    this.titleTargets.forEach(title => {
+      const articleTitle = title.textContent.toLowerCase();
+      const articleContent = title.nextElementSibling.textContent.toLowerCase();
+      const articleAuthor = title.nextElementSibling.nextElementSibling.textContent.toLowerCase();
+
+      if (
+        articleTitle.includes(searchTerm) ||
+        articleContent.includes(searchTerm) ||
+        articleAuthor.includes(searchTerm)
+      ) {
+        title.parentElement.style.display = '';
+      } else {
+        title.parentElement.style.display = 'none';
+      }
+    });
   }
 }
