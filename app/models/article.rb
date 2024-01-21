35cfbdd2acb116806726539840e6a7ecec91aca8
kaviraj <kavirajgaire@gmail.com>
2024-01-21 14:47:46 -0500

    implemented search method

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,9 @@
 class Article < ApplicationRecord
-
+  def self.search(search_term)
+    if search_term
+      where("lower(title) LIKE ? OR lower(content) LIKE ?", "%#{search_term.downcase}%", "%#{search_term.downcase}%")
+    else
+      all
+    end
+  end
 end
