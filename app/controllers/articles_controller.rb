fe3ac89a569ceb0ca1f7ecc880e6685d480ed391
DarrenLo0530 <darren.lo.0530@gmail.com>
2024-01-20 20:22:20 -0500

    Add ability to view individual posts

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,15 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show]
+
   def index
     @articles = Article.all
+  end
+
+  def show
+  end
+
+  private
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,10 +1,23 @@
 require "test_helper"
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  # Index tests
+  # :index
   test 'gets all articles' do
-    get :index
+    get articles_url
     assert_response :success
     assert_not_nil assigns(:articles)
+  end
+
+  # :show
+  test "throws 404 when article doesn't exist" do
+    get article_url(13)
+    assert_response :not_found
+  end
+
+  test "gets correct article" do
+    get article_url(1)
+    assert_response :success
+    assert_not_nil assigns(:article)
+    assert_equal assigns(:article), articles(:one)
   end
 end
