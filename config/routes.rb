bf3f9d1bfdfcc6ce3b0b304f46b257544251851f
Aadi Sanghani <aadisanghani04@gmail.com>
2024-01-20 21:07:26 -0500

    Downloaded dependencies and created article model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+  get 'articles/show'
+  get 'articles/create'
+  get 'articles/update'
+  get 'articles/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
