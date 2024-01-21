6bc57706166debdf4fa201bfd11bc39ce95db318
Aadi Sanghani <aadisanghani04@gmail.com>
2024-01-20 22:03:54 -0500

    Commented the codebase

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,15 @@
 class ArticlesController < ApplicationController
+
+  # GET /articles
+  # Returns a list of all articles.
   def index
     articles = Article.all
     render json: articles, status: :ok
   end
 
+  # GET /articles/:id
+  # Returns a single article identified by its ID.
+  # If the article is not found, returns a 404 Not Found response.
   def show
     article = Article.find(params[:id])
     render json: article, status: :ok
@@ -11,24 +17,38 @@ class ArticlesController < ApplicationController
     render json: { error: "Article not found" }, status: :not_found
   end
 
+  # POST /articles
+  # Creates a new article with the provided parameters.
+  # If creation is successful, returns the created article and a 201 Created status.
+  # If creation fails, returns the error message and a 422 Unprocessable Entity status.
   def create
     article = Article.new(article_params)
     if article.save
       render json: article, status: :created
     else
-      render json: { errors: article.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: "There was an error creating the article" }, status: :unprocessable_entity
     end
   end
 
+  # PATCH/PUT /articles/:id
+  # Updates an existing article identified by its ID.
+  # Returns the updated article if successful or an error message if the update fails.
+  # If the article is not found, returns a 404 Not Found response.
   def update
     article = Article.find(params[:id])
     if article.update(article_params)
       render json: article, status: :ok
     else
-      render json: {error: "Article not found"}, status: :not_found
+      render json: { errors: article.errors.full_messages }, status: :unprocessable_entity
     end
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Article not found" }, status: :not_found
   end
 
+  # DELETE /articles/:id
+  # Deletes an article identified by its ID.
+  # Returns a success message if deleted.
+  # If the article is not found, returns a 404 Not Found response.
   def destroy
     article = Article.find(params[:id])
     if article.destroy
@@ -40,6 +60,10 @@ class ArticlesController < ApplicationController
     render json: { error: "Article not found" }, status: :not_found
   end
 
+  # GET /articles/search
+  # Searches for articles based on a query parameter.
+  # Returns a list of articles matching the query.
+  # If no query is provided, returns a 400 Bad Request response.
   def search
     if params[:query].present?
       articles = Article.search(params[:query])
@@ -49,6 +73,10 @@ class ArticlesController < ApplicationController
     end
   end
 
+  # GET /articles/title
+  # Searches for articles based on a title query parameter.
+  # Returns a list of articles where the title matches the query.
+  # If no query is provided, returns a 400 Bad Request response.
   def search_by_title
     if params[:query].present?
       articles = Article.title(params[:query])
@@ -58,6 +86,10 @@ class ArticlesController < ApplicationController
     end
   end
 
+  # GET /articles/author
+  # Searches for articles based on an author query parameter.
+  # Returns a list of articles where the author matches the query.
+  # If no query is provided, returns a 400 Bad Request response.
   def search_by_author
     if params[:query].present?
       articles = Article.author(params[:query])
@@ -67,6 +99,7 @@ class ArticlesController < ApplicationController
     end
   end
 
+  # method to whitelist parameters allowed for article creation and updating.
   private
   def article_params
     params.require(:article).permit(:title, :content, :author)
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
+
+  # Searches for articles based on a provided search argument.
+  # The method looks for matches in both the title and content of the articles.
+  # Returns nil if no search argument is provided.
   def self.search(search_arg)
     if search_arg
       where('title LIKE ? OR content LIKE ?', "%#{search_arg}%", "%#{search_arg}%")
@@ -7,6 +11,9 @@ class Article < ApplicationRecord
     end
   end
 
+  # Searches for articles based on a provided search argument.
+  # The method looks for matches in the title of the articles.
+  # Returns nil if no search argument is provided.
   def self.title(search_arg)
     if search_arg
       where('title LIKE ?', "%#{search_arg}%")
@@ -15,6 +22,9 @@ class Article < ApplicationRecord
     end
   end
 
+  # Searches for articles based on a provided search argument.
+  # The method looks for matches in the author of the articles.
+  # Returns nil if no search argument is provided.
   def self.author(search_arg)
     if search_arg
       where('author LIKE ?', "%#{search_arg}%")
