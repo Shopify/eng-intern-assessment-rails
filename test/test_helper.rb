7331b3252910e2e697233212b1ba08305e386658
Mary McDonald <mcdonald.e.mary@gmail.com>
2024-01-20 21:30:35 -0500

    Commenting out articles.yml to not affect test file

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,7 +8,7 @@ module ActiveSupport
     parallelize(workers: :number_of_processors)
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    # fixtures :all
+    fixtures :all
 
     # Add more helper methods to be used by all tests here...
   end
