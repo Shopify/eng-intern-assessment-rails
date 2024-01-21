b10d3cc90f2687423c73994950f9c35f5ba6b285
DarrenLo0530 <darren.lo.0530@gmail.com>
2024-01-20 20:55:57 -0500

    Added searching

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,12 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:show]
 
   def index
-    @articles = Article.all
+    # Allow searching 
+    if params[:search]
+      @articles = Article.search(params[:search])
+    else
+      @articles = Article.all
+    end
   end
 
   def show
--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -8,6 +8,20 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     assert_not_nil assigns(:articles)
   end
 
+  test 'search finds articles' do
+    get articles_url, params: { search: 'one' }
+    assert_response :success
+    assert_not_nil assigns(:articles)
+    assert_equal assigns(:articles).length, 1
+  end
+
+  test 'search doesnt find articles' do
+    get articles_url, params: { search: 'none' }
+    assert_response :success
+    assert_not_nil assigns(:articles)
+    assert_equal assigns(:articles).length, 0
+  end
+
   # :show
   test "throws 404 when article doesn't exist" do
     get article_url(13)
