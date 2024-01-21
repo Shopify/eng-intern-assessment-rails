d271bfe16c7122415011c650a1a8a6f5e45db9b2
Patchy <usmanov@ualberta.ca>
2024-01-20 20:08:49 -0700

    Added some documentation in the form of comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,8 +2,11 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: %i[ show edit update destroy ]
 
   def index
+    # Search returns all items if there is a blank or no query parameter
+    #   or returns the matched results based on the title and content
     @articles = Article.search(params[:query])
 
+    # Checks whether a json or html response is required for CURLs and Views
     respond_to do |format|
       format.html
       format.json { render json: @articles, status: :ok }
@@ -30,7 +33,10 @@ class ArticlesController < ApplicationController
   # POST /articles or /articles.json
   def create
     @article = Article.new(article_params)
+
+    # Checks whether a json or html response is required for CURLs and Views
     respond_to do |format|
+      # Checks if the article was saved properly to the database
       if @article.save
         format.html { redirect_to article_url(@article), notice: "Article was successfully created." }
         format.json { render json: @article, status: :created}
@@ -44,6 +50,7 @@ class ArticlesController < ApplicationController
   # PATCH/PUT /articles/<:id> or /articles/<:id>.json
   def update
     respond_to do |format|
+      # Checks if the article was successfully updated
       if @article.update(article_params)
         format.html { redirect_to article_url(@article), notice: "Article was successfully updated." }
         format.json { render json: @article, status: :ok }
@@ -57,6 +64,7 @@ class ArticlesController < ApplicationController
   # DELETE /articles/<:id> or /articles/<:id>.json
   def destroy
     respond_to do |format|
+      # Checks if the Article was successfully destroyed
       if @article.destroy
         format.html { redirect_to articles_url, notice: "Article was successfully destroyed." }
         format.json { render json: { message: "Article was successfully destroyed." }, status: :ok }
@@ -68,19 +76,20 @@ class ArticlesController < ApplicationController
   end
 
   private
+
     # Use callbacks to share a common setup
     def set_article
       @article = Article.find(params[:id])
     rescue ActiveRecord::RecordNotFound
+      # Checks whether a json or html response is required for CURLs and Views
       respond_to do |format|
         format.html { redirect_to articles_url }
         format.json { render json: { error: 'Invalid article ID provided' }, status: :not_found }
       end
     end
 
-    # Only allow a list of trusted parameters through.
+    # Only allow a list of trusted parameters through
     def article_params
       params.require(:article).permit(:title, :content, :author, :date)
     end
-  
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,13 @@
 class Article < ApplicationRecord
-  validates :title, presence: true
-  validates :content, presence: true
+  # Validates the fields to ensure valid inputs
+  validates :title, presence: true, blank: false
+  validates :content, presence: true, blank: false
 
+  # Performs query searches using a query term against the title and content
   def self.search(term)
+    # Checks if the query term is present, or returns all articles if not
     if term.present?
+      # OPTIMIZE: Full-text search would be more performant for large tables
       where('title LIKE ? OR content LIKE ?', "%#{term}%", "%#{term}%")
     else
       all
