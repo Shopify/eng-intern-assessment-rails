29f42a56a8d81cd26f6d3e3381b5afc6a98cf70d
James-T-Ardian <jamesardian01@gmail.com>
2024-01-21 00:18:51 -0800

    add Create functionality for Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,22 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to "/articles"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :content, :author, :date)
+    end
 end
