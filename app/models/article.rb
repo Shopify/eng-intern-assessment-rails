199f1496db77a59353f71530142e7e6dd499a621
Alex Souphanthong <alexsoup8@gmail.com>
2024-01-21 11:07:17 -0500

    added article controller endpoints and search for article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,15 @@
 class Article < ApplicationRecord 
+
+    validates :title, presence:true
+    validates :content, presence:true
+    
+    def self.search(query)
+        sanitized_query = sanitize_sql_like(query)
+        where("title LIKE ? or content LIKE ? or author LIKE ?",
+            "%#{sanitized_query}%",
+            "%#{sanitized_query}%",
+            "%#{sanitized_query}%"
+        )
+
+    end
 end
