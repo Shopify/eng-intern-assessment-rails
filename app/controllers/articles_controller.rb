a0d2f762106a127652b703c5cd0fb1e649c1d23c
Muhammad Ali <mali42@uoguelph.ca>
2024-01-20 21:40:10 -0500

    updated

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,10 +2,6 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: %i[ show edit update destroy ]
 
   # # GET /articles or /articles.json
-  # def index
-  #   @articles = Article.all
-  # end
-
   def index
     if params[:search].present?
       @articles = Article.where("title LIKE ?", "%#{params[:search]}%")
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,7 +11,6 @@ module ActiveSupport
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
-    self.use_transactional_tests = true
 
   end
   
