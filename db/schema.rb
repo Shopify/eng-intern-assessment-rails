5563d62dd7d963e53ec80a1a2fe81a1669466187
rabiut <90636484+rabiut@users.noreply.github.com>
2024-01-20 19:04:05 -0500

    Implemented article model and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_20_203040) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_20_232532) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
+    t.date "date"
   end
 
 end
