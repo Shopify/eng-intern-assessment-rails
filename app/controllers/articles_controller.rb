c5ba9d1267e5ac62ebb2f5a6eee15d893d1b277f
Raymond Ma <60418739+ma-ray@users.noreply.github.com>
2024-01-21 22:29:08 -0500

    Create "New Article" view.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,32 @@
 class ArticlesController < ApplicationController
-  # GET /articles
+  # GET /articles (index page)
   def index
     @articles = Article.all
   end
 
-  # GET /articles/:id
+  # GET /articles/:id (Show article page)
   def show
     @article = Article.find(params[:id])
+  end
+
+  # GET /articles/new (New Article page)
+  def new
+    @article = Article.new
+  end
+
+  # POST /articles
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content, :author, :date)
   end
 end
