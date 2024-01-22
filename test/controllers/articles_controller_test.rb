402652859fa03c331df2729a4b1b98954dfefd59
Zaki Machfj <zakimachfj@gmail.com>
2024-01-22 15:59:22 -0500

    update integration and unit tests for article controller and model

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -17,10 +17,18 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
 
   test "should create article" do
     assert_difference("Article.count") do
-      post articles_url, params: { article: {  } }
+      post articles_url, params: { article: { title: "Sample Article", content: "Sample Content" } }
     end
 
     assert_redirected_to article_url(Article.last)
+  end
+
+  test "should not create article with missing title and content" do
+    assert_no_difference("Article.count") do
+      post articles_url, params: { article: { title: nil, content: nil } }
+    end
+
+    assert_response :unprocessable_entity
   end
 
   test "should show article" do
@@ -34,8 +42,19 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update article" do
-    patch article_url(@article), params: { article: {  } }
-    assert_redirected_to article_url(@article)
+    updated_attributes = { title: 'Updated Title', content: 'Updated content' }
+    patch article_url(@article), params: { article: updated_attributes }
+    assert_redirected_to article_url(@article.reload)
+
+    @article.reload
+    assert_equal updated_attributes[:title], @article.title
+    assert_equal updated_attributes[:content], @article.content
+  end
+
+  test "should not update article with missing and content" do
+    updated_attributes = { title: nil, content: nil }
+    patch article_url(@article), params: { article: updated_attributes }
+    assert_response :unprocessable_entity
   end
 
   test "should destroy article" do
