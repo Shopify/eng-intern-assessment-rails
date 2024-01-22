ba6e5eaa11cd95292219f5a59f8e535089356432
rodrigo-wong <wongmacr@gmail.com>
2024-01-22 02:16:36 -0500

    Added seed to db with two dummy Articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Sample articles data
+sample_articles = [
+  { title: 'Article 1', content: 'Content for article 1.', author: 'Rodrigo', date: Date.today },
+  { title: 'Article 2', content: 'Some other content forr article 2', author: 'David', date: Date.today - 1.day },
+]
+
+# Create or find articles
+sample_articles.each do |article_attrs|
+  Article.find_or_create_by!(title: article_attrs[:title]) do |article|
+    article.content = article_attrs[:content]
+    article.author = article_attrs[:author]
+    article.date = article_attrs[:date]
+  end
+end
