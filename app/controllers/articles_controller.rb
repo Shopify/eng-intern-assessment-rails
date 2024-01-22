df0821afc271e870aeeb6f1ade987cb1f3accf02
IsabellaLCYang <64714074+IsabellaLCYang@users.noreply.github.com>
2024-01-21 21:44:54 -0500

    add update functionality and enhanced to include pop up message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,10 +9,31 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :content, :author, :date))
-    @article.save
+    if @article.save
+      flash[:notice] = "Article was created successfully"
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :content, :author, :date))
+        flash[:notice] = "Article was updated successfully"
+        redirect_to @article
+    else
+        render 'edit'
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :articles, only: [:show, :index, :new, :create]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
 end
