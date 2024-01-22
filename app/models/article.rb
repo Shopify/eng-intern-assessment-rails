e460545222d9a0e17b826aac40f469bdab330d9f
Papershine <hilarylau119@gmail.com>
2024-01-21 22:32:33 -0800

    add search to model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,10 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
+
+  def self.search(text)
+    # lowercase to perform case insensitive search
+    key = "%#{text.downcase}%"
+    self.where("title LIKE ? or content LIKE ?", key, key)
+  end
 end
