5ea609187b2198481fd209b3ff349301dbacac7b
IsabellaLCYang <64714074+IsabellaLCYang@users.noreply.github.com>
2024-01-21 23:50:47 -0500

    Refactor and add comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,12 @@
 class ArticlesController < ApplicationController
+  # perform a find action for a particular article using id before controller actions
+  before_action  :find_article, only: [:show, :edit, :update, :destroy]
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
+    # switch actions based of param
     if params[:clear]
       redirect_to articles_path
     elsif params[:search]
@@ -19,37 +21,52 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
+  # create a new article
+  # if creation was successful, show sucess popup message and redirect to the detailed article page
+  # if not, go to the page with the article creation form and display corresponding errors
   def create
-    @article = Article.new(params.require(:article).permit(:title, :content, :author, :date))
+    @article = Article.new(article_fields)
     if @article.save
-      flash[:notice] = "Article was created successfully"
+      flash[:notice] = "Article " + @article.title + " was created successfully"
       redirect_to @article
     else
       render 'new'
     end
   end
 
+  # update an article
+  # if update was successful, show sucess popup message and redirect to the detailed article page
+  # if not, go to the page with the article edit form and display corresponding errors
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :content, :author, :date))
-        flash[:notice] = "Article was updated successfully"
+    if @article.update(article_fields)
+        flash[:notice] = "Article " + @article.title + " was updated successfully"
         redirect_to @article
     else
         render 'edit'
     end
   end
 
+  # delete an article
+  # if delete was successful, show sucess popup message and redirect to the article listing page
+  # if not, show unsucess pop up message
   def destroy
-    @article = Article.find(params[:id])
     if @article.destroy()
-      flash[:notice] = "Article was deleted successfully"
+      flash[:notice] = "Article " + @article.title + " was deleted successfully"
       redirect_to articles_path
     else
-      flash[:notice] = "Article deletion failed"
+      flash[:notice] = "Article " + @article.title + " deletion failed"
     end
+  end
+
+  private
+  def find_article
+      @article = Article.find(params[:id])
+  end
+
+  def article_fields
+      params.require(:article).permit(:title, :content, :author, :date)
   end
 
 end
