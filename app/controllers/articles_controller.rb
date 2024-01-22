35b3b2c0046f79a4a850b4113621827788dc567a
Raymond Ma <60418739+ma-ray@users.noreply.github.com>
2024-01-21 21:57:30 -0500

    Create Article index and show views.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,11 @@
 class ArticlesController < ApplicationController
+  # GET /articles
   def index
+    @articles = Article.all
+  end
+
+  # GET /articles/:id
+  def show
+    @article = Article.find(params[:id])
   end
 end
