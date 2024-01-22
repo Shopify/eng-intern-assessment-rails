13a89f8886b25a868512375bac25eab48fa0cf02
ryan wong <ryanpersonal18@gmail.com>
2024-01-22 00:26:52 -0700

    New article, error handling

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,39 @@
+class ArticlesController < ApplicationController
+  # Index page
+  def index
+    # Search for article
+    if params[:search]
+      @articles = Article.search(params[:search])
+    else
+      # Display all
+      @articles = Article.all
+    end
+  end
+
+  # New article
+  def new
+    @article = Article.new
+  end
+
+  # Create article & check for empty content(s)
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to action: "index"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # Edit article
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  # Strong param
+  private
+    def article_params
+      params.require(:article).permit(:title, :content, :author, :date)
+    end
+end
