5c0325935473fb99bd38857d784baa0519268aa1
Zaki Machfj <zakimachfj@gmail.com>
2024-01-22 15:44:53 -0500

    add seeding script to generate 20 sample articles with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Article.destroy_all
+
+20.times do |index|
+  Article.create!(
+    title: Faker::Lorem.sentence(word_count: 3),
+    content: Faker::Lorem.paragraph(sentence_count: 2),
+    author: Faker::Name.name,
+    date: Faker::Date.between(from: 2.days.ago, to: Date.today)
+  )
+end
+
+puts "Created #{Article.count} articles."
