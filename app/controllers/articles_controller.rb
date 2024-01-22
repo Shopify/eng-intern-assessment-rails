a141ebcf01d3e2d18bcbe1606f9ba4dc46ffef48
James-T-Ardian <jamesardian01@gmail.com>
2024-01-21 20:58:42 -0800

    Add a way to update articles + styling changes to make UI cleaner

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,9 +11,23 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      redirect_to "/articles"
+      redirect_to articles_path
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to articles_path
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
