939280e7f69c804bc4c546f4704294e8d3d70c72
Smarth Gupta <102758221+straw-hat-gupta@users.noreply.github.com>
2024-01-22 11:32:11 -0800

    added logo and cleaned up code

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,14 +1,15 @@
 class Article < ApplicationRecord
-    def self.search(query)
-        if query.present?
-          where("title LIKE :query OR content LIKE :query OR author LIKE :query", query: "%#{query}%")
-        else
-          all
-        end
-    end
+  # Performs a partial match search across title, content, and author fields.
+  # Returns all articles if no query is specified.
+  def self.search(query)
+      if query.present?
+        where("title LIKE :query OR content LIKE :query OR author LIKE :query", query: "%#{query}%")
+      else
+        all
+      end
+  end
 
-    # validate text fields
-    validates :title, presence: true, uniqueness: true, length: { minimum: 5 }
-
-    validates :content, presence: true, length: { minimum: 10 }
+  # validate text fields for presence, uniqueness, and minimum length
+  validates :title, presence: true, uniqueness: true, length: { minimum: 5 }
+  validates :content, presence: true, length: { minimum: 10 }
 end
