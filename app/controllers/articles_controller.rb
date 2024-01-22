77297937342f0659dab08cdfa4df2a6f102ea16b
Raymond Ma <60418739+ma-ray@users.noreply.github.com>
2024-01-21 23:38:47 -0500

    Add ability to delete articles in show view.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -41,6 +41,14 @@ class ArticlesController < ApplicationController
     end
   end
 
+  # DELETE /articles/:id
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :content, :author, :date)
