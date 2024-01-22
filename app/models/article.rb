60f315702517988240a6b59f80e5569ac0ebc415
Omer Munam <39843323+omer-munam@users.noreply.github.com>
2024-01-22 02:01:25 -0500

    All test cases passed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
-    validates :title, :author, :content, :date, presence: true
+    validates :title, :content, presence: true
 
+    def self.search(search)
+        return Article.where("content LIKE CONCAT('%',?,'%') OR title LIKE CONCAT('%',?,'%')",search,search)
+    end
 end
