1fc729a1f1ec1d5ffdbef12c3b48c444bd371b26
Pumpkinmann <jackyshen1011@gmail.com>
2024-01-22 20:50:51 -0500

    implment search, and validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ApplicationRecord
+    validates :title, presence: true
+    validates :content, presence: true
+    
+    # search the specified article from the database
+    def self.search(query)
+        where("title LIKE ? OR content LIKE ?", "%#{query}%", "%#{query}%")
+    end
 end
