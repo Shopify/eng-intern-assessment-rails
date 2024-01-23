b606e5bab96faf1453ed7cd490b40a9685aa9c85
jbanghw <jbanghw@gmail.com>
2024-01-23 08:02:42 -0500

    Implement required functionalities (CRUD + search). Pass all tests.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+
+  def self.search(search)
+    return Article.where("title LIKE ? OR content LIKE ?", "%#{search}%", "%#{search}%")
+  end
 end
