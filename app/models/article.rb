fbca82847c4c8654bad2fdb45bf975d496d82b97
Kevin He <kevin66he@hotmail.com>
2024-01-23 17:34:41 -0500

    Articles Can be Added in the View

---
+++ app/models/article.rb
@@ -0,0 +1,18 @@
+class Article < ApplicationRecord
+
+  validates :title, presence: true
+  validates :content, presence: true, length: { minimum: 10 }
+  validates :author, presence: true
+  validates :date, presence: true
+  validate :date_must_be_before_specific_date
+
+  private
+
+  def date_must_be_before_specific_date
+    return if date.blank?
+
+    if date > Date.today
+      errors.add(:date, "can't be greater than today, " + Date.today.strftime("%Y-%m-%d"))
+    end
+  end
+end
