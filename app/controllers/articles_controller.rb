d64a7b78cb4c8ba43a8f0518fcd4028f5ef43b53
neemasadry <neemasadry@gmail.com>
2024-01-23 01:23:53 -0500

    Styled flash messages and form errors and extracted each into respective partials under views/shared

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
 
     respond_to do |format|
       if @article.save
-        format.html { redirect_to article_url(@article), notice: "Article was successfully created." }
+        format.html { redirect_to article_url(@article), flash: { success: "Article was successfully created." } }
         format.json { render :show, status: :created, location: @article }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class ArticlesController < ApplicationController
   def update
     respond_to do |format|
       if @article.update(article_params)
-        format.html { redirect_to article_url(@article), notice: "Article was successfully updated." }
+        format.html { redirect_to article_url(@article), flash: { success: "Article was successfully updated." } }
         format.json { render :show, status: :ok, location: @article }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class ArticlesController < ApplicationController
     @article.destroy!
 
     respond_to do |format|
-      format.html { redirect_to articles_url, notice: "Article was successfully destroyed." }
+      format.html { redirect_to articles_url, flash: { success: "Article was successfully destroyed." } }
       format.json { head :no_content }
     end
   end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,17 @@
 class SearchController < ApplicationController
+  before_action :set_query, only: :index
+
   def index
-    @results = Article.search(params[:query])
+    if @query.empty?
+      redirect_to root_path, flash: { warning: "Please enter valid search term(s) in the search field." }
+    else
+      @results = Article.search(@query)
+    end
+  end
+
+  private
+
+  def set_query
+    @query = params[:query]
   end
 end
