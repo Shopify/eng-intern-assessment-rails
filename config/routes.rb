62de66af6aa48bf71e999c618aef7c3166151db7
Jacob Zhou <zhoujaco9220@gmail.com>
2024-01-23 15:15:20 -0500

    Created home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :articles
+
+  get 'home/index'
+  root 'home#index'
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # resources :
