fb97b054ae9f3a5a1f15e782f9381f893e531f4c
Smarth Gupta <102758221+straw-hat-gupta@users.noreply.github.com>
2024-01-22 18:39:17 -0800

    Refactored to adhere to Ruby style guide

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[ show edit update destroy ]
+  before_action :set_article, only: %i[show edit update destroy]
+
   # Retrieves a list of articles, filtered by a search query if provided.
   def index
     @articles = Article.search(params[:search])
@@ -13,7 +14,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      redirect_to @article, notice: "Article was successfully created."
+      redirect_to @article, notice: 'Article was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
@@ -21,7 +22,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      redirect_to @article, notice: "Article was successfully updated."
+      redirect_to @article, notice: 'Article was successfully updated.'
     else
       render :edit, status: :unprocessable_entity
     end
@@ -29,17 +30,18 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    redirect_to articles_url, notice: "Article was successfully destroyed."
+    redirect_to articles_url, notice: 'Article was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_article
-      @article = Article.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def article_params
-      params.require(:article).permit(:title, :content, :author, :date)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def article_params
+    params.require(:article).permit(:title, :content, :author, :date)
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,15 +1,16 @@
 class Article < ApplicationRecord
+  # Validation for text fields
+  validates :title, presence: true, uniqueness: true, length: { minimum: 5 }
+  validates :content, presence: true, length: { minimum: 10 }
+
   # Performs a partial match search across title, content, and author fields.
   # Returns all articles if no query is specified.
   def self.search(query)
-      if query.present?
-        where("title LIKE :query OR content LIKE :query OR author LIKE :query", query: "%#{query}%")
-      else
-        all
-      end
+    if query.present?
+      where('title LIKE :query OR content LIKE :query OR author LIKE :query',
+            query: "%#{query}%")
+    else
+      all
+    end
   end
-
-  # validate text fields for presence, uniqueness, and minimum length
-  validates :title, presence: true, uniqueness: true, length: { minimum: 5 }
-  validates :content, presence: true, length: { minimum: 10 }
 end
