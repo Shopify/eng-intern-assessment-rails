030e74b6e0a79d1b1d1b437462eab29124a57141
SukhmanSra <ssra5@uwo.ca>
2024-01-22 23:39:55 -0500

    added the index view, updated the model's validation, and updated the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'articles' => 'articles#index'
-  get 'articles/:id' => 'articles#show'
+  
+  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
