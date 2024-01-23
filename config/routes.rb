32ffdd7a569cac0f175f3fd1c3095d853b657226
Kevin He <kevin66he@hotmail.com>
2024-01-23 14:35:15 -0500

    Displaying Individual Articles

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root "articles#index"
 
   get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
