10c34dcd8f6dc0a1779e96763e4fff4c0fb323d3
Nicolas-Bolouri <nicolasbolouri@gmail.com>
2024-01-23 22:31:24 +0800

    update controller and implement base views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,50 +1,41 @@
 class ArticlesController < ApplicationController
-    before_action :set_article, only: [:show, :edit, :update, :destroy]
-  
-    def index
-      @articles = params[:search] ? Article.search(params[:search]) : Article.all
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = params[:search] ? Article.search(params[:search]) : Article.all
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to articles_url
+    else
+      @articles = Article.all
+      render :index
     end
-  
-    def show
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to articles_url
+    else
+      @articles = Article.all
+      render :index
     end
-  
-    def new
-      @article = Article.new
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_url
+  end
+
+  private
+    def set_article
+      @article = Article.find(params[:id])
     end
-  
-    def create
-      @article = Article.new(article_params)
-      if @article.save
-        redirect_to @article, notice: 'Article was successfully created.'
-      else
-        render :new, status: :unprocessable_entity
-      end
+
+    def article_params
+      params.require(:article).permit(:title, :content, :author, :date)
     end
-  
-    def edit
-    end
-  
-    def update
-      if @article.update(article_params)
-        redirect_to @article, notice: 'Article was successfully updated.'
-      else
-        render :edit, status: :unprocessable_entity
-      end
-    end
-  
-    def destroy
-      @article.destroy
-      redirect_to articles_url, notice: 'Article was successfully destroyed.'
-    end
-  
-    private
-  
-      def set_article
-        @article = Article.find(params[:id])
-      end
-  
-      def article_params
-        params.require(:article).permit(:title, :content, :author, :date)
-      end
 end
-  
--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -15,45 +15,30 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should show article" do
-    get article_url(@article)
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_article_url
-    assert_response :success
-  end
-
   test "should create article" do
     assert_difference('Article.count', 1) do
       post articles_url, params: { article: { title: 'New Article', content: 'New content', author: 'New Author', date: Date.today } }
     end
-    assert_redirected_to article_path(Article.last)
+    assert_redirected_to articles_url
   end
 
   test "should not create article with invalid params" do
     assert_no_difference('Article.count') do
       post articles_url, params: { article: { content: 'Invalid content' } } 
     end
-    assert_response :unprocessable_entity
-  end
-
-  test "should get edit" do
-    get edit_article_url(@article)
     assert_response :success
   end
 
   test "should update article" do
     patch article_url(@article), params: { article: { title: 'Updated Title' } }
-    assert_redirected_to article_path(@article)
+    assert_redirected_to articles_url
     @article.reload
     assert_equal 'Updated Title', @article.title
   end
 
   test "should not update article with invalid params" do
     patch article_url(@article), params: { article: { title: '' } } 
-    assert_response :unprocessable_entity
+    assert_response :success
   end
 
   test "should destroy article" do
