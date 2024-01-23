3508d8c3a382cf2a39958edb78f6a52e34e9955b
Nicolas-Bolouri <nicolasbolouri@gmail.com>
2024-01-23 22:01:54 +0800

    added controller test

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,7 +1,65 @@
-require "test_helper"
+require 'test_helper'
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @article = Article.create(title: 'Initial Title', content: 'Initial content', author: 'Initial Author', date: Date.today)
+  end
+
+  test "should get index" do
+    get articles_url
+    assert_response :success
+  end
+
+  test "should get index with search" do
+    get articles_url, params: { search: 'some query' }
+    assert_response :success
+  end
+
+  test "should show article" do
+    get article_url(@article)
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_article_url
+    assert_response :success
+  end
+
+  test "should create article" do
+    assert_difference('Article.count', 1) do
+      post articles_url, params: { article: { title: 'New Article', content: 'New content', author: 'New Author', date: Date.today } }
+    end
+    assert_redirected_to article_path(Article.last)
+  end
+
+  test "should not create article with invalid params" do
+    assert_no_difference('Article.count') do
+      post articles_url, params: { article: { content: 'Invalid content' } } 
+    end
+    assert_response :unprocessable_entity
+  end
+
+  test "should get edit" do
+    get edit_article_url(@article)
+    assert_response :success
+  end
+
+  test "should update article" do
+    patch article_url(@article), params: { article: { title: 'Updated Title' } }
+    assert_redirected_to article_path(@article)
+    @article.reload
+    assert_equal 'Updated Title', @article.title
+  end
+
+  test "should not update article with invalid params" do
+    patch article_url(@article), params: { article: { title: '' } } 
+    assert_response :unprocessable_entity
+  end
+
+  test "should destroy article" do
+    assert_difference('Article.count', -1) do
+      delete article_url(@article)
+    end
+    assert_redirected_to articles_url
+  end
 end
