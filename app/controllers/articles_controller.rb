71e6580cdfe7fbed270de846a9b05acdf32404c8
Sujan Sharma <ak@brokenai.ca>
2024-01-23 18:15:52 -0500

    added search functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,7 @@ class ArticlesController < ApplicationController
   # GET /articles or /articles.json
   def index
     @articles = Article.all
+    @articles = Article.search(params[:search])
   end
 
   # GET /articles/1 or /articles/1.json
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ApplicationRecord
+  def self.search(query)
+    if query.present?
+      where('title LIKE :query OR content LIKE :query', query: "%#{query}%")
+    else
+      all
+    end
+  end
 end
