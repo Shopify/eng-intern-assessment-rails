43fee430fa50cd8b6b6c10ea6b9c3f58dc8676f2
Omer Munam <39843323+omer-munam@users.noreply.github.com>
2024-01-23 03:00:36 -0500

    Frontend complete

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,10 @@ class Article < ApplicationRecord
     validates :title, :content, presence: true
 
     def self.search(search)
-        return Article.where("content LIKE CONCAT('%',?,'%') OR title LIKE CONCAT('%',?,'%')",search,search)
+        if search
+            @articles = Article.where("content LIKE CONCAT('%',?,'%') OR title LIKE CONCAT('%',?,'%')",search,search)
+        else 
+            @articles = Article.all
+        end
     end
 end
