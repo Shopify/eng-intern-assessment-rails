0ca88a91ca0421ad5b522e755b43633be94650c7
nizar lahlali <nizar.lahlali.1@ens.etsmtl.ca>
2024-01-22 20:36:07 -0500

    added detele method to Article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,18 @@
 class ArticlesController < ApplicationController
 
-  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :set_article, only: [:show, :edit, :update]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
 
   def new
     @article = Article.new
+    @article.date = Date.today
   end
 
   def create
@@ -14,6 +23,23 @@ class ArticlesController < ApplicationController
     else
       render :new
     end
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to @article, notice: 'Article was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path, notice: 'Article was successfully deleted.'
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,16 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  root 'articles#menu'
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :articles, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
+  resources :articles do
+    collection do
+      get 'search'
+    end
+  end
+  
 end
