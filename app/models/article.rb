b55a661e939187cb3202e1a134de7d27b52b5344
zhongjie-wu <vivekwuca@gmail.com>
2024-01-24 09:14:42 -0500

    validations added for title and date

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,17 +1,25 @@
 class Article < ApplicationRecord
-    # # Validations
-    # validates :title, presence: true
-    # validates :content, presence: true
-    # validates :author, presence: true
-    # # If you want to validate that the date is present and in the past (or today):
-    # validates :date, presence: true, inclusion: { in: (Date.new(2000)..Date.today) }
+    # Validates that the title is present
+    validates :title, presence: true
 
-    # Search: search articles by keywords
+    # Validates that date, if entered, is current or in the past
+    validate :date_must_be_in_the_past_or_today, if: -> { date.present? }
+
+
+    # Search articles by keywords
     def self.search(search_term)
         # SQLite compatible case-insensitive search
         # search range includes title, content, and author
         where("LOWER(title) LIKE ? OR LOWER(content) LIKE ? OR LOWER(author) LIKE ?", 
               "%#{search_term.downcase}%", "%#{search_term.downcase}%", "%#{search_term.downcase}%")
+    end
+
+    private
+
+    def date_must_be_in_the_past_or_today
+        if date > Date.today
+        errors.add(:date, "can't be in the future")
+        end
     end
       
 end
