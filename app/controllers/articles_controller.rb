469078ee2338965dbcaa0f9afa8573551990ba2d
orangeepop <112613050+orangeepop@users.noreply.github.com>
2024-01-24 00:12:35 -0800

    Add code documentation and css stylesheet

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,25 @@
 class ArticlesController < ApplicationController
+  # index page shows all articles
   def index
     @articles = Article.all
   end
 
+  # search page shows all articles that match query
   def search
-    # @article = Article.where("? LIKE CONCAT('%', title, '%')", params[:title])
-    @parameter = params[:search].downcase
-    @articles = Article.all.where("title LIKE :search OR content LIKE :search", search: "%#{@parameter}%")
+    @articles = Article.search(params[:search])
   end
 
+  # shows contents of article by its id
   def show
     @article = Article.find(params[:id])
   end
 
+  # renders view for creating a new article
   def new
     @article = Article.new
   end
 
+  # creates a new article
   def create
     @article = Article.new(article_params)
 
@@ -27,10 +30,12 @@ class ArticlesController < ApplicationController
     end
   end
 
+  # edits an existing article
   def edit
     @article = Article.find(params[:id])
   end
 
+  # updates an existing article
   def update
     @article = Article.find(params[:id])
 
@@ -41,6 +46,7 @@ class ArticlesController < ApplicationController
     end
   end
 
+  # destroys an article
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
@@ -48,6 +54,7 @@ class ArticlesController < ApplicationController
     redirect_to root_path, status: :see_other
   end
 
+  # filters params used to create or update an article
   private
     def article_params
       params.require(:article).permit(:title, :content, :author)
