718b3886f614bb8b890e782ac17c876704004df6
mabrar02 <mahdeen02@gmail.com>
2024-01-24 17:49:23 -0400

    implemented CREATE from CRUD functionalities

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
-
+  validates :title, presence: true
+  validates :content, presence: true, length: {minimum: 10}
+  
   def self.search(search)
     where("title LIKE ? OR content LIKE ?", "%#{search}#%", "%#{search}#%")
   end
