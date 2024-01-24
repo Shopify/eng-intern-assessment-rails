f910177118277cf53ae30ba36f9673a49db50c88
nebrask <nebraskhan12@gmail.com>
2024-01-24 10:15:06 -0500

    NK-02 Implemented search functionality to Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ApplicationRecord
+    validates :title, :content, presence: true
+    validates :author, :date, presence: true, allow_nil: true
+  
+    def self.search(query)
+        where("title LIKE ? OR content LIKE ?", "%#{query}%", "%#{query}%")
+    end
 end
