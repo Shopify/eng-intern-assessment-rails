a844bc6cffac0dc6d5907346655f7c95295d7f79
SukhmanSra <ssra5@uwo.ca>
2024-01-23 20:59:44 -0500

    created all of the CRUD methods in the controller, created views for create and edit along with a modal for reuse

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ApplicationRecord
     validates :title, presence: true
     validates :author, presence: true
     validates :content, presence: true
+
   def self.search(search)
     if search
       @articles = self.where("title LIKE :q or author LIKE :q or content LIKE :q", q: "%#{search}%")
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "articles#index"
   get 'articles' => 'articles#index'
   
   resources :articles
