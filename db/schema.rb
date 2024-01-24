cee6524c8e69f0d92c89235b598497c8bd04c41b
Steven Son <sdson9@gmail.com>
2024-01-24 01:36:59 -0800

    adding date and author column to articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_24_074204) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_24_093519) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
+    t.date "date"
   end
 
 end
