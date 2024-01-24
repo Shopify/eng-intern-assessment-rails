446f4a3880a4c31e7300c766b8a09393eb51a682
Sujan Sharma <ak@brokenai.ca>
2024-01-23 21:02:34 -0500

    user cand search by author

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   def self.search(query)
     if query.present?
-      where('title LIKE :query OR content LIKE :query', query: "%#{query}%")
+      where('title LIKE :query OR content LIKE :query OR author LIKE :query', query: "%#{query}%")
     else
       all
     end
