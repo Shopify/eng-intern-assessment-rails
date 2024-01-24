fd224aaf828751046eaa8846fdc36722fcd88f47
Taha Iftikhar <iftikhta@sheridancollege.ca>
2024-01-24 17:33:26 -0500

    Added show action and view, added h eader partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,16 @@
 class ArticlesController < ApplicationController
+
   def index
     @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  ##Strong parameters
+  private
+    def article_params
+      params.require(:article).permit(:title, :content, :author, :date)
+    end
 end
