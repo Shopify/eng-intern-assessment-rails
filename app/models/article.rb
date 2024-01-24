ae80cf11cf489291d85b2d195f93176bf3b31213
shairyousuf <syjahin@mun.ca>
2024-01-24 07:56:19 -0330

    Created model functionality and added search

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+  validates :title, :content, presence: true
+
+  def self.search(query)
+    where("title LIKE ? OR content LIKE ?", "%#{query}%", "%#{query}%")
+  end
 end
