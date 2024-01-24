18b184ae7df06b563ed7600500ca3af02299092f
nebrask <nebraskhan12@gmail.com>
2024-01-24 11:39:56 -0500

    NK-03 Added CRUD actions to RecordNotFound handling and ArticlesController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  
+    private
+  
+    def record_not_found
+        render plain: "404 Not Found", status: 404
+    end
+    
 end
