e05dbaeac8a02269f30412f4de6b24a5e77478cb
shairyousuf <syjahin@mun.ca>
2024-01-24 07:55:14 -0330

    Added CRUD and search to controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,36 @@
 class ArticlesController < ApplicationController
-  def index
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      render json: @article, status: :created
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      render json: @article
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    head :no_content
+  end
+
+  def search
+    @articles = Article.search(params[:query])
+    render json: @articles
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content, :author, :date)
   end
 end
