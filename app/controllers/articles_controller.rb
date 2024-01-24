3108f7c427a5eb41d563092456faaa791219d060
Steven Son <sdson9@gmail.com>
2024-01-24 01:05:45 -0800

    adding delete action, from index and show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -35,6 +35,13 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
   private
 
   def article_params
