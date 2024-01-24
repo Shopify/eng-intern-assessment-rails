daac622d10a2f52db98f128f451e098269010c2c
Adam Chen <encodeous7@gmail.com>
2024-01-23 19:30:26 -0500

    Create validations for article titles

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,9 @@ class Article < ApplicationRecord
   scope :search, ->(name) {
     where("title like ? or content like ?", "%#{name}%", "%#{name}%")
   }
+  # Ensure title is always present, as we use it for routing
+  validates :title, presence: true
+
+  # Restrict the length of the title to be at least 1 character and at most 255
+  validates :title, length: { maximum: 255, minimum: 1 }
 end
