680570c71be3ff5d900d89d2c657eeed79e3e86d
LIamB12 <liam.beenken@gmail.com>
2024-01-24 20:34:15 -0500

    Added create and update methods to ArticlesController * removed CSRF protection on article endpoints for easier testing and development

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  # allow cross-origin requests to crud endpoints while developing
+  skip_before_action :verify_authenticity_token
 
   def index
     # return success message and all articles if request ok
@@ -14,6 +16,66 @@ class ArticlesController < ApplicationController
     rescue StandardError => e
       return_standard_error(e)
     end
+  end
+
+  def create
+    # filter non-permitted params, attempt to create and save article, return success if valid
+    begin
+      @article = Article.new(article_params)
+      @article.save
+      render json: {
+        status: "success",
+        message: "Article successfully created",
+        data: { article: @article }
+      }, status: :ok
+
+    # if passed attributes outside of permitted ones, return error 422
+    rescue ActiveModel::ForbiddenAttributesError => e
+      render json: {
+        status: "error",
+        error: { code: 422, message: e.message }
+      }, status: :unprocessable_entity
+
+    # otherwise return standard 500 error
+    rescue StandardError => e
+      return_standard_error(e)
+    end
+  end
+
+  def update
+    # find article by id, update params that pass permit check, return success if ok
+    begin
+      @article = Article.find(params[:id])
+      @article.update(article_params)
+      render json: {
+        status: "success",
+        message: "Article successfully updated",
+
+      }, status: :ok
+
+    # if no article has passed id, return a 404 error
+    rescue ActiveRecord::RecordNotFound => e
+      return_404_error(e)
+
+    # otherwise return standard 500 error
+    rescue StandardError => e
+      return_standard_error(e)
+    end
+  end
+
+  # param filtering stops danger from blind passing params to crud methods
+  private
+  def article_params
+    params.permit(:content, :title, :date, :author)
+  end
+
+  # renders basic 404 error json
+  private
+  def return_404_error(e)
+    render json: {
+      status: "error",
+      error: { code: 404, message: e.message }
+    }, status: :not_found
   end
 
   # renders basic 500 error json
