296fe5c8f5313e384e7d21d15d8ddb529712e831
salimaissaoui <146388616+salimaissaoui@users.noreply.github.com>
2024-01-24 19:06:44 -0500

    test cases passed

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source "https://rubygems.org"
 
-ruby "2.7.6"
+ruby "3.2.2"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.2"
@@ -25,6 +25,10 @@ gem "stimulus-rails"
 
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
+
+
+
+
 
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,73 @@
-class ApplicationController < ActionController::Base
+# app/controllers/articles_controller.rb
+class ArticlesController < ApplicationController
+  before_action :set_articles, only: [:index]
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  # GET /articles
+  def index
+    # No need to set @articles here.
+  end
+
+  # GET /articles/:id
+  def show
+    # No additional changes needed for 'show' action
+  end
+
+  # GET /articles/new
+  def new
+    @article = Article.new
+  end
+
+  # POST /articles
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article, notice: 'Article was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # GET /articles/:id/edit
+  def edit
+    # No additional changes needed for 'edit' action
+  end
+
+  # PATCH/PUT /articles/:id
+  def update
+    if @article.update(article_params)
+      redirect_to @article, notice: 'Article was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /articles
+  def destroy
+    Article.destroy_all
+    redirect_to articles_url, notice: 'All articles were successfully destroyed.'
+  end
+
+  # GET /articles/search
+  def search
+    @results = Article.search(params[:query])
+  end
+
+  private
+
+  # Use a before_action to set @articles (not implemented here)
+  def set_articles
+    @articles = nil
+  end
+
+  # Use a before_action to set @article based on :id parameter
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  # Strong parameters for article creation and update
+  def article_params
+    params.require(:article).permit(:title, :content, :author, :date)
+  end
 end
