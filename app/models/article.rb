1db1aca874d48be8536071199a5a8a0ae3762b94
LIamB12 <liam.beenken@gmail.com>
2024-01-24 19:59:34 -0500

    Added search method to Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,11 @@
 class Article < ApplicationRecord
+    def self.search(query)
+
+        # Sanitize search input and use LIKE sql query to search title and content
+        safe_str = sanitize_sql(["title LIKE ? OR content LIKE ?",
+                                "%#{query}%",
+                                "%#{query}%"])
+        # return array of articles which match the search
+        where(safe_str)
+    end
 end
