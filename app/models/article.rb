82cfe9e420da49f584243a03bfbb462e51839f5f
Isaac Thill <ithill22@turing.edu>
2024-01-25 13:59:51 -0700

    Add article model and articles controller with CRUD

---
+++ app/models/article.rb
@@ -0,0 +1,9 @@
+class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+
+  def self.search(query)
+    where('title LIKE ? OR content LIKE ?', "%#{query}%", "%#{query}%")
+  end
+
+end
