7830e2dc95777a5fccee3c4116c00dfb4dec0097
Rodrigo Farinango <rodrigofarii@gmail.com>
2024-01-25 15:50:53 -0600

    feat(search): implement article search functionality
    - Added a search method in the Article model to allow users to search for articles based on title or content.
    - Implemented a search bar in the view, enabling users to input search queries and find relevant articles.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,50 +1,47 @@
 class ArticlesController < ApplicationController
     def index
-        @articles = Article.all
+        if params[:search]
+            @articles = Article.search(params[:search])
+          else
+            @articles = Article.all
+          end
     end
-
+  
     def show
-        @article = Article.find(params[:id])
+      @article = Article.find(params[:id])
     end
-
+  
     def new
-        @article = Article.new
+      @article = Article.new
     end
-
+  
     def create
-        @article = Article.new(article_params)
-
-        if @article.save
+      @article = Article.new(article_params)
+  
+      if @article.save
         redirect_to @article
-        else
+      else
         render :new, status: :unprocessable_entity
-        end
+      end
     end
-
+  
     def edit
-        @article = Article.find(params[:id])
+      @article = Article.find(params[:id])
     end
-
+  
     def update
-        @article = Article.find(params[:id])
-
-        if @article.update(article_params)
+      @article = Article.find(params[:id])
+  
+      if @article.update(article_params)
         redirect_to @article
-        else
+      else
         render :edit, status: :unprocessable_entity
-        end
+      end
     end
-
-    def destroy
-        @article = Article.find(params[:id])
-        @article.destroy
-    
-        redirect_to root_path, status: :see_other
-    end
-    
+  
     private
-        def article_params
+      def article_params
         params.require(:article).permit(:title, :content, :author, :date)
-        end
-end
+      end
+  end
   
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ApplicationRecord
     validates :title, presence: true
     validates :content, presence: true, length: { minimum: 10 }
+
+    def self.search(query)
+        Article.where('title LIKE ?', "%#{query}%").or(Article.where('content LIKE ?', "%#{query}%"))
+    end
 end
-  
