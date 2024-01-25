58dd91d5e5a38bff728bdff2f37f0caddc8774b6
Jason Milad <jasonmilad29@gmail.com>
2024-01-25 11:32:10 -0500

    all except for date and search function implemented

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,54 @@
+class ArticlesController < ApplicationController
+  def index
+
+    #this is to ensure that the articles are ordered from most recent to least recent.
+    @articles = Article.order(created_at: :desc)
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  def destroy
+    @article = Article.find(params[:id])
+
+    @article.destroy
+
+    redirect_to :article_path, status: :see_other
+
+  end
+
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :content, :author, :date)
+    end
+end
