67c4c44cdfc5eba6d69439f732baebb0a28f7405
Taha Iftikhar <iftikhta@sheridancollege.ca>
2024-01-24 19:52:02 -0500

    moved header, added destroy and new,  article helpers added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,27 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    puts "Ran destroy #{params[:id]}"
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
   ##Strong parameters
   private
     def article_params
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,12 @@
 module ArticlesHelper
+
+  ##Since formating style might change between article and future data types I prefer to tie it to this model
+  def published_date(date)
+    date.present? ? date.strftime('%A, %b, %d, %Y') : "No publication date"
+  end
+
+  ##With nulls allowed for author I think this a reasonable way to handle its display
+  def published_author(author)
+    author.present? ? author : "Unknown author"
+  end
 end
