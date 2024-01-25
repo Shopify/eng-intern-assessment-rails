528031de80871de594231d2bf112f27c3b520f86
oshalash38 <omarshalash@live.com>
2024-01-25 15:47:14 -0500

    add views and controllers to be able to interact with API on browser

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,2 @@
 class Article < ApplicationRecord
-  def self.search(query)
-    if query.present?
-      where('title LIKE ? OR content LIKE ?', "%#{query}%", "%#{query}%")
-    else
-      all
-    end
-  end
 end
