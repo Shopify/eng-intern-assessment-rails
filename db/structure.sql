cda696c75745e06b776460fa771eac26dc304e83
Khairo Khatib <khairo.khatib@gmail.com>
2024-01-24 23:49:07 -0500

    feat: create the full text search table migration

--- db/structure.sql
+++ db/structure.sql
@@ -3,6 +3,14 @@ CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMAR
 CREATE TABLE IF NOT EXISTS "articles" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "author" varchar, "title" varchar NOT NULL, "content" text NOT NULL, "date" datetime(6), "created_at" datetime(6) NOT NULL, "updated_at" datetime(6) NOT NULL);
 CREATE TABLE sqlite_sequence(name,seq);
 CREATE UNIQUE INDEX "index_articles_on_title" ON "articles" ("title");
+CREATE VIRTUAL TABLE fts_articles USING fts5(author, title, content, article_id)
+/* fts_articles(author,title,content,article_id) */;
+CREATE TABLE IF NOT EXISTS 'fts_articles_data'(id INTEGER PRIMARY KEY, block BLOB);
+CREATE TABLE IF NOT EXISTS 'fts_articles_idx'(segid, term, pgno, PRIMARY KEY(segid, term)) WITHOUT ROWID;
+CREATE TABLE IF NOT EXISTS 'fts_articles_content'(id INTEGER PRIMARY KEY, c0, c1, c2, c3);
+CREATE TABLE IF NOT EXISTS 'fts_articles_docsize'(id INTEGER PRIMARY KEY, sz BLOB);
+CREATE TABLE IF NOT EXISTS 'fts_articles_config'(k PRIMARY KEY, v) WITHOUT ROWID;
 INSERT INTO "schema_migrations" (version) VALUES
+('20240124031723'),
 ('20240123211321');
 
