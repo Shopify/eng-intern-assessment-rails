412be2303a92af044a424fe4bded2c2d1ffb177e
charleyoshi ʕ •ᴥ•ʔ <92558610+charleyoshi@users.noreply.github.com>
2024-01-25 16:17:28 -0700

    add search function to model

--- db/migrate/20240125211704_create_articles.rb
+++ db/migrate/20240125211704_create_articles.rb
@@ -2,7 +2,7 @@ class CreateArticles < ActiveRecord::Migration[7.1]
   def change
     create_table :articles do |t|
       t.string :title
-      t.string :content
+      t.text :content
       t.string :author
       t.date :date
 
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema[7.1].define(version: 2024_01_25_211704) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.string "content"
+    t.text "content"
     t.string "author"
     t.date "date"
     t.datetime "created_at", null: false
