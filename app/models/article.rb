ce96d6dfd5701a284723535c1d4494a03d1e445e
nebrask <nebraskhan12@gmail.com>
2024-01-24 19:49:58 -0500

    NK-07 Added neccessary comments and cleaned up code

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,10 @@
 class Article < ApplicationRecord
+    # Validates title and content.
     validates :title, :content, presence: true
+    # Optionally validates author and date.
     validates :author, :date, presence: true, allow_nil: true
-  
+    
+    # Search functionality by filtering though articles title and content.
     def self.search(query)
         where("title LIKE ? OR content LIKE ?", "%#{query}%", "%#{query}%")
     end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,11 +11,14 @@ module ActiveSupport
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
+    
+    # Initializes DatabaseCleaner and cleans the database before each test.
     setup do
       DatabaseCleaner.strategy = :transaction
       DatabaseCleaner.clean_with(:truncation)
     end
   
+    # Cleans the database after each test.
     teardown do
       DatabaseCleaner.clean
     end
