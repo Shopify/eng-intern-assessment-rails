5d3a7e636c2dc53c175392f385a6e96a990bbe8c
jeffreychan <kkjeffreychan@gmail.com>
2024-01-26 12:36:41 -0500

    changed class method name from "query" -> "search", also added regex to remove extra spaces

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,9 +2,10 @@ class Article < ApplicationRecord
   validates :title, presence: { message: "Title can not be blank!"}
   validates :content, presence: { message: "Content can not be blank!"}
 
-  def self.query(query)
+  def self.search(query)
     if query.present?
-      where("title LIKE ? OR content LIKE ? OR author LIKE ?", "%#{query}%", "%#{query}%", "%#{query}%")
+      sanitized_query = query.gsub(/\s+/, ' ') # Replace consecutive spaces with a single space
+      where("title LIKE ? OR content LIKE ? OR author LIKE ?", "%#{sanitized_query}%", "%#{sanitized_query}%", "%#{sanitized_query}%")
     else
       none
     end
