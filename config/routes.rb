42e4d2f10ede3919c4fc45a28d22c4f636fd38db
Thi Mai Anh Nguyen <maianh.26111997@gmail.com>
2024-01-26 14:28:07 -0500

    Implemented search funtionality and CRUD actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root "articles#index"
+  # Maps HTTP verbs, Article URLs and Article Controllers
+  resources :articles do
+    # Define a route for search. Since we want to display a list of Article objects, we want to create a collection route
+    collection do
+      get :search
+    end
+  end
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
