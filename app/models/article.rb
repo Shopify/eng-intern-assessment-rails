ccf456633f94adb4f749ad2cfca1b501d4e41a08
Maxim Piorischin <maximpiorischin@Logans-MacBook-Pro.local>
2024-01-26 15:01:46 -0500

    article search, re-creating views

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,9 @@
 class Article < ApplicationRecord
-
+    def self.search(search)
+        if search
+            where(["title LIKE ? OR content LIKE ? or author LIKE ?","%#{search}%", "%#{search}%", "%#{search}%"])
+        else
+            all
+        end
+    end
 end
