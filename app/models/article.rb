63312689425640d655b6603ff596ceeb818488f0
Eric Wu <ericzfwu@gmail.com>
2024-01-26 17:45:42 -0500

    Add guard clause for Article Model against empty queries

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,12 +4,15 @@
 # date:date
 
 # Article model represents an instance of an article
-# Title and content cannot be empty and set maximum lengths for data integrity
 class Article < ApplicationRecord
+  # Title and content cannot be empty and set maximum lengths for data integrity
   validates :title, presence: true, length: { maximum: 100}
   validates :content, presence: true, length: { maximum: 1000 }
 
+  # Search database for titles and content with query
   def self.search(query)
+    return all if query.nil? || query.strip.empty?
+
     sanitized_query = ActiveRecord::Base.sanitize_sql_like(query)
     where('title LIKE :query OR content LIKE :query', query: "%#{sanitized_query}%")
   end
