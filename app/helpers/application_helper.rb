bddb8b922576255408edf898f4f3c62b566bb0dc
sharjilm <sharjilm@users.noreply.github.com>
2024-01-26 14:49:19 -0500

    Improving user interface and user experience of the application using Bootstrap and SCSS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def bootstrap_class_for_flash(flash_type)
+    case flash_type
+      when 'notice' then 'success'
+      when 'alert' then 'danger'
+      else flash_type.to_s
+    end
+  end
 end
