dbba430edefb2276db2e509d2052473a6ecd45f7
httq <h1999ieu@gmail.com>
2024-01-25 23:26:31 -0700

    update controller

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,13 +1,18 @@
 class Article < ApplicationRecord
   before_save :set_date
+
+  # Title and content are needed, author is optional
   validates :title, presence: true
   validates :content, presence: true
 
+  # Search for match in title or content
   def self.search(query)
     where("title LIKE ? OR content LIKE ?", "%#{query}%", "%#{query}%")
   end
 
   private
+
+  # Update the date field when article is created or updated
   def set_date
       self.date = Date.today
   end
