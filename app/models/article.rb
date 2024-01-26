64e4c109c74029e95e999c8a75cc6e337962e475
SukhmanSra <ssra5@uwo.ca>
2024-01-25 20:02:35 -0500

    Created the show/read view along with some finishing touches. Also commented all of the code.

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,7 @@ class Article < ApplicationRecord
     validates :author, presence: true
     validates :content, presence: true
 
+  #@description a function which returns all articles which match the parameters with the table attributes
   def self.search(search)
     if search
       @articles = self.where("title LIKE :q or author LIKE :q or content LIKE :q", q: "%#{search}%")
