21b302796b62bc2cbb33b911729e96b52134d399
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2024-01-26 13:51:08 -0500

    feat: Implement search through author functionality which passes all provided test cases

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
     # Search for articles by title or body or author
     def self.search(search)
         if search
-            where(["title LIKE ? OR content LIKE ?", "%#{search}%", "%#{search}%"])
+            where(["title LIKE ? OR content LIKE ? OR author LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%"])
         else
             all
         end
