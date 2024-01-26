1958b61fb1f6815f9c6913c23399b35bc91081a8
httq <h1999ieu@gmail.com>
2024-01-25 17:42:17 -0700

    added article controller

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,14 @@
 class Article < ApplicationRecord
+  before_save :set_date
+  validates :title, presence: true
+  validates :content, presence: true
+
   def self.search(query)
     where("title LIKE ? OR content LIKE ?", "%#{query}%", "%#{query}%")
+  end
+
+  private
+  def set_date
+      self.date = Date.today
   end
 end
