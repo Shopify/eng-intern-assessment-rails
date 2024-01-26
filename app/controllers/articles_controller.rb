5ac7ac769cd759c1df3ed3240c4815230d36173a
maxpiorischin <maxpiorischin@hotmail.com>
2024-01-26 04:08:18 -0500

    Added index, multiple route functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,44 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def create
+    @article = Article.new(params_article)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+
+  def read 
+    @article = Article.find_by(id: params[:id])
+  end
+
+  def update
+    @article = Article.find_by(id: params[:id])
+    if @article.update(params_article)
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find_by(id: params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  def search
+    @articles = Article.where("title LIKE ?", "%#{params[:q]}%")
+  end
+  
+
+  private
+
+  def params_article
+    params.require(:article).permit(:title, :content, :author, :date)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_26_082026) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.string "author"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
