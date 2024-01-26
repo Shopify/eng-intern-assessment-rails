ad807d7e9a5572a3a540cf6f6d0aca8a1de23bff
Dave Marco <marcovitch.david@gmail.com>
2024-01-26 11:33:53 -0500

    added actual css and cleaned up code

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ApplicationRecord
   def self.search(query)
+    # Filter articles by searching for a query in the title or content.
+    # The `LIKE` operator is used for a case-insensitive partial match.
+    # The query is inserted with percentage signs (%) for wildcard matching.
     where("title LIKE ? OR content LIKE ?", "%#{query}%", "%#{query}%")
   end
 end
