9536c84154c5e47f7de92096b2f164be0c2834b9
kelly1040 <keyin.huang@gmail.com>
2024-01-27 14:19:38 -0500

    Added views for CRUD functions

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,8 +1,7 @@
 require "test_helper"
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get articles_index_url
-    assert_response :success
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
