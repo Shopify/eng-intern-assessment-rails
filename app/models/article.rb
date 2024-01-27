0e704871331a7a50fc4db0125405107fdcb0a4f9
Grace Kan <gracex1@students.cs.ubc.ca>
2024-01-26 20:14:51 -0800

    added edit/update

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
+  validates :author, presence: true
+  validates :date, presence: true
   #add validation for author and date
 end
