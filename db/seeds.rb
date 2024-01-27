90a3ee2f7af46cec93d79405d357f1928a587bfa
jpfiscal <jfiscalini@gmail.com>
2024-01-27 13:14:19 -0500

    create essential models and schema updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,6 @@
 #
 # Example:
 #
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+  ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+    MovieGenre.find_or_create_by!(name: genre_name)
+  end
---
+++ test/models/movie_gentre_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class MovieGentreTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
