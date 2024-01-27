a0c25cd461c8200aff34c51b8eede86a691eea2b
Mackenzie Kudrenecky <mkudrene@student.ubc.ca>
2024-01-26 20:20:11 -0800

    implements edit view, updates show with edit link, edit/update controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,21 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+      @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+
   private
   def article_params 
     params.require(:article).permit(:title, :content, :author, :date)
