71c242c8c5561a1cbe2d2368eb38e8f9fc91c0bc
Mackenzie Kudrenecky <mkudrene@student.ubc.ca>
2024-01-26 19:34:11 -0800

    passes read model and controller tests

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
-class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+class ArticlesControllerTest < ActionDispatch::IntegrationTest  
+  # called before every single test
+  setup do
+    @article = articles(:one)
+  end
+
+  test "should get index" do
+    get articles_url
+    assert_response :success
+  end
+
+  test "should show article" do
+    get article_url(@article)
+    assert_response :success
+  end
 end
