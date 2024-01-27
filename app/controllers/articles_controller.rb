0b7161282c63ea2487e9d1a1a2939188c1ca43b6
Grace Kan <gracex1@students.cs.ubc.ca>
2024-01-27 11:41:31 -0800

    changed show to search and tests passing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,9 +3,14 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  def show
-    @article = Article.find(params[:id]);
-  end
+  def search
+
+      if params[:query].present?
+        @articles = Article.search(params[:query])
+      else
+        @articles = Article.all
+      end
+  end 
 
   def new
     @article = Article.new
