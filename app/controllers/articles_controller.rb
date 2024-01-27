0c044882084dfc2eca18407b8923104e4b1792ae
harleenm <harleenmehta02@gmail.com>
2024-01-27 03:01:05 -0500

    Add search

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,10 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def search
+    @articles = Article.search(params[:search])
+  end
+
   def show
     @article = Article.find(params[:id])
   end
