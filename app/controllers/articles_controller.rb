0baa57e3f302de979501cc7031f3e63075795cce
Gavin George <67127770+legofanclub@users.noreply.github.com>
2024-01-27 11:05:21 -0500

    add destroy action to article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,12 @@
 class ArticlesController < ApplicationController
   def index
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
 end
