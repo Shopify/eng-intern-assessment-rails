b3c72636af67a5221b9211c520eb0fa5b6e112f0
Khoj Pez <khoj2407@gmail.com>
2024-01-26 17:59:50 -0800

    Added Create/Update functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,17 +8,38 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
-end
+    validates :title, presence: true
+    validates :content, presence: true, length: { minimum: 10 }
+  end  
