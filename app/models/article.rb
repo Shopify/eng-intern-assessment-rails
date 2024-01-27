c98517f69610f1f11cb42fc5da6bcb279079bbe5
Susie <sliu@Richards-Mac-Studio.local>
2024-01-26 18:19:48 -0600

    Updating the search function so that it correctly sanitizes the query and adds a wildcard character to both the start and end of the query. Previously only added the wildcard to the end of the query

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,20 @@
 class Article < ApplicationRecord
- # Your code here
+ before_validation :set_date, on: create
+
+ # We want to validate that articles have all fields completed before saving them to ensure the traceability of articles within our Encyclopedia
+ validates :title, presence: true
+ validates :content, presence: true
+ validates :date, presence:true
+ validates :author, presence:true
+ 
  def self.search(query)
-    where("title LIKE :query OR content LIKE :query", query: sanitize_sql_like(query) + "%")
+    sanitized_query = sanitize_sql_like(query)
+    where("title LIKE :query OR content LIKE :query", query: "%#{sanitized_query}%")
+ end
+
+ private
+ 
+ def set_date
+    self.date ||= Date.current
  end
 end
