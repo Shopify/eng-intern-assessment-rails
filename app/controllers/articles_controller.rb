475b2e178c48285daea6822acfd77dd46cd07b93
Josh Hutchison <joshhutchison@hotmail.com>
2024-01-27 14:23:33 -0500

    add 2 tests, added comments, cleaned up html and spacing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  # Display a list of articles, optionally filtered by search query
   def index
     if params[:search]
       @articles = Article.search(params[:search])
@@ -7,14 +8,17 @@ class ArticlesController < ApplicationController
     end
   end
 
+  # Show details of a specific article
   def show
     @article = Article.find(params[:id])
   end
 
+  # Display a form for creating a new article
   def new
     @article = Article.new
   end
 
+  # Create a new article based on form submission
   def create
     @article = Article.new(article_params)
     @article.date = Date.today
@@ -26,10 +30,12 @@ class ArticlesController < ApplicationController
     end
   end
 
+  # Display a form for editing an existing article
   def edit
     @article = Article.find(params[:id])
   end
 
+  # Update an existing article based on form submission
   def update
     @article = Article.find(params[:id])
 
@@ -40,6 +46,7 @@ class ArticlesController < ApplicationController
     end
   end
 
+  # Delete an existing article
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
@@ -48,8 +55,8 @@ class ArticlesController < ApplicationController
   end
 
   private
+    # Define the allowed parameters for article creation and update
     def article_params
       params.require(:article).permit(:title, :content, :author, :date)
     end
-
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,10 @@
 class Article < ApplicationRecord
+  # Validation: Ensure presence of title and content for every article
+  validates :title, presence: true
+  validates :content, presence: true
 
-    validates :title, presence: true
-    validates :content, presence: true
-
-    def self.search(query)
-        where("title LIKE :query OR content LIKE :query", query: "%#{query}%")
-    end
+  # Method for searching articles based on title, content, or author
+  def self.search(query)
+    where("title LIKE :query OR content LIKE :query OR author LIKE :query", query: "%#{query}%")
+  end
 end
