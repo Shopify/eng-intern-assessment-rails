f43bde009e3c923193f186e2e6dd63675020a552
Grace Kan <gracex1@students.cs.ubc.ca>
2024-01-26 21:07:24 -0800

    added delete, fixed create validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id]);
-  end 
+  end
 
   def new
     @article = Article.new
@@ -34,6 +34,14 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+ 
+ 
+    redirect_to root_path, status: :see_other
+  end
+ 
   private
   def article_params
     params.require(:article).permit(:title, :content, :author, :date)
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,4 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
-  validates :author, presence: true
-  validates :date, presence: true
-  #add validation for author and date
 end
