a265277d8b726ed41e5bf124d6ccbf13955e9ead
Matthew Dunn <mattldunn@gmail.com>
2024-01-27 14:04:02 -0500

    Search + tests passing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    @articles = Article.search(params[:search])
   end
 
   def show
@@ -44,6 +44,6 @@ class ArticlesController < ApplicationController
 
   private
     def article_params
-      params.require(:article).permit(:title, :author, :content, :date)
+      params.require(:article).permit(:title, :author, :content, :date, :search)
     end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,12 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
+
+  def self.search(search)
+    if search
+      @articles = Article.where("title LIKE ? or content LIKE ?", "%#{search}%", "%#{search}%") # search by title and content
+    else
+      @articles = Article.all
+    end
+  end
 end
