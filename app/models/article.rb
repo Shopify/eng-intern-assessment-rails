f04186a8d458c9c852033b624a35f9e770ce5cb8
Neriman Dikmen <sanerneriman@gmail.com>
2024-01-28 16:02:17 -0500

    Add search method to models which allows partial match

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,10 @@
 class Article < ApplicationRecord
+  def self.search(search)
+    if search
+        # partial match: as long as the title(s) or content(s) include the given param, it will be retrieved
+        @articles = Article.where("title like ?", "%" + search + "%").or(Article.where("content like ?", "%" + search + "%"))
+    elsif
+        @articles = Article.all
+    end
+  end
 end
