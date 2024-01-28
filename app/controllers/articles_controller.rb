bae22f09aec12e3970ea361867338cd10c11ac2f
Kim Pham <kpham@surfer-172-29-42-229-hotspot.internet-for-guests.com>
2024-01-28 17:37:41 +0100

    done with basic crud functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,63 @@
 class ArticlesController < ApplicationController
   def index
-  end
-
-  def new
-  end
-
-  def create
+    if params[:query]
+      @articles = Article.search(params[:query])
+    else
+    @articles = Article.all
+    end
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
-  def edit
+  def new
+    @article = Article.new
   end
 
-  def update
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+    # # check for validity
+    # if @article.valid?
+    #   flash[:errors] = 'Article Created Successfully'
+    #   redirect_to articles_path
+    # else
+    #   flash[:errors] = @article.errors.full_messages
+    #   render :new
+    # end
+
   end
 
-  def destroy
+def edit
+    @article = Article.find(params[:id])
+end
+
+def update
+  @article = Article.find(params[:id])
+
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render :edit, status: :unprocessable_entity
+  end
+end
+
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+
+  redirect_to root_path, status: :see_other
+end
+
+
+  private
+  def article_params 
+    params.require(:article).permit(:title, :content, :author, :date)
   end
 end
