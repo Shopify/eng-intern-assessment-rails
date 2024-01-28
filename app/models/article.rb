726b7301f9d7108bd5a1dffad443ac9b64a5a6dd
vithushon <v.thayalan@mail.utoronto.ca>
2024-01-28 17:18:46 -0500

    Implemented search method within Article class. Tests all seem to pass now.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,11 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
+
+  # Implements search functionality within class Article.
+  # Checks to see if input matches title, content, or author
+  def self.search (query)
+    key = "%#{query}%"
+    @articles = Article.where("title LIKE ? or content LIKE ? or author LIKE?", key, key, key)
+  end
 end
