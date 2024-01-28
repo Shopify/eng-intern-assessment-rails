c8208951e32ac2a5351b589f9daab5be2e961135
Chohyeon kim <chohyeonkim94@gmail.com>
2024-01-27 19:37:17 -0800

    Add Article class and generate CreateArticles migration

---
+++ app/models/article.rb
@@ -0,0 +1,8 @@
+class Article < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 1 }
+  validates :content, presence: true, length: { minimum: 1 }
+
+  def self.search(search_keyword)
+    Article.where('title LIKE :search OR content LIKE :search', search: "%#{search_keyword}%")
+  end
+end
