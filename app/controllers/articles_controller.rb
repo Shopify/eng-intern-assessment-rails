2115e97dd1cc39c11bbc439e7b63c4a07fbf99b5
Hamza Dincer <hamzamustakdincer@gmail.com>
2024-01-28 10:31:09 -0500

    basic crud function implemented to controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,63 @@
 class ArticlesController < ApplicationController
-end
+
+    # GET /articles
+    # Shows a list of all articles
+    def index
+      @articles = Article.all
+    end
+  
+    # GET /articles/:id
+    # Shows a specific article based on its id
+    def show
+      @article = Article.find(params[:id])
+    end
+  
+    # GET /articles/new
+    # Displays the form for creating a new article
+    def new
+      @article = Article.new
+    end
+  
+    # GET /articles/:id/edit
+    # Displays the form for editing an existing article
+    def edit
+      @article = Article.find(params[:id])
+    end
+  
+    # POST /articles
+    # Creates a new article
+    def create
+      @article = Article.new(article_params)
+      if @article.save
+        redirect_to @article
+      else
+        render 'new'
+      end
+    end
+  
+    # PATCH/PUT /articles/:id
+    # Updates an existing article
+    def update
+      @article = Article.find(params[:id])
+      if @article.update(article_params)
+        redirect_to @article
+      else
+        render 'edit'
+      end
+    end
+  
+    # DELETE /articles/:id
+    # Deletes a specific article
+    def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+      redirect_to articles_path
+    end
+  
+    private
+    # Strong parameters for article creation and update
+    def article_params
+      params.require(:article).permit(:title, :content, :author, :date)
+    end
+    
+  end
