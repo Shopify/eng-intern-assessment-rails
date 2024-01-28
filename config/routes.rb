02858c5ea6bcb643a46ac40588168da1c5be0488
Mansoor Syed <mansoorhu10@gmail.com>
2024-01-28 14:50:02 -0500

    Created article model and first controller action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'articles/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,4 +8,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "articles#index"
+
+  get "/articles", to: "articles#index"
 end
