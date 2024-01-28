8f244fa1735a6730f857446783f554f2c2f4dc1f
Omer Munam <39843323+omer-munam@users.noreply.github.com>
2024-01-28 17:43:30 -0500

    Fixed delete button + Refactoring

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,18 @@
 class Article < ApplicationRecord
-    validates :title, :content, presence: true
+    validates :title, :content, presence: true      # validating title and content according to test cases
 
+    # Entry method for searching articles
     def self.search(search)
-        if search
-            @articles = Article.where("content LIKE CONCAT('%',?,'%') OR title LIKE CONCAT('%',?,'%')",search,search)
-        else 
-            @articles = Article.all
-        end
+        return all unless search        # returns all articles if search is empty
+
+        articles_searched_by_attribute(:content, search)            # search the string in content or title according to test cases
+            .or(articles_searched_by_attribute(:title, search))
+    end
+    
+    private
+    
+    # Method for searching articles by attribute
+    def self.articles_searched_by_attribute(attribute, search)
+        where(arel_table[attribute].matches("%#{search}%"))     # used arel table to construct where query based on the attribute parameter
     end
 end
