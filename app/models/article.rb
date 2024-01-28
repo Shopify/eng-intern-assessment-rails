278943f9696f68fe23725fa68e219cd05a23f392
Hamza Dincer <hamzamustakdincer@gmail.com>
2024-01-28 14:30:25 -0500

    added future date validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,7 @@ class Article < ApplicationRecord
   # Validations to ensure data integrity:
   validates :title, presence: true, uniqueness: true
   validates :content, presence: true, uniqueness: true
+  before_validation :validate_date_not_in_future
 
   # Case Insensitive Search function for retrieving articles based on search criteria.
   # It allows users to search for articles by title, content, or author.
@@ -13,6 +14,13 @@ class Article < ApplicationRecord
       where('LOWER(title) LIKE :term OR LOWER(content) LIKE :term OR LOWER(author) LIKE :term', term: "%#{term}%")
     else
       all
+    end
+
+  end
+  # Validation method to check if the date is not in the future
+  def validate_date_not_in_future
+    if date && date > Date.today
+      errors.add(:date, "cannot be a future date")
     end
   end
 end
