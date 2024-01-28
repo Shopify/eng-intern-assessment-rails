a97239398538064ed2665376350a20978e900375
Gavin George <67127770+legofanclub@users.noreply.github.com>
2024-01-28 17:51:35 -0500

    add default date setting to new articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  attribute :date, :date, default: Date.today
+
   def self.search(query)
     where("title LIKE ? OR content LIKE ?", "%#{query}%", "%#{query}%")
   end
