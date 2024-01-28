dcf5690f75c69345fb19d3f565fbfc413fe07296
naman108 <natha.paquette@gmail.com>
2024-01-28 17:14:26 -0400

    implemented create operation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,34 @@
 # This controller is responsible for creating new articles, displaying existing articles, searching for articles,
 # and deleting articles.
 class ArticlesController < ApplicationController
+  def search; end
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find_by(id: params[:id])
+  end
+
+  # GET /article/new
+  def new
+    @article = Article.new
+  end
+
+  # POST /article
+  def create
+    @article = Article.new(article_params)
+
+    if @article.valid? && @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content, :author, :date)
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,23 @@
 class Article < ApplicationRecord
+  before_create :set_default_date
+  before_update :set_default_date
+
+  validates :title, presence: true
+  validates :content, presence: true
+  # an author is not required for an article as it will be set to 'Anonymous' by default
+  validates :author, presence: false
+  # a date is not required for an article as it will be set to the current date by default
+  validates :date, presence: false
+
+  private
+
+  # Sets the default date to the current time.
+  def set_default_date
+    self.date ||= Time.now
+  end
+
+  # Sets the default author to 'Anonymous'.
+  def set_default_author
+    self.author ||= 'Anonymous'
+  end
 end
