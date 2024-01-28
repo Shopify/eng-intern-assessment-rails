a8ea60c2fb50e7573409849e9905260a41505fe8
naman108 <natha.paquette@gmail.com>
2024-01-28 14:22:09 -0400

    Generate article resource outline

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# The ArticlesController handles requests for the Article model.
+# This controller is responsible for creating new articles, displaying existing articles, searching for articles,
+# and deleting articles.
+class ArticlesController < ApplicationController
+end
