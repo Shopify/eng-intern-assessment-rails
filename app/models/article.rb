91e70241185f88657b67236dfbe9d3a5e7383641
1ncipient <hso28@uwo.ca>
2024-01-27 23:43:46 -0500

    Implement create operation for articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+    validates :title, presence: true, length: { maximum: 300 }
+    validates :body, presence: true, length: { minimum: 10 }
+    validates :author, presence: true, length: { maximum: 100 }    
+
 end
