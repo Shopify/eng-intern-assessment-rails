81fb8344353e57c21195213961117f4855442485
Hamza Dincer <hamzamustakdincer@gmail.com>
2024-01-28 11:32:56 -0500

    added search functionality

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,13 @@ class Article < ApplicationRecord
     validates :title, presence: true
     validates :content, presence: true
 
-    # search function here
-
+    # search function 
+    def self.search(search_term)
+        if search_term
+          term = search_term.downcase
+          where('LOWER(title) LIKE :term OR LOWER(content) LIKE :term OR LOWER(author) LIKE :term', term: "%#{term}%")
+        else
+          all
+        end
+    end    
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,3 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-
-[
-  { title: "The History of Computing", content: "An in-depth look at the history of computing...", author: "Alex Smith", date: Date.new(2020,1,1) },
-  { title: "Exploring the Ocean Depths", content: "Discover the mysteries of the deep ocean...", author: "Marie Johnson", date: Date.new(2020,6,15) },
-  { title: "Space Exploration: Past and Present", content: "A journey through the history of space exploration...", author: "Chris Wong", date: Date.new(2021,3,10) }
-].each do |article_attrs|
-  Article.find_or_create_by!(title: article_attrs[:title]) do |article|
-    article.content = article_attrs[:content]
-    article.author = article_attrs[:author]
-    article.date = article_attrs[:date]
-  end
-end
