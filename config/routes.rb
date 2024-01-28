b57967c3ae2870f246079b07fa9582a581f64e9e
dkucz <kuczdominik@gmail.com>
2024-01-28 16:50:44 -0500

    Configured routes.rb so that Articles shows up on website

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # Adds RESTful routes for articles
+  resources :articles 
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
