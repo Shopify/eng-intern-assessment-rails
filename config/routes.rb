b5398363609e82ca1cad163239a3122a3f672f87
Omar Nunez Siri <90123670+omarnunezsiri@users.noreply.github.com>
2024-01-28 00:37:30 +0000

    ✨ Set articles index page as root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Resource route for articles
   resources :articles
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,5 +9,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "articles#index"
 end
