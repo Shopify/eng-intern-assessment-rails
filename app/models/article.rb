aebfd3c0ec02a55168b53599a012ac373055e999
dkucz <kuczdominik@gmail.com>
2024-01-28 13:48:03 -0500

    Added validation for title and content, as well as search method for Article Model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,12 @@
 class Article < ApplicationRecord
+    # Asserts all articles have a title
+    validates :title, presence: true
+    # Asserts all articles have contents
+    validates :content, presence: true
+
+    # Search method for articles
+    def self.search(term)
+        # Searches for articles that contain term in title or content
+        where("title LIKE ? OR content LIKE ?", "%#{term}%", "%#{term}%")
+    end
 end
