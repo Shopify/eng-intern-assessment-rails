9cd466534a362f64fba32ce58e73ab2ccf32c8b2
sarabah <sarabah@remote.students.cs.ubc.ca>
2024-01-27 16:42:40 -0800

    feat: create articles controller and set application home page

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "articles#index"
+  get "/articles", to: "articles#index"
 end
