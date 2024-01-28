93f3aee015512680b2b035125bfb7ec89a3a3dfd
Jasper St. Pierre <jasperstpierre@hotmail.com>
2024-01-28 00:23:39 -0500

    feat: search scope on articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
+
+  scope :search, ->(query) { where("title LIKE :query OR content LIKE :query", query: "%#{query}%") }
 end
