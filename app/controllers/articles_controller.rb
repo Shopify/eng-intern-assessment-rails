d0bfca12041a51cd849b926d1a27777dfa7f968e
Neriman Dikmen <sanerneriman@gmail.com>
2024-01-29 18:04:32 -0500

    Debug the edit functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -41,7 +41,7 @@ class ArticlesController < ApplicationController
   def update
     respond_to do |format|
       if @article.update(article_params)
-        format.html { redirect_to new(@article), notice: "Article was successfully updated." }
+        format.html { redirect_to new, notice: "Article was successfully updated." }
         format.json { render :show, status: :ok, location: @article }
       else
         format.html { render :edit, status: :unprocessable_entity }
