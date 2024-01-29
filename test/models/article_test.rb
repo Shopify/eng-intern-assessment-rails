e3d87184301a10a9e1adba38d6be28f0be971d40
spitgranger <richard.fan2000@hotmail.com>
2024-01-29 00:01:04 -0500

    feature: add unit test for blank query

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -65,4 +65,13 @@ class ArticleTest < ActiveSupport::TestCase
     assert_includes results, article2
     assert_not_includes results, article1
   end
+
+  test 'display relevant articles if search query provided is empty' do
+    article1 = Article.create(title: 'Sample Article', content: 'Lorem ipsum dolor sit amet.')
+    article2 = Article.create(title: 'Another Article', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
+    results = Article.search("")
+    assert_includes results, article2
+    assert_includes results, article1
+  end
+
 end
