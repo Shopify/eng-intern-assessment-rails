d68f03bc50c425dff8ea7866e940d72d9e9275f0
Ramandeep <ramandeepsaini1379@gmail.com>
2024-01-29 11:19:05 -0500

    Completed index.html.erb file for homepage, edited application.css file to design homepage

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,11 @@
+# defines a class called article
 class Article < ApplicationRecord
+    # checks that title is valid
     validates :title, presence: true
+    # checks that content is valid
     validates :content, presence: true
   
+    # defines method to search for article
     def self.search(query)
       where('title LIKE ? OR content LIKE ?', "%#{query}%", "%#{query}%")
     end
