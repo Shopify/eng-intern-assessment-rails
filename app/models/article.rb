4416a96042759f32a35b06a55b7678c3e98f8956
邓子昕 <dengzixin@s-MacBook-Pro.local>
2024-01-29 17:36:35 -0500

    final version

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,13 +1,20 @@
 class Article < ApplicationRecord
-
-def self.search(term)
-    if term.present?
-      where('title LIKE ? OR content LIKE ?', "%#{term}%", "%#{term}%")
-    else
-      all
+    # Define a class-level search method that searches for articles based on a provided term.
+    # If a term is present, it performs a SQL query to find articles with titles or content
+    # containing the term (case-insensitive search using SQL's LIKE operator).
+    # If no term is provided, it returns all articles.
+    #
+    # Params:
+    # - term: The search term to be used for filtering articles.
+    #
+    # Returns:
+    # - An ActiveRecord relation representing the search results or all articles.
+    def self.search(term)
+      if term.present?
+        where('title LIKE ? OR content LIKE ?', "%#{term}%", "%#{term}%")
+      else
+        all
+      end
     end
- end
-
-
-
-end
+  end
+  
