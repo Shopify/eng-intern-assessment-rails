cfaa73efdc6f57dd1b0528d37e72c583d226940d
jeffreychan <kkjeffreychan@gmail.com>
2024-01-28 23:55:04 -0500

    Implement CRUD operations in ArticlesController, add search endpoint, and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :articles do
+    collection do
+      get 'search'
+    end
+  end
+  
+  root "articles#index"
 end
