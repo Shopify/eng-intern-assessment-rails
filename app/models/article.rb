c6128f57c32105447715789fc9fe9fc52d9eff51
naman108 <natha.paquette@gmail.com>
2024-01-29 10:38:21 -0400

    added documentation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,14 +1,19 @@
+# frozen_string_literal: true
+
+# Article model that represents an article in the database.
 class Article < ApplicationRecord
   before_create :set_default_date
   before_update :set_default_date
 
   validates :title, presence: true
   validates :content, presence: true
+
   # an author is not required for an article as it will be set to 'Anonymous' by default
   validates :author, presence: false
   # a date is not required for an article as it will be set to the current date by default
   validates :date, presence: false
 
+  # Returns all articles that match the search term.
   def self.search(search_term)
     where('title LIKE ? OR content LIKE ?', "%#{search_term}%", "%#{search_term}%")
   end
@@ -24,5 +29,4 @@ class Article < ApplicationRecord
   def set_default_author
     self.author ||= 'Anonymous'
   end
-  
 end
