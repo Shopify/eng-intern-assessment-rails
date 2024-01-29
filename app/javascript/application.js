fb06e36db68d45d464965fab1fa8483f10c02768
dkucz <kuczdominik@gmail.com>
2024-01-28 20:06:46 -0500

    Added necessary imports for delete article functionality

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import Rails from '@rails/ujs'
+import "@rails/ujs";
+
+Rails.start();
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,5 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "@rails/ujs", to: "rails-ujs.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
