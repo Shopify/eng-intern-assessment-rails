12586b504972af67ad66cd88277d1dc0b44d5bb3
geepara <62688021+geepara@users.noreply.github.com>
2024-01-29 16:34:11 -0500

    article model

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,4 +1,23 @@
-class ArticleController < ApplicationController
+class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
