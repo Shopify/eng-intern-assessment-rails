e17f1c90dd52a943f3dfdab0c29991e68b1a8aad
邓子昕 <dengzixin@s-MacBook-Pro.local>
2024-01-29 15:41:52 -0500

    CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,24 +8,55 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article=Article.new
+    @article.date=Date.today
   end
 
 
   def create
+    @article=Article.new(article_params)
+    if @article.save 
+      redirect_to articles_path
+    else
+      render('new')
+    end
+
   end
 
   def edit
+    @article=Article.find(params[:id])
   end
 
   def update
+    @article=Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    else
+      render('edit')
+    end
+    
   end
 
 
   def delete
+    @article=Article.find(params[:id])
   end
 
   def destroy
+    @article=Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 
 
+end
+
+private 
+def article_params
+  params.require(:article).permit(
+    :title, 
+    :content, 
+    :author,
+    :date
+  )
 end
