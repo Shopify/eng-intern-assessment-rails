8860bb8d74a76ff90941779582def22b3585e05f
cvgjnh <harry.ty.hu@gmail.com>
2024-01-29 03:10:12 -0800

    Add extra comments for documentation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,24 @@
 class ArticlesController < ApplicationController
+  # Retrieves all articles from the database and assigns them to @articles.
   def index
     @articles = Article.all
   end
 
+  # Retrieves a specific article from the database based on the provided ID and assigns it to @article.
+  # Params:
+  # - id: The ID of the article to retrieve.
   def show
     @article = Article.find(params[:id])
   end
 
+  # Initializes a new instance of Article and assigns it to @article.
   def new
     @article = Article.new
   end
 
+  # Creates a new article based on the provided parameters and saves it to the database.
+  # If the article is successfully saved, redirects to the show page of the created article.
+  # Otherwise, renders the new template with a status of :unprocessable_entity.
   def create
     @article = Article.new(article_params)
 
@@ -21,10 +29,18 @@ class ArticlesController < ApplicationController
     end
   end
 
+  # Retrieves a specific article from the database based on the provided ID and assigns it to @article.
+  # Params:
+  # - id: The ID of the article to retrieve.
   def edit
     @article = Article.find(params[:id])
   end
 
+  # Updates a specific article in the database based on the provided ID and parameters.
+  # If the article is successfully updated, redirects to the show page of the updated article.
+  # Otherwise, renders the edit template with a status of :unprocessable_entity.
+  # Params:
+  # - id: The ID of the article to update.
   def update
     @article = Article.find(params[:id])
 
@@ -35,6 +51,10 @@ class ArticlesController < ApplicationController
     end
   end
 
+  # Deletes a specific article from the database based on the provided ID.
+  # After deletion, redirects to the root path with a status of :see_other.
+  # Params:
+  # - id: The ID of the article to delete.
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
@@ -43,8 +63,9 @@ class ArticlesController < ApplicationController
   end
 
   private
-    def article_params
-      params.require(:article).permit(:title, :content)
-    end
 
+  # Strong parameters for article creation and update.
+  def article_params
+    params.require(:article).permit(:title, :content, :author, :date)
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,15 @@
 class Article < ApplicationRecord
-    validates :title, presence: true
-    validates :content, presence: true
+  # Although not explicitly required by the tests, logically it makes sense that
+  # an encyclopedia article should have a title and content.
+  # The author and date are optional, as per the tests.
+  validates :title, presence: true
+  validates :content, presence: true
 
-    def self.search(query) # searches for the query in the title or content of the article
-        where("title LIKE ? OR content LIKE ?", "%#{query}%", "%#{query}%")
-    end
+  # Searches for articles that match the given query in the title or content.
+  #
+  # @param query [String] The search query.
+  # @return [ActiveRecord::Relation] The collection of articles that match the query.
+  def self.search(query)
+    where("title LIKE ? OR content LIKE ?", "%#{query}%", "%#{query}%")
+  end
 end
