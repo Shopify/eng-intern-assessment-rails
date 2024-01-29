87cce0a36d35c4462d0f88f736c7b46a44feb854
Ahren Chen <ahren@hotmail.ca>
2024-01-29 00:56:40 -0500

    Deleted useless test comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -42,10 +42,10 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    #@article = Article.find(params[:id])
+    @article = Article.find(params[:id])
     @article.destroy
 
-    #redirect_to root_path, status: :see_other
+    redirect_to root_path, status: :see_other
   end
 
   private
