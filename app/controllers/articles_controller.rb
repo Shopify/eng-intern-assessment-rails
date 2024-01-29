103f693a3381a14b85f84a8701a1bdd6f90c22d9
Ben Grass <bvgrass@outlook.com>
2024-01-29 17:59:06 -0500

    Add ArticlesController with related views, tests, and routes

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,19 @@
+class ArticlesController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def display
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
