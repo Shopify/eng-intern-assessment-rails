1c3bb19302f61e969e3d88cea08dbf5bab1f8115
Huiliang Xia <hxia47@uwo.ca>
2024-01-29 18:10:08 -0500

    🫡 Tests passed

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "articles#index"
+  get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
+
+  
 end
