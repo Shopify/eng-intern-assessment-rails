b8361243904f6f36162a8052db3188a941896293
Sherwyn Dsouza <sherwyndsouza1999@gmail.com>
2024-01-29 12:34:30 -0500

    Updated logic for creating new article and displaying all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,34 @@
 class ArticlesController < ApplicationController
+  
+  # List all articles
+  def index
+    @articles = Article.all
+  end
+
+  # Create a new article
+  def new
+    @article = Article.new
+  end
+
+  # Return an article by its id
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  # Create a new article based on the given input
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # Defines the parameters permitted for creating a new article
+  private
+  def article_params
+    params.require(:article).permit(:title, :content, :author, :date)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  root "articles#index"
+
+  # get "articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+  # replacing get functionality with resources
+  # Added search route as collection as it acts on the entire object and not a specific instance
+  resources :articles do
+    collection do
+      get "search"
+    end
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
