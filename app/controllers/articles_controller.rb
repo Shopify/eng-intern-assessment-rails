d5d104fa9620aaaae640d8a317462ca99cb63c68
kaizenJPEG <96929597+kaizenJPEG@users.noreply.github.com>
2024-01-29 15:05:16 -0500

    Updated controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,20 +4,39 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article, notice: 'Article was successfully created.'
+    else
+      render :new
+    end
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article, notice: 'Article was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_url, notice: 'Article was successfully destroyed.'
   end
 end
