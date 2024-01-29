f6f791324273b69fcd625df9cfabbb0c5ce590c7
spitgranger <richard.fan2000@hotmail.com>
2024-01-28 23:58:13 -0500

    fix: not redirecting to root path when article is deleted

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -70,7 +70,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
 
-    redirect_to root_path, status: :no_content
+    redirect_to root_path, status: :see_other
   end
 
   private
