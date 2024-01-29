3518338289aa43bd390356349afab608340bf9d3
naman108 <natha.paquette@gmail.com>
2024-01-29 10:35:30 -0400

    fixed rubocop linting

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,45 +1,46 @@
-require "test_helper"
+require 'test_helper'
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @article = articles(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get articles_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_article_url
     assert_response :success
   end
 
-  test "should create article" do
-    assert_difference("Article.count") do
-      post articles_url, params: { article: {title:'Test Article', content: 'this is a test', author: 'Test author' } }
+  test 'should create article' do
+    assert_difference('Article.count') do
+      post articles_url,
+           params: { article: { title: 'Test Article', content: 'this is a test', author: 'Test author' } }
     end
 
     assert_redirected_to article_url(Article.last)
   end
 
-  test "should show article" do
+  test 'should show article' do
     get article_url(@article)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_article_url(@article)
     assert_response :success
   end
 
-  test "should update article" do
+  test 'should update article' do
     patch article_url(@article), params: { article: { content: 'this is modified content' } }
     assert_redirected_to article_url(@article)
   end
 
-  test "should destroy article" do
-    assert_difference("Article.count", -1) do
+  test 'should destroy article' do
+    assert_difference('Article.count', -1) do
       delete article_url(@article)
     end
 
