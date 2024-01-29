b72d019d3bfee34cac2f957f3e6c968199deaeb6
bikramjit-dhaliwal <45516492+bikramjit-dhaliwal@users.noreply.github.com>
2024-01-29 11:44:44 -0600

    Generated a controller
    Used scaffold generator to create a controller and some basic views

--- app/controllers/article_controller.rb
+++
@@ -1,3 +0,0 @@
-class ArticleController < ApplicationController
-
-end
--- test/controllers/article_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ArticleControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
