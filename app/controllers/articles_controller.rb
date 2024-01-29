214ec2c29484205823e9284d3152964fab26f709
Zach Legesse <zachlegesse@gmail.com>
2024-01-29 23:53:01 +0000

    made the create method use strong parameters

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,17 +10,27 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  # Creates an article to be used as a template for the user to fill out
   def new
     @article = Article.new
   end
 
+  # Creates a new article based on the user given parameters
   def create
-    @article = Article.new(title:params[:article][:title],content:params[:article][:content])
+    @article = Article.new(article_params)
 
+    # If the article cannot be saved (i.e. its invalid), alert the user
     if @article.save
       redirect_to @article
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  # This method filters parameters pushed when a new article is created
+  # This saves our app from some rudementary sql attacks
+  private
+  def article_params
+    params.require(:article).permit(:title,:content)
   end
 end
