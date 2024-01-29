ea807521f51d1f5fc168e8195705749c91b7c1f7
Zach Legesse <zachlegesse@gmail.com>
2024-01-29 23:53:16 +0000

    added validaters to teh Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+    # This makes sure that title and content both exist when
+    # a new article is being created.
+    validates :title, presence: true
+    validates :content, presence: true
 end
