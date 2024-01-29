8bb0fd91164b5e2fc88db7050325a8ace7b27754
bikramjit-dhaliwal <45516492+bikramjit-dhaliwal@users.noreply.github.com>
2024-01-28 19:00:47 -0600

    Model Search Implemented
    The search function has been added to the model. All tests are now passing.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ApplicationRecord
+  self.table_name = "articles"
+
+  # Given text, return all results with a title/content that contains said text
+  def self.search(text)
+    @results = Article.all.where("title LIKE ? OR content LIKE ?", "%#{text}%", "%#{text}%")
+  end
 end
