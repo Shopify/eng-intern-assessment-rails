dc2fc7ec7ed9415eff4b3bf6c80807a505239fef
unknown <dennis.kiprono@mail.utoronto.ca>
2024-01-28 23:04:58 -0500

    Implement Article views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    def bootstrap_class_for_flash(type)
+        case type
+        when 'notice' then 'alert-info'
+        when 'success' then 'alert-success'
+        when 'error' then 'alert-danger'
+        when 'alert' then 'alert-warning'
+        else type.to_s
+        end
+    end
 end
