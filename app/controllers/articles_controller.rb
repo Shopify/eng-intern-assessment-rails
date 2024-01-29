d4cfc6c21d8d48090eb3dbbcbce7272d29df0fd1
Zach Legesse <zachlegesse@gmail.com>
2024-01-29 21:38:30 +0000

    Updated index to use the 'link_to' helper function Added a new and create method to the Articles controller Created new.html.erb to allow for user creation of articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,26 @@
 class ArticlesController < ApplicationController
+  
+  # gathers all articles for use in index.html.erb
   def index
     @articles = Article.all
   end
+
+  # finds articles as requested through articles/:id for use in show.html.erb
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title:params[:article][:title],content:params[:article][:content])
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
