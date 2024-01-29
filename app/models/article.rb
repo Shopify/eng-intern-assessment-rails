7d30952329cc8a7a78c4641989dbf25d0aef227e
Neriman Dikmen <sanerneriman@gmail.com>
2024-01-29 17:25:09 -0500

    Add validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,10 @@
 class Article < ApplicationRecord
+  
+    validates :title, presence: true
+    validates :content, presence: true
+    # validates :author, presence: true
+    # validates :date, presence: true
+  
   def self.search(search)
     if search
         # partial match: as long as the title(s) or content(s) include the given param, it will be retrieved
