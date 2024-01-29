89cc8529a2b092e2b6b28922bdb0e372931463c6
Juan Velilla <juandvelillamantilla@gmail.com>
2024-01-28 20:00:47 -0500

    added comments to the controller,  reduced repetition, set before action"

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,10 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: %i[:show, :edit, :update, :destroy]
+    
+    # GET /articles
+    # GET /articles?search=:term
+    # If a search term is provided, returns articles that match the search term.
+    # Otherwise, returns all articles, ordered by the updated_at field in descending order.
     def index
         if params[:search].present?
             @articles = Article.search(params[:search])
@@ -6,51 +12,61 @@ class ArticlesController < ApplicationController
             @articles = Article.order("updated_at DESC")
         end
     end
+    
+    # GET /articles/:id
+    # Returns a single article identified by the id parameter.
+    def show, end
 
+    # GET /articles/new
+    # Initializes a new, unsaved article.
     def new 
         @article = Article.new
     end
 
-    def show
-        @article = Article.where(id: params[:id]).first
-    end 
-
+    # POST /articles
+    # Attempts to create a new article with the provided parameters. If successful, redirects to the articles index.
+    # If unsuccessful, does nothing (validation and error handling to be added).
     def  create
         @article = Article.new(article_params)
         if @article.save
-            redirect_to articles_path
+            redirect_to articles_path, notice: 'Article was successfully created.'
         else
-        # empty for now if I have time I will add validation.            
+        # TODO: Add validation and error handling.            
         end
     end  
 
-    def edit
-        @article = Article.where(id: params[:id]).first
-    end
-
+    # GET /articles/:id/edit
+    # Finds an article identified by the id parameter and prepares it for editing.
+    def edit, end
+    
+    # PATCH/PUT /articles/:id
+    # Attempts to update an article identified by the id parameter with the provided parameters.
+    # If successful, redirects to the article's detail view. If unsuccessful, re-renders the edit view.
     def update
-        @article = Article.where(id: params[:id]).first
         if @article.update(article_params)
-            redirect_to article_path 
+            redirect_to article_path, notice: 'Article was successfully updated.'
         else
             render :edit
         end
     end
 
+    # DELETE /articles/:id
+    # Attempts to destroy an article identified by the id parameter. If successful, redirects to the articles index.
+    # If unsuccessful, does nothing (error handling to be added).
     def destroy
-        @article = Article.find(params[:id])
-        if @article.destroy
-            redirect_to articles_path
-        else
-            # Add error handling here if needed
-        end
+        @article.destroy
+        redirect_to articles_path, notice: 'Article was successfully destroyed.'
     end
-
 
     private
-
-    def article_params
-        params.require(:article).permit(:title)
+    
+    # Use callbacks to share common setup or constraints between actions
+    def set_article
+        @article = Article.find(params[:id])
     end
 
+    # Only allow a list of trusted parameters through.
+    def article_params
+        params.require(:article).permit(:title, :content, :author)
+    end
 end
