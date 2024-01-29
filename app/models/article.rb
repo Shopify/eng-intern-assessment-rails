661cb3a58c66fb12ec2d0b09a99ed99eba1bddfe
Gary050613 <64561657+Gary050613@users.noreply.github.com>
2024-01-29 00:12:03 -0800

    Added CRUD in controllers, added views

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true, length: { minimum: 10 }
+
   def self.search(text)
     key = "%#{text.downcase}%"
     @article = Article.where("LOWER(title) LIKE ? or LOWER(content) LIKE ? or LOWER(author) LIKE ?", key, key, key)
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "articles#index"
+  resources :articles
 end
