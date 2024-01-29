ca145b6a0db021322865c1e091355da7030ac575
cvgjnh <harry.ty.hu@gmail.com>
2024-01-29 00:08:44 -0800

    Add search functionality to pass tests

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
     validates :title, presence: true
     validates :content, presence: true
+
+    def self.search(query) # searches for the query in the title or content of the article
+        where("title LIKE ? OR content LIKE ?", "%#{query}%", "%#{query}%")
+    end
 end
