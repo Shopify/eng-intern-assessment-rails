73ab65c2b187d91758ee0ab4b6b32427e2dbb870
Juan Velilla <juandvelillamantilla@gmail.com>
2024-01-29 13:29:24 -0500

    refactor: Changed main route to root to articles index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
+  # set Articles index page as the main page
+  root 'articles#index'
+
   resources :articles
 end
