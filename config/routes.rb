ba8b3f5d9fb982a3d56fa5ed3d5de9c8f6cdb6f1
ANUJAN MATHISEKARAN <amathise@uwaterloo.ca>
2024-01-29 17:14:13 -0500

    feat: add articles and search routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root "articles#index"
+  get 'articles', to: 'articles#index'
+  get 'search', to: 'articles#search'
+  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
