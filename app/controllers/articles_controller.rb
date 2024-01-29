b27049d7b173fa0f28364f5ab8a36c5d90a6a257
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2024-01-29 09:45:57 -0500

    feature/Add articles and display links on index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,41 @@
 class ArticlesController < ApplicationController
+  # Display a list of all articles
   def index
     @articles = Article.all
+  end
+
+  # Display a single article
+  def show
+  end
+
+  # Render a form to create a new article
+  def new
+    @article = Article.new
+  end
+
+  # Create a new article based on form submission
+  def create
+    @article = Article.new(article_params)
+    
+    # Attempt to save the article
+    if @article.save
+      #If the article is successfully saved, redirect to the show page with a success notice
+      redirect_to @article, notice: 'Article was successfully created.'
+    else
+      # If there are errors, re-render the new article form
+      render :new
+    end
+  end
+
+  private
+
+  # Define the permitted parameters for creating a new article
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  # Find the article before executing certain actions
+  def find_article
+    @article = Article.find(params[:id])
   end
 end
