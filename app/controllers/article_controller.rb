2da45e62dbe55d8b3c92925ccadfb8f87fd4e57c
geepara <62688021+geepara@users.noreply.github.com>
2024-01-29 16:50:05 -0500

    update article method

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -7,17 +7,28 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-  def new
-    @article = Article.new
-  end
-
   def create
-    @article = Article.new(title: "...", body: "...")
+    @article = Article.new(article_params)
 
     if @article.save
-      redirect_to @article
+      render json: @article, status: :created
     else
       render :new, status: :unprocessable_entity
     end
   end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      render json: @article, status: :ok
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :content, :author, :date)
+    end
 end
