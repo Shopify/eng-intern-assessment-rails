17f33bd099ecd7d1b1380194d16378b0840dc7e9
spitgranger <richard.fan2000@hotmail.com>
2024-01-28 23:59:34 -0500

    chore: update comments on article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,8 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
 
+  # Define the class search method on the Article model. It uses the SQL "LIKE" keyword to find a list of articles
+  # which contains either the provided query in the title or content
   def self.search(search_query)
     where("title LIKE ? or content LIKE ?", "%" + search_query + "%", "%" + search_query + "%")
   end
