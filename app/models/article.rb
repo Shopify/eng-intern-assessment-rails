bae2608fb47553edd9cce4b1c9f858cc322b5f02
afrazhemraj <afrazhemraj@gmail.com>
2024-01-29 13:35:07 -0500

    feature-001: add basic crud functionality

---
+++ app/models/article.rb
@@ -0,0 +1,6 @@
+class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true, length: { minimum: 10 }
+  validates :author, presence: true
+  validates :date, presence: false
+end
