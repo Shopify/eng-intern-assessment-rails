e83d33e3f03ecd88983ef0af8a6491a21eacfa67
naman108 <natha.paquette@gmail.com>
2024-01-29 10:34:50 -0400

    fixed system tests and linting

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,23 +4,28 @@
 # This controller is responsible for creating new articles, displaying existing articles, searching for articles,
 # and deleting articles.
 class ArticlesController < ApplicationController
+
+  # GET /article
   def index
     @articles = Article.all
   end
 
+  # GET /article/:id
   def show
     @article = Article.find_by(id: params[:id])
   end
 
+  # GET /article/:id/edit
   def edit
     @article = Article.find(params[:id])
   end
 
+  # PATCH/PUT /article/:id
   def update
     @article = Article.find_by(id: params[:id])
 
     if @article.update(article_params)
-      redirect_to @article
+      redirect_to @article, notice: 'Article was successfully updated'
     else
       render :edit, status: :unprocessable_entity
     end
@@ -36,21 +41,23 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.valid? && @article.save
-      redirect_to @article
+      redirect_to @article, notice: 'Article was successfully created'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  # DELETE /article/:id
   def destroy
     @article = Article.find_by(id: params[:id])
     @article.destroy
 
-    redirect_to articles_path, notice: :see_other
+    redirect_to articles_path, notice: 'Article was successfully destroyed'
   end
 
   private
 
+  # Only allow a list of trusted parameters through.
   def article_params
     params.require(:article).permit(:title, :content, :author, :date)
   end
