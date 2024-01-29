8d1388e127a0043357ce3223fc4e706ad2f6f0eb
uistn <d2mcvica@uwaterloo.ca>
2024-01-28 19:07:08 -0500

    Added skeleton view and controller

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,57 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  # search
+  def index
+    if params[:query].present?
+        @articles = Article.where("title LIKE ?", "%#{params[:query]}%")
+    else
+        @articles = Article.all
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content, :author, :date)
+  end
+end
