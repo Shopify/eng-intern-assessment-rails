595f0a0eae6c068206ed837471a0d92cd1cea29d
Amy Kang <83683262+kang-amy@users.noreply.github.com>
2024-01-29 20:57:13 +0000

    fixing typo comments

--- db/migrate/20240129001446_add_author_and_date_to_articles.rb
+++ db/migrate/20240129001446_add_author_and_date_to_articles.rb
@@ -1,5 +1,5 @@
 class AddAuthorAndDateToArticles < ActiveRecord::Migration[7.1]
-  # Migration to add an author, string and date column for the articles table
+  # Migration to add an author and date column for the articles table
   def change
     add_column :articles, :author, :string
     add_column :articles, :date, :date
