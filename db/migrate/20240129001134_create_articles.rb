65b8580d9606f45842b24091dbee0d48b5323d3f
Amy Kang <83683262+kang-amy@users.noreply.github.com>
2024-01-29 20:37:20 +0000

    adding commentsrails test :)

--- db/migrate/20240129001134_create_articles.rb
+++ db/migrate/20240129001134_create_articles.rb
@@ -1,4 +1,6 @@
 class CreateArticles < ActiveRecord::Migration[7.1]
+  # Migration for creating an articles table
+  # Includes an article title, text, and timestamps
   def change
     create_table :articles do |t|
       t.string :title
