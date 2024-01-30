6b402bbd8b309e5f4662104cbc22d9d99c6c0775
Colin Xiong <xiong.colin899@gmail.com>
2024-01-29 21:46:03 -0500

    Added comments for readability

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,21 +1,26 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[show edit update destroy]
+  # callback sets up article for show, edit, update, and destroy.
+  b_action :set_article, only: %i[show edit update destroy]
 
   # GET /articles
+  # Display a list of all articles.
   def index
     @articles = Article.all
   end
 
   # GET /articles/1
+  # Display a specific article (article set up by callback).
   def show
   end
 
   # GET /articles/new
+  # Display a form for creating a new article.
   def new
     @article = Article.new
   end
 
   # POST /articles
+  # Create a new article.
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -26,10 +31,12 @@ class ArticlesController < ApplicationController
   end
 
   # GET /articles/1/edit
+  # Display a form for editing an existing article (article set up by callback).
   def edit
   end
 
   # PATCH/PUT /articles/1
+  # Update an existing article (article set up by callback).
   def update
     if @article.update(article_params)
       redirect_to @article, notice: 'Article was successfully updated.'
@@ -39,18 +46,19 @@ class ArticlesController < ApplicationController
   end
 
   # DELETE /articles/1
+  # Delete an existing article (article set up by callback).
   def destroy
     @article.destroy
     redirect_to articles_url, notice: 'Article was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    # Callbacks to share common setup or constraints between actions.
     def set_article
       @article = Article.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+    # Allow trusted parameters through.
     def article_params
       params.require(:article).permit(:title, :content, :author, :date)
     end
