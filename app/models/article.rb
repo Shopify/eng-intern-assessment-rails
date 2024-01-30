df49a62be16c26d5435406ba8e39b64e447bd44d
Camran Hansen <camran.hansen@mail.utoronto.ca>
2024-01-29 21:55:59 -0500

    add search functionality to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,19 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true, length: { minimum: 10 }
+
+  #
+  # return articles where title, content, or author contains the search term
+  # or return all articles if the search term is empty
+  #
+  def self.search(term)
+    if term.empty?
+      all
+    else
+      # use sanitized search term for query
+      # see documentation below:
+      # https://api.rubyonrails.org/v7.1.3/classes/ActiveRecord/Sanitization/ClassMethods.html#method-i-sanitize_sql_like
+      where('title LIKE :term OR content LIKE :term OR author LIKE :term', term: "%#{sanitize_sql_like(term)}%")
+    end
+  end
 end
