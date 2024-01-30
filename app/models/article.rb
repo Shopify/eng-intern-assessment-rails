8c4cba48a6de4b7da0c05121c150cfbb0cbebe5e
Colin Xiong <xiong.colin899@gmail.com>
2024-01-29 22:01:18 -0500

    Minor changes for readability

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,14 @@
 class Article < ApplicationRecord
+  # Ensure that articles have a title and content.
   validates :title, presence: true
   validates :content, presence: true
 
+  # Search for articles by title or content.
   def self.search(term)
+    # If the search term is present, return articles that match the term.
     if term.present?
       where('title LIKE ? OR content LIKE ?', "%#{term}%", "%#{term}%")
+    # Otherwise, return all articles.
     else
       all
     end
