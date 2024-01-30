57e26fe3ccd9321ce10882481da16414494d80d1
Jonathan <jonathanginevro@outlook.com>
2024-01-29 21:05:12 -0500

    Added title and content validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ApplicationRecord
 
+    validates :title, presence: true
+    validates :content, presence: true
+
     def self.search(search)
         where("title LIKE ? OR content LIKE ?", "%#{search}%", "%#{search}%")
     end
