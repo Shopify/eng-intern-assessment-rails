c9dba87e407deb323cbe782ca4981b0e4a884f5a
ashwinak <ashwinak@umich.edu>
2024-01-29 20:02:46 -0500

    Implemented Article search functionality

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,9 @@
 class Article < ApplicationRecord
-    def self.search(query)
-        
+    def self.search(query_string)
+        return Article.where(
+            "articles.title LIKE :query OR "\
+            "articles.content LIKE :query", 
+            {query: "%#{query_string}%"}
+        )
     end
 end
