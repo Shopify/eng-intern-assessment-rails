d07c4f85a965aac481b56d541066d679c9153aba
geepara <62688021+geepara@users.noreply.github.com>
2024-01-29 19:09:49 -0500

    change controller from article to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,33 +7,39 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def new
+    @article = Article.new
+  end
+
   def create
     @article = Article.new(article_params)
 
     if @article.save
-      render json: @article, status: :created
+      redirect_to @article
     else
-      render json: @articles.errors, status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def update
     @article = Article.find(params[:id])
 
     if @article.update(article_params)
-      render json: @article, status: :ok
+      redirect_to @article
     else
-      render json: @articles.errors, status: :unprocessable_entity
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
     @article = Article.find(params[:id])
-    if @article.destroy
-      head: :no_content
-    else
-      render json: @articles.errors, status: :unprocessable_entity
+    @article.destroy
 
+    redirect_to root_path, status: :see_other
   end
 
   def search
