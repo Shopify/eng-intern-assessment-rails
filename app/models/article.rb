ff7cc4c72cac081dc59c4c8e0089461dd85d06b2
kyro105360 <kyro.nas123@gmail.com>
2024-01-29 21:08:03 -0500

    improved logic of searchbar

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,5 @@
 class Article < ApplicationRecord
     def self.search(search)
-        if search
-            where('title LIKE ? OR content LIKE ? OR author LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%")
-        end
+        Article.where('title LIKE ? OR content LIKE ? OR author LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%")
     end
 end
