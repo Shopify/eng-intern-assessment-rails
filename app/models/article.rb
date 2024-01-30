9061c5c6128d16d47ad0df7606808154d155553e
Znjis <ilyes2011@live.ca>
2024-01-29 20:54:51 -0500

    final changes:  displaying date, searching by date (mediocre)

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,7 +8,7 @@ class Article < ApplicationRecord
 
   def self.search(query)
     if query.present?
-      where("title LIKE ? OR content LIKE ? OR author LIKE ?", "%#{query}%", "%#{query}%", "%#{query}%")
+      where("title LIKE ? OR content LIKE ? OR author LIKE ? OR date LIKE ?", "%#{query}%", "%#{query}%", "%#{query}%", "%#{query}%")
     else
       all
     end
