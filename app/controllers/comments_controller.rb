6ec58df46e149f0b97b14a93dff68f7785aa44f9
zvani1 <zephvaniterson@gmail.com>
2024-01-29 21:21:11 -0500

    10 Deleting Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,13 @@ class CommentsController < ApplicationController
       redirect_to article_path(@article)
     end
   
-
+    def destroy
+      @article = Article.find(params[:article_id])
+      @comment = @article.comments.find(params[:id])
+      @comment.destroy
+      redirect_to article_path(@article), status: :see_other
+    end
+  
     private
       def comment_params
         params.require(:comment).permit(:commenter, :body, :status)
