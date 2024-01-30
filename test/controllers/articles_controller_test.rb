97b639f721873cea89bf01a8508bf2fbe788ffda
ANUJAN MATHISEKARAN <amathise@uwaterloo.ca>
2024-01-29 19:35:24 -0500

    test: add controller tests for article creation and search

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -5,4 +5,18 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     get articles_url
     assert_response :success
   end
+
+  test "should create article" do
+    assert_difference('Article.count') do
+      post articles_url, params: { article: {title: 'Sample Article', content: 'Lorem ipsum dolor sit amet.', author: 'John Doe'}}
+    end
+    assert_redirected_to article_path(Article.last)
+  end
+
+  test "should create then search for article" do
+    post articles_url, params: { article: {title: 'Sample Article', content: 'Lorem ipsum dolor sit amet.', author: 'John Doe'}}
+    get search_url, params: {query: "Lorem ipsum"}
+    assert_response 200
+    assert_select "a", "Sample Article"
+  end
 end
