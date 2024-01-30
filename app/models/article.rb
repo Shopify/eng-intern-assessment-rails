367dc2fdba8c5ce9f3ad34312fb21b721b405e08
tlikhit <58948612+tlikhit@users.noreply.github.com>
2024-01-29 21:20:44 -0500

    Add validations and default date setter to Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,10 @@
 class Article < ApplicationRecord
+  # title and content must be present & unique
+  validates_presence_of :title, :content
+  validates_uniqueness_of :title, :content, case_sensitive: false, message: 'must be unique'
+  # Set the date to today's date if it's not specified
+  after_initialize :set_default_date
+
   # Search for articles where the title or content contain a specified query.
   #
   # @param query [String] The search term to be used in the query.
@@ -6,5 +12,11 @@ class Article < ApplicationRecord
   def self.search(query)
     sanitized_query = sanitize_sql_like(query)
     where("title LIKE ? OR content LIKE ?", "%#{sanitized_query}%", "%#{sanitized_query}%")
+  end
+
+  private
+
+  def set_default_date
+    self.date ||= Date.today
   end
 end
