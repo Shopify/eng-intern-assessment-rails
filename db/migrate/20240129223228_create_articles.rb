1bc553d86c40c532f3e4d77992c553274a5b8180
Znjis <ilyes2011@live.ca>
2024-01-29 20:46:59 -0500

    fix: date was assigned as string, now fixed with date type

--- db/migrate/20240129223228_create_articles.rb
+++ db/migrate/20240129223228_create_articles.rb
@@ -4,7 +4,7 @@ class CreateArticles < ActiveRecord::Migration[7.1]
       t.string :author
       t.string :title
       t.text :content
-
+      t.date :date
       t.timestamps
     end
   end
--- db/migrate/20240130012211_add_date_to_articles.rb
+++
@@ -1,5 +0,0 @@
-class AddDateToArticles < ActiveRecord::Migration[7.1]
-  def change
-    add_column :articles, :date, :string
-  end
-end
