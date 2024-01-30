1fbe8f8eaae10b37f7deeea3358d7fdaad8154ad
Aastha Sharma <aastha.sharma@mail.utoronto.ca>
2024-01-29 22:24:57 -0500

    changed the Gemfile, and added another html page for edit

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// Imports used for the application
 import "@hotwired/turbo-rails"
 import "controllers"
