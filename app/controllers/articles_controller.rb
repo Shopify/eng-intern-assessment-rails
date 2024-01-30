772b2e3ed29e237c1e57d826631b4e6a15e9e7a6
zvani1 <zephvaniterson@gmail.com>
2024-01-29 21:30:10 -0500

    Finished the Basic Ruby On Rails
    Added basic authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
+
   def index
     @articles = Article.all
   end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+
+    http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+
     def create
       @article = Article.find(params[:article_id])
       @comment = @article.comments.create(comment_params)
