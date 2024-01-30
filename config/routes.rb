293f2bcfb1e70a62f7baf58a2de53773f596dc55
Ben Grass <bvgrass@outlook.com>
2024-01-29 23:22:32 -0500

    Add detailed comments to ArticlesController, routes, and Article model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root "articles#index"
-  resources :articles
+  root "articles#index" # set root to direct to index method of articles_controller
+  resources :articles # sets up routes for articles automatically
 end
