90fe3d133420c8e806fa49d31c6d430d410b18ea
naomiamayaa <naomi.colombia.a@gmail.com>
2024-01-29 23:06:57 -0500

    Final changes made to controller, article and html

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,10 @@
 class Article < ApplicationRecord
+  # Validation: Title and content must be present for an article
   validates :title, presence: true
   validates :content, presence: true
 
+  # Class method for searching articles based on a query
+  # Matching titles or content containing the query string are returned
   def self.search(query)
     where('title LIKE ? OR content LIKE ?', "%#{query}%", "%#{query}%")
   end
