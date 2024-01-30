19159fee20e6281b8616491f5599c5bb53308724
Mujtaba Iqbal <iqbalmujtaba10@gmail.com>
2024-01-29 19:30:02 -0500

    finalized views, added some comments for readibility

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,26 @@
 class ArticlesController < ApplicationController
 
+  # GET /articles
+  # Displays search results if search parameter present, otherwise displays list of all articles
   def index
     @articles = params[:search] ? Article.search(params[:search]) : Article.all
   end
 
+  # GET /articles/:id
+  # Redirects user to page for the article with the given id
   def show
     @article = Article.find(params[:id])
     redirect_to articles_path, alert: 'Invalid search query' if @article.nil?
   end
 
+  # GET /articles/new
+  # Redirects to form for adding new article
   def new
     @article = Article.new
   end
 
+  # POST /articles
+  # New article created with given information and redirects to article's page if successful
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -22,10 +30,15 @@ class ArticlesController < ApplicationController
     end
   end
 
+
+  # GET /articles/:id/edit
+  # Redirects to form for editing the article's fields
   def edit
     @article = Article.find(params[:id])
   end
 
+  # PATCH /articles/:id
+  # updates the article's fields, redirects to now-edited article if successful
   def update
     @article = Article.find(params[:id])
     if @article.update(article_params)
@@ -35,6 +48,8 @@ class ArticlesController < ApplicationController
     end
   end
 
+  # DELETE /articles/:id
+  # deletes the article with the given id and redirects to homepage
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
@@ -43,6 +58,7 @@ class ArticlesController < ApplicationController
 
   private
 
+  # Strong parameters, for when new articles are added or existing articles are updated.
   def article_params
     params.require(:article).permit(:title, :content, :author, :date)
   end
