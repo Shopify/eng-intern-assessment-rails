1fea531ce8a6187d6f3861973ded81705ab12532
dingdongg <cs.dg.kim@gmail.com>
2024-01-29 20:04:33 -0800

    add route for search endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get "/articles", to: "articles#index"
   root "articles#index"
+  get "/search", to: "articles#search"
 
   # Defines the root path route ("/")
   # root "posts#index"
