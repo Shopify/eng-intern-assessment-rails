709f3b72cd0748b120306f7a0a55ce4a94826f2e
sean <seanmingli2001@gmail.com>
2024-01-29 23:15:16 -0500

    Added further documentation for the controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,52 +1,58 @@
-# app/controllers/articles_controller.rb
-
 class ArticlesController < ApplicationController
-    before_action :set_article, only: [:show, :edit, :update, :destroy]
-  
-    def index
-      @articles = Article.all
-    end
-  
-    def show
-    end
-  
-    def new
-      @article = Article.new
-    end
-  
-    def create
-      @article = Article.new(article_params)
-      if @article.save
-        redirect_to @article, notice: 'Article was successfully created.'
-      else
-        render :new
-      end
-    end
-  
-    def edit
-    end
-  
-    def update
-      if @article.update(article_params)
-        redirect_to @article, notice: 'Article was successfully updated.'
-      else
-        render :edit
-      end
-    end
-  
-    def destroy
-      @article.destroy
-      redirect_to articles_url, notice: 'Article was successfully destroyed.'
-    end
-  
-    private
-      def set_article
-        @article = Article.find(params[:id])
-      end
-  
-      def article_params
-        params.require(:article).permit(:title, :content, :author, :date)
-      end
-      
+  # Ensures set_article method is called before specified actions to DRY up finding an article by ID
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  # Lists all articles
+  def index
+    @articles = Article.all
   end
-  
+
+  # Shows a single article, @article is set by set_article method
+  def show
+  end
+
+  # Initializes a new article instance for the 'new' form
+  def new
+    @article = Article.new
+  end
+
+  # Creates a new article from the form parameters
+  def create
+    @article = Article.new(article_params)
+    if @article.save # If the article saves successfully
+      redirect_to @article, notice: 'Article was successfully created.'
+    else # If the article doesn't save due to validation failures
+      render :new
+    end
+  end
+
+  # Loads the article for editing, @article is set by set_article method
+  def edit
+  end
+
+  # Updates the article with the form parameters
+  def update
+    if @article.update(article_params) # If the article updates successfully
+      redirect_to @article, notice: 'Article was successfully updated.'
+    else # If the update fails due to validation errors
+      render :edit
+    end
+  end
+
+  # Deletes the article
+  def destroy
+    @article.destroy
+    redirect_to articles_url, notice: 'Article was successfully destroyed.'
+  end
+
+  private
+    # Sets the @article instance variable from the ID in the parameters
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    # Strong parameters: whitelists the parameters allowed for article creation/updating
+    def article_params
+      params.require(:article).permit(:title, :content, :author, :date)
+    end
+end
