bd793afba9eba537cc1a7d0c981c7e647f310842
Ben Grass <bvgrass@outlook.com>
2024-01-29 21:54:42 -0500

    implement destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -35,6 +35,10 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
   end
 
   private
