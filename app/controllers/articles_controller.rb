6e404099dd10880d5b90c0ead1d41d104b6a5ac1
Ben Grass <bvgrass@outlook.com>
2024-01-29 21:31:21 -0500

    add create and edit buttons and implement edit and update functions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,9 +21,17 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
