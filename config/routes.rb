116628184646bf28f9f8e0aedf1485774cd15e4c
geepara <62688021+geepara@users.noreply.github.com>
2024-01-29 19:10:20 -0500

    new and edit views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  root "article#index"
+  root "articles#index"
 
-  resources :article
+  resources :articles
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
