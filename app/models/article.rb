530e9738b916f0bc0bfab02e38f458b98f0ad0e5
dingdongg <cs.dg.kim@gmail.com>
2024-01-29 19:57:45 -0800

    implement class method search()

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+    def self.search( query_string )
+        Article.where(["title LIKE ? OR content LIKE ?", "%#{query_string}%", "%#{query_string}%"])
+    end
 end
