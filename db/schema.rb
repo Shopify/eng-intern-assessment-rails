01f9122cd2306fa19a540c5be2999ef542bab830
Kai Kit Jeffrey Chan <jeffreykkchan@gmail.com>
2025-02-11 15:44:24 -0500

    Creating migration for co-authors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_19_010931) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_11_204305) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
